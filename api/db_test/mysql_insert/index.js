e91dffdf011a6ac098198951aaac494a70c06a8d
justinmealey <justin@pleenq.com>
2015-08-13 13:58:38 -0700

    Added mysql count and insert

---
+++ api/db_test/mysql_insert/index.js
@@ -0,0 +1,20 @@
+/**
+ * API: Db_test: Insert
+ */
+
+// Dependencies
+var Db_test = require('jaws-lib').models.Db_test;
+
+// Function
+exports.handler = function(event, context) {
+    Db_test.insertRow(event.body, function(error, result) {
+        if (error) return context.fail(error);
+
+        /**
+         * Return
+         */
+
+        return context.succeed(result);
+
+    });
+};
