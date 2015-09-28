03e854d925b9dc4d92a556a7c372a83eff9abdbc
Austen Collins <austencollins@gmail.com>
2015-09-28 16:33:07 -0700

    tests: refactor create test project

--- tests/all.js
+++ tests/all.js
@@ -15,9 +15,8 @@ describe('AllTests', function() {
   });
 
   //require tests vs inline so we can run sequentially
-  //require('./cli/tag');
-  //require('./cli/module_install');
-  //require('./cli/env');
+  require('./cli/tag');
+  require('./cli/env');
   //require('./cli/module_create');
   //require('./cli/run');
 
@@ -29,5 +28,5 @@ describe('AllTests', function() {
    //require('./cli/deploy_resources');
    //require('./cli/deploy_endpoint');
    //require('./cli/new_stage_region');
-   require('./cli/new_project');
+   //require('./cli/new_project');
 });
--- tests/cli/tag.js
+++ tests/cli/tag.js
@@ -22,11 +22,11 @@ describe('Test "tag" command', function() {
   before(function(done) {
     testUtils.createTestProject(
             config.name,
-            config.usEast1Bucket,
             config.stage,
+            config.region,
+            config.domain,
             config.iamRoleArnLambda,
-            config.iamRoleArnApiGateway,
-            config.envBucket)
+            config.iamRoleArnApiGateway)
         .then(function(pp) {
           projPath = pp;
           process.chdir(projPath);
