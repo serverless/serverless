b0ab78b4e98d088c0049d05a0b96f7c3e160dcdc
Jeremy McJunkin <jeremy@doapps.com>
2015-10-14 15:00:08 -0500

    java test project

---
+++ tests/test-prj/aws_modules/java/get8/src/main/java/com/java_test/java/Lambda.java
@@ -0,0 +1,9 @@
+package com.java_test.java;
+
+import java.util.Map;
+
+public abstract class Lambda {
+    public static Map<String,Object> get8(Map<String,Object> input) {
+        return input;
+    }
+}
