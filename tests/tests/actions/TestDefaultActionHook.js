b4d2cd436347a80a561080e094d20f0c6db49696
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-12-07 13:08:39 +0200

    added test: DefaultActionHook

--- tests/tests/actions/TestDefaultActionHook.js
+++ tests/tests/actions/TestDefaultActionHook.js
@@ -1,19 +1,64 @@
 'use strict';
 
 /**
- * Test: Module Create Action
- * - Creates a new project in your system's temp directory
- * - Creates a new Module inside test project
+ * Test: Default Action Hook
+ * - Adds a pre hook to a default action (ModuleCreate)
+ * - validates the action ran correctly with the hook attached to the event
  */
 
-let Serverless      = require('../../../lib/Serverless.js'),
-    path      = require('path'),
-    utils     = require('../../../lib/utils/index'),
-    assert    = require('chai').assert,
-    testUtils = require('../../test_utils'),
-    config    = require('../../config');
+let Serverless = require('../../../lib/Serverless.js'),
+    SPlugin    = require('../../../lib/ServerlessPlugin'),
+    path       = require('path'),
+    utils      = require('../../../lib/utils/index'),
+    assert     = require('chai').assert,
+    testUtils  = require('../../test_utils'),
+    config     = require('../../config');
 
 let serverless;
+
+/**
+ * Define Plugin
+ */
+
+class CustomPlugin extends SPlugin {
+
+  constructor(S, config) {
+    super(S, config);
+  }
+
+  /**
+   * Define your plugins name
+   */
+
+  static getName() {
+    return 'com.yourdomain.' + CustomPlugin.name;
+  }
+  
+  /**
+   * Register Hooks
+   */
+
+  registerHooks() {
+
+    this.S.addHook(this._defaultActionPreHook.bind(this), {
+      action: 'moduleCreate',
+      event:  'pre'
+    });
+
+    return Promise.resolve();
+  }
+  
+  _defaultActionPreHook(evt) {
+    let _this = this;
+    return new Promise(function (resolve) {
+      setTimeout(function () {
+        evt.hook = 'defaultActionPreHook';
+        // Add evt data
+        return resolve(evt);
+      }, 250);
+    });
+  }
+}
 
 /**
  * Validate Event
@@ -27,9 +72,11 @@ let validateEvent = function(evt) {
   assert.equal(true, typeof evt.endpoint != 'undefined');
   assert.equal(true, typeof evt.runtime != 'undefined');
   assert.equal(true, typeof evt.pkgMgr != 'undefined');
+  assert.equal(true, typeof evt.hook != 'undefined');
 };
 
-describe('Test action: Module Create', function() {
+
+describe('Test Default Action With Pre Hook', function() {
 
   before(function(done) {
     this.timeout(0);
@@ -39,6 +86,8 @@ describe('Test action: Module Create', function() {
           serverless = new Serverless({
             interactive: false,
           });
+          
+          serverless.addPlugin(new CustomPlugin(serverless, {}));
           done();
         });
   });
@@ -47,9 +96,9 @@ describe('Test action: Module Create', function() {
     done();
   });
 
-  describe('Module Create positive tests', function() {
+  describe('Test Default Action With Pre Hook', function() {
 
-    it('create a new module with defaults', function(done) {
+    it('adds a pre hook to Module Create default Action', function(done) {
       this.timeout(0);
       let event = {
         module:   'temp',
@@ -58,7 +107,7 @@ describe('Test action: Module Create', function() {
 
       serverless.actions.moduleCreate(event)
           .then(function(evt) {
-
+            
             validateEvent(evt);
 
             let functionJson = utils.readAndParseJsonSync(path.join(serverless._projectRootPath, 'back', 'modules', 'temp', 'one', 's-function.json'));
