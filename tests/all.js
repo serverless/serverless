00e099152f6ff4b3810623550e003f6a9235de31
Eslam A. Hefnawy <eahefnawy@gmail.com>
2015-12-07 13:22:15 +0200

    generate help when no commands are provided

--- tests/all.js
+++ tests/all.js
@@ -12,7 +12,7 @@ describe('All Tests', function() {
   after(function() {});
 
   //require('./tests/actions/TestPluginCustom');
-  //require('./tests/actions/TestDefaultActionHook');
+  require('./tests/actions/TestDefaultActionHook');
   //require('./tests/actions/ProjectCreate');
   //require('./tests/actions/StageCreate');
   //require('./tests/actions/RegionCreate');
@@ -23,7 +23,7 @@ describe('All Tests', function() {
   //require('./tests/actions/EnvGet');
   //require('./tests/actions/EnvSetUnset');
   //require('./tests/actions/ResourcesDeploy');
-  require('./tests/actions/FunctionRun');
+  //require('./tests/actions/FunctionRun');
   //require('./tests/actions/FunctionDeploy');
 
 });
