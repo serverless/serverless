b82381bc28ebe67ef3fdad03d5d14439e4e3548f
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-01-05 21:16:53 +0200

    refactored get methods on classes

--- tests/all.js
+++ tests/all.js
@@ -11,19 +11,19 @@ describe('All Tests', function() {
 
   after(function() {});
 
-  //require('./tests/actions/TestPluginCustom');
-  //require('./tests/actions/TestDefaultActionHook');
-  //require('./tests/actions/ProjectCreate');
-  //require('./tests/actions/StageCreate');
-  //require('./tests/actions/RegionCreate');
-  //require('./tests/actions/ModuleInstall');
-  //require('./tests/actions/ModuleCreate');
-  //require('./tests/actions/FunctionCreate');
-  //require('./tests/actions/EnvList');
-  //require('./tests/actions/EnvGet');
-  //require('./tests/actions/EnvSetUnset');
-  //require('./tests/actions/ResourcesDeploy');
-  //require('./tests/actions/FunctionRun');
+  require('./tests/actions/TestPluginCustom');
+  require('./tests/actions/TestDefaultActionHook');
+  require('./tests/actions/ProjectCreate');
+  require('./tests/actions/StageCreate');
+  require('./tests/actions/RegionCreate');
+  require('./tests/actions/ModuleInstall');
+  require('./tests/actions/ModuleCreate');
+  require('./tests/actions/FunctionCreate');
+  require('./tests/actions/EnvList');
+  require('./tests/actions/EnvGet');
+  require('./tests/actions/EnvSetUnset');
+  require('./tests/actions/ResourcesDeploy');
+  require('./tests/actions/FunctionRun');
   require('./tests/actions/FunctionDeploy');
-  //require('./tests/actions/EndpointDeploy');
+  require('./tests/actions/EndpointDeploy');
 });
