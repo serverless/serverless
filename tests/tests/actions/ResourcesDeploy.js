e9ec9abe29d95be7d6ae78f0d9131c53527f05c0
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-01-17 14:21:54 +0200

    fixed resources deploy test

--- tests/tests/actions/ResourcesDeploy.js
+++ tests/tests/actions/ResourcesDeploy.js
@@ -50,11 +50,11 @@ describe('Test action: Resources Deploy', function() {
 
             SUtils.sDebug('Adding test bucket resource');
 
-            let Project = new serverless.classes.Project(serverless);
-            // generate unique bucket name to get a unique CF template on every test.
-            // CF will remove the previous bucket for us!
-            Project.cloudFormation.Resources['testBucket' + (new Date).getTime().toString()] = { "Type" : "AWS::S3::Bucket" };
-            return Project.save()
+            let newProject = serverless.state.project.get();
+            newProject.cloudFormation.Resources['testBucket' + (new Date).getTime().toString()] = { "Type" : "AWS::S3::Bucket" };
+            serverless.state.set({project: newProject});
+
+            return serverless.state.save()
               .then(function() {
                 done();
               });
