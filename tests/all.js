e8758ebbbbd66c4ad18e19f806670474da4d2fee
ac360 <austen@serverless.com>
2016-01-21 16:16:43 -0800

    ComponentCreate, ModuleCreate, FunctionCreate: use state.setAsset() on creation to add new items to the state before save.

--- tests/all.js
+++ tests/all.js
@@ -20,9 +20,9 @@ describe('All Tests', function() {
   //require('./tests/actions/TestDefaultActionHook');
   //require('./tests/actions/StageCreate');
   //require('./tests/actions/RegionCreate');
-  //require('./tests/actions/ComponentCreate');
-  //require('./tests/actions/ModuleCreate');
-  //require('./tests/actions/FunctionCreate');
+  require('./tests/actions/ComponentCreate');
+  require('./tests/actions/ModuleCreate');
+  require('./tests/actions/FunctionCreate');
   //require('./tests/actions/EnvList');
   //require('./tests/actions/EnvGet');
   //require('./tests/actions/EnvSetUnset');
