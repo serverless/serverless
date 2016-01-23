a8250978dee5c07c7b85dd7f392364adb6465eaf
Ryan S. Brown <sb@ryansb.com>
2016-01-23 09:44:06 -0500

    Add component `lib` import to example handler

--- lib/templates/python2.7/handler.py
+++ lib/templates/python2.7/handler.py
@@ -6,6 +6,13 @@ import logging
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
+# this adds the component-level `lib` directory to the Python import path
+import sys
+sys.path.append("../../")
+
+# import the shared library, now anything in component/lib/__init__.py can be
+# referenced as `lib.something`
+import lib
 
 def handler(event, context):
     log.debug("Received event {}".format(json.dumps(event)))
