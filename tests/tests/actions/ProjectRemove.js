2b5c89ebcbbc42e16c918214a9312e47cfe4fd78
Egor Kislitsyn <egor@kislitsyn.com>
2016-01-29 11:20:43 +0700

    adds ProjectRemove and StageRemove actions

---
+++ tests/tests/actions/ProjectRemove.js
@@ -0,0 +1,76 @@
+
+'use strict';
+
+/**
+ * Test: Project Remove Action
+ */
+
+let Serverless    = require('../../../lib/Serverless'),
+    path          = require('path'),
+    assert        = require('chai').assert,
+    testUtils     = require('../../test_utils'),
+    config        = require('../../config');
+
+let serverless;
+
+/**
+ * Validate Event
+ * - Validate an event object's properties
+ */
+
+let validateEvent = function(evt) {
+  assert.equal(true, typeof evt.data !== 'undefined');
+  assert.equal(true, typeof evt.data.project !== 'undefined');
+};
+
+/**
+ * Tests
+ */
+
+describe('Test Action: Project Remove', function() {
+
+  before(function(done) {
+    this.timeout(0);
+
+    testUtils.createTestProject(config)
+      .then(projPath => {
+        this.timeout(0);
+        process.chdir(projPath);  // Ror some weird reason process.chdir adds /private/ before cwd path
+
+        serverless = new Serverless({
+          interactive: false,
+          awsAdminKeyId:     config.awsAdminKeyId,
+          awsAdminSecretKey: config.awsAdminSecretKey,
+          projectPath: projPath
+        });
+
+        return serverless.state.load().then(function() {
+          done();
+        });
+      });
+  });
+
+  after(function(done) {
+    done();
+  });
+
+  describe('Project Remove positive tests', function() {
+    it('should remove project', function(done) {
+      this.timeout(0);
+
+      let evt = {
+        options: {
+          noExeCf:    config.noExecuteCf
+        }
+      };
+
+      serverless.actions.projectRemove(evt)
+        .then(function(evt) {
+          // Validate Event
+          validateEvent(evt);
+          done()
+        })
+        .catch(done);
+    });
+  });
+});
