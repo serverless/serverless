f82d9a5b1761d76a83943bd2a398607caa8d4d07
Egor Kislitsyn <egor@kislitsyn.com>
2016-02-04 19:35:37 +0700

    improves FunctionLogs action

--- tests/tests/actions/FunctionLogs.js
+++ tests/tests/actions/FunctionLogs.js
@@ -7,17 +7,9 @@
  */
 
 let Serverless = require('../../../lib/Serverless.js'),
-    path       = require('path'),
-    utils      = require('../../../lib/utils/index'),
     assert     = require('chai').assert,
     testUtils  = require('../../test_utils'),
     config     = require('../../config');
-
-let Lambda     = require('../../../lib/utils/aws/Lambda')({
-      region:          config.region,
-      accessKeyId:     config.awsAdminKeyId,
-      secretAccessKey: config.awsAdminSecretKey
-    });
 
 let serverless;
 
@@ -27,21 +19,26 @@ let serverless;
  */
 
 let validateEvent = function(evt) {
-  assert.equal(true, typeof evt.options.component != 'undefined');
-  assert.equal(true, typeof evt.options.module != 'undefined');
-  assert.equal(true, typeof evt.options.function != 'undefined');
+  assert.equal(true, typeof evt.options.stage != 'undefined');
+  assert.equal(true, typeof evt.options.region != 'undefined');
   assert.equal(true, typeof evt.data.results != 'undefined');
+};
+
+
+const evt = {
+  options: {
+    stage:     config.stage,
+    region:    config.region,
+    duration: '7days',
+    path: 'nodejscomponent/module1/function1'
+  }
 };
 
 describe('Test action: Function Logs', function() {
   this.timeout(0);
 
   before(function() {
-    return Lambda.invokePromised({
-      FunctionName: 's-test-prj-nodejscomponent-module1-function1',
-      Qualifier: config.stage
-    })
-    .then(() => testUtils.createTestProject(config))
+    return testUtils.createTestProject(config)
     .then(projPath => {
       process.chdir(projPath);
 
@@ -52,24 +49,16 @@ describe('Test action: Function Logs', function() {
         projectPath: projPath
       });
       return serverless.state.load();
+    })
+    .then(() => {
+      return serverless.actions.functionInvoke(evt);
     });
   });
 
   describe('Function Logs positive tests', function() {
 
     it('should get logs for the function', function() {
-      let evt = {
-        options: {
-          component: 'nodejscomponent',
-          module:    'module1',
-          function:  'function1',
-          stage:     config.stage,
-          region:    config.region,
-          duration: '7days',
-          path: 'nodejscomponent/module1/function1'
-        }
 
-      };
 
       return serverless.actions.functionLogs(evt)
         .then(validateEvent);
