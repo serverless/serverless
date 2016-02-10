8dc9057f424cf43f4f237fd850b9a01a752fdf4c
ac360 <austen@serverless.com>
2016-02-10 13:43:27 -0800

    DashDeploy: new hotkeys

--- tests/tests/classes/ServerlessProjectTest.js
+++ tests/tests/classes/ServerlessProjectTest.js
@@ -65,6 +65,7 @@ describe('Test Serverless Project Class', function() {
 
     it('Get populated instance data', function(done) {
       let data = instance.getPopulated({ stage: config.stage, region: config.region });
+      console.log("here", JSON.stringify(data))
       assert.equal(true, JSON.stringify(data).indexOf('$${') == -1);
       assert.equal(true, JSON.stringify(data).indexOf('${') == -1);
       // We've set a template in the project that gets extended at the module level and function level, check it:
