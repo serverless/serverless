663f6ab3f4ca059aa39c5784ba8f12666b1de545
Egor Kislitsyn <egor@kislitsyn.com>
2016-03-14 21:46:44 +0700

    Moves ENV vars to s-function.json

--- tests/all.js
+++ tests/all.js
@@ -17,9 +17,6 @@ describe('All Tests', function() {
   require('./tests/actions/StageCreate');
   require('./tests/actions/RegionCreate');
   require('./tests/actions/FunctionCreate');
-  require('./tests/actions/EnvList');
-  require('./tests/actions/EnvGet');
-  require('./tests/actions/EnvSetUnset');
   require('./tests/actions/ResourcesDeploy');
   require('./tests/actions/FunctionRun');
   require('./tests/actions/FunctionLogs');
