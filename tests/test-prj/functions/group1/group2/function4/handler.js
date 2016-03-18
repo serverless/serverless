adeb2f3d0721716c87a699c9ab1509047cd3222a
ac360 <austen@serverless.com>
2016-03-17 23:06:08 -0700

    utils: remove persistent request and rely on AWS SDK's new rety logic

--- tests/test-prj/functions/group1/group2/function4/handler.js
+++ tests/test-prj/functions/group1/group2/function4/handler.js
@@ -1,6 +1,39 @@
-'use strict';
 
-// Lambda Handler
-module.exports.handler = function(event, context) {
-  return context.done(null, { message: '"functionFour" lambda function has run successfully' });
+console.log('Loading function');
+
+exports.handler = function(event, context) {
+  var token = event.authorizationToken;
+  // Call oauth provider, crack jwt token, etc.
+  // In this example, the token is treated as the status for simplicity.
+
+  switch (token) {
+    case 'allow':
+      context.succeed(generatePolicy('user', 'Allow', event.methodArn));
+      break;
+    case 'deny':
+      context.succeed(generatePolicy('user', 'Deny', event.methodArn));
+      break;
+    case 'unauthorized':
+      context.fail("Unauthorized");
+      break;
+    default:
+      context.fail("error");
+  }
+};
+
+var generatePolicy = function(principalId, effect, resource) {
+  var authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = '2012-10-17'; // default version
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = 'execute-api:Invoke'; // default action
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
 };
