64ca7c5e1d4392503b924cc911232bec1fa005b0
Egor Kislitsyn <egor@kislitsyn.com>
2016-04-20 16:35:46 +0700

    adds `eslint` and and the Airbnb JS style rules config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [],
+  "rules": {
+    "func-names": "off",
+
+    // doesn't work in node v4 :(
+    "strict": "off",
+    "prefer-rest-params": "off",
+  }
+};
