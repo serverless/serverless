da11c08e79a0d87b1289bcbbfaf246a5316904bf
Philipp Muens <raute1337@gmx.de>
2016-05-11 16:37:40 +0200

    Add additional test for Utils class

--- tests/tests/classes/Utils.js
+++ tests/tests/classes/Utils.js
@@ -1,32 +1,97 @@
 'use strict';
 
 /**
- * Test: Serverless Function Class
+ * Test: Utils Function Class
  */
 
-const assert = require('chai').assert,
-	    Utils = require('../../../lib/classes/Utils')({});
+const path = require('path');
+const os = require('os');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+const Utils = require('../../../lib/classes/Utils')({});
 
+const SUtils = new Utils();
 
-let SUtils = new Utils();
-
-describe('Test Utils Class', () => {
+describe('Utils class tests', () => {
 
   after((done) => {
     done();
   });
 
-  it('Test Export Object', () => {
+  it('should export an object', () => {
     const data = {
-      class: 'SampleClass',
+      _class: 'SampleClass',
       publicProp: 'somethingPublic',
-      functionProp: () => {}
+      functionProp: () => {
+      }
     };
 
     const Obj = SUtils.exportObject(data);
-		assert.equal(Obj.publicProp, 'somethingPublic');
-		assert.equal(typeof Obj._class, 'undefined');
-		assert.equal(typeof Obj.functionProp, 'undefined');
-	});
+    assert.equal(Obj.publicProp, 'somethingPublic');
+    assert.equal(typeof Obj._class, 'undefined');
+    assert.equal(typeof Obj.functionProp, 'undefined');
+  });
+
+  it('should generate a shortId', () => {
+    const id = SUtils.generateShortId(6);
+    assert.equal(typeof id, 'string');
+    assert.equal(id.length, 6);
+  });
+
+  it('should check if a directory exists synchronously', () => {
+    const dir = SUtils.dirExistsSync(__dirname);
+    const noDir = SUtils.dirExistsSync(path.join(__dirname, '..', 'XYZ'));
+
+    assert.equal(dir, true);
+    assert.equal(noDir, false);
+  });
+
+  it('should check if a file exists synchronously', () => {
+    const file = SUtils.fileExistsSync(__filename);
+    const noFile = SUtils.fileExistsSync(path.join(__dirname, 'XYZ.json'));
+
+    assert.equal(file, true);
+    assert.equal(noFile, false);
+  });
+
+  it('should write a file synchronously', () => {
+    let tmpFilePath = path.join(os.tmpdir(), (new Date).getTime().toString(), 'anything.json');
+
+    SUtils.writeFileSync(tmpFilePath, { foo: 'bar' });
+    let obj = SUtils.readFileSync(tmpFilePath);
+
+    assert.equal(obj.foo, 'bar');
+  });
+
+  it('should write a file asynchronously', () => {
+    let tmpFilePath = path.join(os.tmpdir(), (new Date).getTime().toString(), 'anything.json');
+
+    // note: use return when testing promises otherwise you'll have unhandled rejection errors
+    return SUtils.writeFile(tmpFilePath, { foo: 'bar' }).then(() => {
+      let obj = SUtils.readFileSync(tmpFilePath);
+
+      expect(obj.foo).to.equal('bar');
+    });
+  });
+
+  it('should read a file synchronously', () => {
+    let tmpFilePath = path.join(os.tmpdir(), (new Date).getTime().toString(), 'anything.json');
+
+    SUtils.writeFileSync(tmpFilePath, { foo: 'bar' });
+    let obj = SUtils.readFileSync(tmpFilePath);
+
+    assert.equal(obj.foo, 'bar');
+  });
+
+  it('should read a file asynchronously', () => {
+    let tmpFilePath = path.join(os.tmpdir(), (new Date).getTime().toString(), 'anything.json');
+
+    SUtils.writeFileSync(tmpFilePath, { foo: 'bar' });
+
+    // note: use return when testing promises otherwise you'll have unhandled rejection errors
+    return SUtils.readFile(tmpFilePath).then((obj) => {
+      expect(obj.foo).to.equal('bar');
+    });
+  });
 
 });
