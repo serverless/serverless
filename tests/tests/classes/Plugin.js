7e60d9e6e21552ecb699195271860c41fb63f7c3
Philipp Muens <raute1337@gmx.de>
2016-05-13 08:15:39 +0200

    Refactor Plugin class and Plugin tests

--- tests/tests/classes/Plugin.js
+++ tests/tests/classes/Plugin.js
@@ -6,17 +6,21 @@
 
 const expect = require('chai').expect;
 const Serverless = require('../../../lib/Serverless');
+const Plugin = require('../../../lib/classes/Plugin');
 
-// create a new Serverless instance
-const serverless = new Serverless({
-  interactive: false,
-});
+describe('Plugin', () => {
 
-const Plugin = require('../../../lib/classes/Plugin')(serverless);
+  let SPlugin;
+  let serverless;
 
-const SPlugin = new Plugin();
-
-describe('Plugin class', () => {
+  beforeEach(() => {
+    // create a new Serverless instance
+    serverless = new Serverless({
+      interactive: false,
+    });
+    
+    SPlugin = new Plugin(serverless);
+  });
 
   it('should return a valid name', () => {
     const name = SPlugin.getName();
@@ -37,7 +41,12 @@ describe('Plugin class', () => {
     });
   });
 
-  describe('Add action', () => {
+  describe('#addAction()', () => {
+
+    beforeEach(() => {
+      SPlugin.addAction(actionMock, actionMockConfig);
+      SPlugin.addAction(actionMock, actionMockConfigSimple);
+    });
 
     const actionMock = () => { return { foo: 'bar' }; };
     const actionMockConfig = {
@@ -65,74 +74,71 @@ describe('Plugin class', () => {
       contextAction: 'mock',
     };
 
-    SPlugin.addAction(actionMock, actionMockConfig);
-    SPlugin.addAction(actionMock, actionMockConfigSimple);
-
-    it('should have an empty options property when adding an action', () => {
+    it('should have an empty options property', () => {
       expect(serverless.commands.action2.mock.options.length).to.equal(0);
     });
 
-    it('should have an empty parameters property when adding an action', () => {
+    it('should have an empty parameters property', () => {
       expect(serverless.commands.action2.mock.parameters.length).to.equal(0);
     });
 
-    it('should have an options property when adding an action', () => {
+    it('should have an options property', () => {
       expect(serverless.commands.action.mock).to.have.property('options');
     });
 
-    it('should have an action property when adding an action', () => {
+    it('should have an action property', () => {
       expect(serverless.actions).to.have.property('actionMock');
     });
 
-    it('should have a mock property when adding an action', () => {
+    it('should have a mock property', () => {
       expect(serverless.commands.action).to.have.property('mock');
     });
 
-    it('should have a parameters property when adding an action', () => {
+    it('should have a parameters property', () => {
       expect(serverless.commands.action.mock).to.have.property('parameters');
     });
 
-    it('should add a context action when adding an action', () => {
+    it('should add a context action', () => {
       expect(serverless.commands.action.mock.contextAction).to.equal('mock');
     });
 
-    it('should add a handler when adding an action', () => {
+    it('should add a handler', () => {
       expect(serverless.commands.action.mock.handler).to.equal('actionMock');
     });
 
-    it('should add a context when adding an action', () => {
+    it('should add a context', () => {
       expect(serverless.commands.action.mock.context).to.equal('action');
     });
 
-    it('should have a specified options array when adding an action', () => {
+    it('should have a specified options array', () => {
       expect(serverless.commands.action.mock.options.length).to.equal(1);
     });
 
-    it('should have an option inside the options array when adding an action', () => {
+    it('should have an option inside the options array', () => {
       expect(serverless.commands.action.mock.options[0].option).to.equal('mockOption');
     });
 
-    it('should have a shortcut inside the options array when adding an action', () => {
+    it('should have a shortcut inside the options array', () => {
       expect(serverless.commands.action.mock.options[0].shortcut).to.equal('m');
     });
 
-    it('should have a description inside the options array when adding an action', () => {
+    it('should have a description inside the options array', () => {
       expect(serverless.commands.action.mock.options[0].description).to.equal('Mock option');
     });
 
-    it('should have a specified parameters array when adding an action', () => {
+    it('should have a specified parameters array', () => {
       expect(serverless.commands.action.mock.parameters.length).to.equal(1);
     });
 
-    it('should have a parameter inside the parameters array when adding an action', () => {
+    it('should have a parameter inside the parameters array', () => {
       expect(serverless.commands.action.mock.parameters[0].parameter).to.equal('mockParam');
     });
 
-    it('should have a description inside the parameters array when adding an action', () => {
+    it('should have a description inside the parameters array', () => {
       expect(serverless.commands.action.mock.parameters[0].description).to.equal('Mock param');
     });
 
-    it('should have a position inside the parameters array when adding an action', () => {
+    it('should have a position inside the parameters array', () => {
       expect(serverless.commands.action.mock.parameters[0].position).to.equal('0->');
     });
 
@@ -153,60 +159,77 @@ describe('Plugin class', () => {
 
   });
 
-  describe('Add a pre hook', () => {
+  describe('#addHook()', () => {
 
-    before((done) => {
-      const preHookMock = () => { return true; };
+    describe('when adding a pre hook', () => {
+
+      const preHookMock = () => {
+        return true;
+      };
       const preHookMockConfig = {
         action: 'actionMock',
         event: 'pre',
       };
+      const actionMock = () => { return { foo: 'bar' }; };
+      const actionMockConfig = {
+        handler: 'actionMock',
+        context: 'action',
+        contextAction: 'mock',
+      };
 
-      SPlugin.addHook(preHookMock, preHookMockConfig);
+      beforeEach(() => {
+        SPlugin.addAction(actionMock, actionMockConfig);
+        SPlugin.addHook(preHookMock, preHookMockConfig);
+      });
 
-      done();
+      it('should have a specified preHooks array', () => {
+        expect(serverless.hooks.actionMockPre.length).to.equal(1);
+      });
+
+      it('should have an empty postHooks array', () => {
+        expect(serverless.hooks.actionMockPost.length).to.equal(0);
+      });
+
+      it('should have a function as a pre hook', () => {
+        expect(serverless.hooks.actionMockPre[0]).to.be.a('function');
+      });
+
     });
 
-    it('should have a specified preHooks array when adding a pre hook', () => {
-      expect(serverless.hooks.actionMockPre.length).to.equal(1);
-    });
+    describe('When adding a post hook', () => {
 
-    it('should have an empty postHooks array when adding a pre hook', () => {
-      expect(serverless.hooks.actionMockPost.length).to.equal(0);
-    });
-
-    it('should have a function as a pre hook', () => {
-      expect(serverless.hooks.actionMockPre[0]).to.be.a('function');
-    });
-
-  });
-
-  describe('Add a post hook', () => {
-
-    before((done) => {
-      const postHookMock = () => { return true; };
+      const postHookMock = () => {
+        return true;
+      };
       const postHookMockConfig = {
         action: 'actionMock',
         event: 'post',
       };
+      const actionMock = () => { return { foo: 'bar' }; };
+      const actionMockConfig = {
+        handler: 'actionMock',
+        context: 'action',
+        contextAction: 'mock',
+      };
 
-      SPlugin.addHook(postHookMock, postHookMockConfig);
+      beforeEach(() => {
+        SPlugin.addAction(actionMock, actionMockConfig);
+        SPlugin.addHook(postHookMock, postHookMockConfig);
+      });
 
-      done();
+      it('should have a specified postHooks array', () => {
+        expect(serverless.hooks.actionMockPost.length).to.equal(1);
+      });
+
+      it('should have an empty preHooks array', () => {
+       expect(serverless.hooks.actionMockPre.length).to.equal(0);
+      });
+
+      it('should have a function as a post hook', () => {
+        expect(serverless.hooks.actionMockPost[0]).to.be.a('function');
+      });
+
     });
-
-    it('should have a specified postHooks array when adding a post hook', () => {
-      expect(serverless.hooks.actionMockPost.length).to.equal(1);
-    });
-
-    it('should not remove the pre hook from the preHooks array when adding a post hook', () => {
-      expect(serverless.hooks.actionMockPre.length).to.equal(1);
-    });
-
-    it('should have a function as a post hook', () => {
-      expect(serverless.hooks.actionMockPost[0]).to.be.a('function');
-    });
-
   });
 
 });
