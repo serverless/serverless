db1a6e7389728230e8f590ab624eb62cffcff3fe
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-05-16 21:50:28 +0200

    basic new plugin system

--- tests/all.js
+++ tests/all.js
@@ -7,8 +7,9 @@ describe('All Tests', function() {
   before(function() {});
   after(function() {});
 
-  require('./tests/classes/Utils');
-  //require('./tests/classes/Plugin');
+  require('./tests/classes/Serverless');
+  // require('./tests/classes/Utils');
+  // require('./tests/classes/Plugin');
   // require('./tests/classes/Project');
   // require('./tests/classes/ProviderAws');
   // require('./tests/classes/Function');
