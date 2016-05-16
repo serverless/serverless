0338119a46d9bf861528e57a801623d234b4871f
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-05-16 22:17:03 +0200

    further cleaning of new plugin system

--- tests/tests/classes/Serverless.js
+++ tests/tests/classes/Serverless.js
@@ -8,117 +8,108 @@ const expect = require('chai').expect;
 const Serverless = require('../../../lib/Serverless');
 
 describe('Serverless', () => {
+  let serverless;
 
-	let serverless;
+  beforeEach(() => {
+    serverless = new Serverless({
+      interactive: false,
+    });
+  });
 
-	beforeEach(() => {
-		// create a new Serverless instance
-		serverless = new Serverless({
-			interactive: false,
-		});
-	});
+  describe('addPlugin()', () => {
+    it('should add plugins', () => {
+      class PluginMock {
+        constructor() {
+          this.commands = {};
+        }
+      }
 
-	describe('addPlugin()', () => {
+      serverless.addPlugin(PluginMock);
 
-		it('should add plugins', () => {
+      expect(serverless._plugins.length).to.be.equal(1);
+      expect(typeof serverless._plugins[0]).to.be.equal('object');
+    });
+  });
 
-			class PluginMock {
-				constructor() {
-					this.commands = {}
-				}
-			}
+  describe('loadCommands()', () => {
+    it('should load commands', () => {
+      class PluginMock {
+        constructor() {
+          this.commands = {
+            test: {
+              usage: 'test usage',
+            },
+          },
+        }
+      }
 
-			serverless.addPlugin(PluginMock);
+      serverless.addPlugin(PluginMock);
+      serverless.loadCommands();
 
-			expect(serverless._plugins.length).to.be.equal(1);
-			expect(typeof serverless._plugins[0]).to.be.equal('object');
-		});
-	});
+      expect(Object.keys(serverless.commands)[0]).to.be.equal('test');
+    });
+  });
 
-	describe('loadCommands()', () => {
+  describe('runCommand()', () => {
+    beforeEach(() => {
+      const argv = {
+        _: ['test'],
+      };
 
-		it('should load commands', () => {
+      class PluginMock {
+        constructor() {
+          this.commands = {
+            test: {
+              usage: 'test usage',
+              lifeCycleEvents: [
+                'resources',
+                'functions',
+              ],
+            },
+          }
 
-			class PluginMock {
-				constructor() {
-					this.commands = {
-						test: {
-							usage: 'test usage'
-						}
-					}
-				}
-			}
+          this.hooks = {
+            'test:resources': this.deployResources,
+            'test:functions': this.deployFunctions,
+          };
+        }
 
-			serverless.addPlugin(PluginMock);
-			serverless.loadCommands();
+        deployResources() {
+          return;
+        }
 
-			expect(Object.keys(serverless.commands)[0]).to.be.equal('test');
-		});
+        deployFunctions() {
+          return;
+        }
+      }
 
-	});
-
-	describe('runCommand()', () => {
-
-		beforeEach(() => {
-			const argv = {
-				_: ['test']
-			};
-
-			class PluginMock {
-				constructor() {
-					this.commands = {
-						test: {
-							usage: 'test usage',
-							lifeCycleEvents: [
-								'resources',
-								'functions'
-							]
-						}
-					}
-
-					this.hooks = {
-						'test:resources': this.deployResources,
-						'test:functions': this.deployFunctions,
-					};
-				}
-
-				deployResources() {
-					return;
-				}
-
-				deployFunctions() {
-					return;
-				}
-			};
-
-			serverless.addPlugin(PluginMock);
-			serverless.loadCommands();
-			serverless.runCommand(argv);
-		});
+      serverless.addPlugin(PluginMock);
+      serverless.loadCommands();
+      serverless.runCommand(argv);
+    });
 
 
-		it('should load plugin events', () => {
-			expect(serverless.events.length).to.be.equal(6);
-		});
+    it('should load plugin events', () => {
+      expect(serverless.events.length).to.be.equal(6);
+    });
 
-		it('should load plugin cli commands', () => {
-			expect(serverless.cli.raw._[0]).to.be.equal('test');
-		});
+    it('should load plugin cli commands', () => {
+      expect(serverless.cli.raw._[0]).to.be.equal('test');
+    });
 
-		it('should load plugin cli hooks', () => {
-			expect(serverless.hooks.length).to.be.equal(2);
-		});
+    it('should load plugin cli hooks', () => {
+      expect(serverless.hooks.length).to.be.equal(2);
+    });
 
-		it('should activate debug mode', () => {
-			serverless.runCommand({debug: true, _: ['test']});
-			expect(process.env.DEBUG).to.be.equal('*');
-		});
+    it('should activate debug mode', () => {
+      serverless.runCommand({ debug: true, _: ['test'] });
+      expect(process.env.DEBUG).to.be.equal('*');
+    });
 
-		it('should return version number', () => {
-			return serverless.runCommand({_: ['version']}).then((version) => {
-				expect(version).to.be.equal('0.5.5');
-			});
-		});
-	});
-
+    it('should return version number', () => {
+      return serverless.runCommand({ _: ['version'] }).then((version) => {
+        expect(version).to.be.equal('0.5.5');
+      });
+    });
+  });
 });
