c7054784a21563ed4159a6fd93fef81b90d6087c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-05-17 17:53:27 +0200

    basic service and config working

---
+++ tests/tests/classes/Service.js
@@ -0,0 +1,169 @@
+'use strict';
+
+/**
+ * Test: Service Class
+ */
+
+const path = require('path');
+const os = require('os');
+const YAML = require('js-yaml');
+const expect = require('chai').expect;
+const Service = require('../../../lib/classes/Service');
+const Utils = require('../../../lib/classes/Utils');
+const Serverless = require('../../../lib/Serverless');
+
+
+
+describe('Service class', () => {
+
+  after((done) => {
+    done();
+  });
+
+  const S = new Serverless();
+  
+  it('should construct with defaults', () => {
+    const serviceInstance = new Service(S);
+
+    expect(serviceInstance.service).to.be.equal(null);
+    expect(serviceInstance.custom).to.deep.equal({});
+    expect(serviceInstance.plugins).to.deep.equal([]);
+    expect(serviceInstance.functions).to.deep.equal({});
+    expect(serviceInstance.environment).to.deep.equal({});
+    expect(serviceInstance.resources.aws).to.deep.equal({});
+    expect(serviceInstance.resources.azure).to.deep.equal({});
+    expect(serviceInstance.resources.google).to.deep.equal({});
+
+  });
+
+  it('should construct with data', () => {
+    const data = {
+      service: 'testService',
+      custom: {
+        customProp: 'value'
+      },
+      plugins: ['testPlugin'],
+      functions: {
+        functionA: {}
+      },
+      resources: {
+        aws: {
+          resourcesProp: 'value'
+        },
+        azure: {},
+        google: {}
+      }
+    };
+
+    const serviceInstance = new Service(S, data);
+
+    expect(serviceInstance.service).to.be.equal('testService');
+    expect(serviceInstance.custom).to.deep.equal({ customProp: 'value' });
+    expect(serviceInstance.plugins).to.deep.equal(['testPlugin']);
+    expect(serviceInstance.functions).to.deep.equal({ functionA: {} });
+    expect(serviceInstance.environment).to.deep.equal({});
+    expect(serviceInstance.resources.aws).to.deep.equal({ resourcesProp: 'value' });
+    expect(serviceInstance.resources.azure).to.deep.equal({});
+    expect(serviceInstance.resources.google).to.deep.equal({});
+  });
+
+  let serviceInstance;
+  describe('load()', () => {
+
+    before(() => {
+      const SUtils = new Utils();
+      const tmpDirPath = path.join(os.tmpdir(), (new Date).getTime().toString());
+      const serverlessYaml = {
+        service: 'testService',
+        custom: {
+          customProp: 'value'
+        },
+        plugins: ['testPlugin'],
+        functions: {
+          functionA: {}
+        },
+        resources: {
+          aws: {
+            resourcesProp: 'value'
+          },
+          azure: {},
+          google: {}
+        }
+      };
+      const serverlessEnvYaml = {
+        vars: {
+          varA: 'varA'
+        },
+        stages: {
+          dev: {
+            vars: {
+              varB: 'varB'
+            },
+            regions: {
+              aws_useast1: {
+                vars: {
+                  varC: 'varC'
+                }
+              }
+            }
+          }
+        }
+      };
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.yaml'), YAML.dump(serverlessYaml));
+      SUtils.writeFileSync(path.join(tmpDirPath, 'serverless.env.yaml'), YAML.dump(serverlessEnvYaml));
+
+      const S = new Serverless({ servicePath: tmpDirPath })
+      serviceInstance = new Service(S);
+    });
+
+    /*
+     * I wanna split this into seperate tests, but that would mean reading
+     * from the file system on every test, because the load() method returns a promise
+     * so we can't keep it in the before() block
+     */
+    it('should load from filesystem', () => {
+
+      return serviceInstance.load().then((serviceInstance) => {
+        expect(serviceInstance.service).to.be.equal('testService');
+        expect(serviceInstance.custom).to.deep.equal({ customProp: 'value' });
+        expect(serviceInstance.plugins).to.deep.equal(['testPlugin']);
+        expect(serviceInstance.environment.vars).to.deep.equal({ varA: 'varA' });
+        expect(serviceInstance.environment.stages.dev.regions.aws_useast1.vars.varC).to.be.equal('varC');
+        expect(serviceInstance.resources.aws).to.deep.equal({ resourcesProp: 'value' });
+        expect(serviceInstance.resources.azure).to.deep.equal({});
+        expect(serviceInstance.resources.google).to.deep.equal({});
+      });
+
+    });
+
+    it('should throw error if servicePath not configured', () => {
+
+    });
+
+  });
+
+  describe('save()', () => {
+
+    it('should save data', () => {
+
+    });
+
+  });
+
+  describe('toObject()', () => {
+
+    it('should convert service instance to object', () => {
+
+    });
+
+  });
+
+  describe('toObjectPopulated()', () => {
+
+    it('should convert service instance to POPULATED object', () => {
+
+    });
+
+  });
+
+});
