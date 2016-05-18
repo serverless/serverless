cc6aed7c2f1c8fc254c5fff25f845c382d62adf8
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-05-18 14:51:28 +0200

    added getVersion test

--- tests/all.js
+++ tests/all.js
@@ -7,12 +7,12 @@ describe('All Tests', function() {
   before(function() {});
   after(function() {});
 
-  //require('./tests/classes/Config');
+  // require('./tests/classes/Config');
   // require('./tests/classes/Service');
-  // require('./tests/classes/Serverless');
+  require('./tests/classes/Serverless');
   // require('./tests/classes/Utils');
   // require('./tests/classes/Plugin');
-  require('./tests/classes/YamlParser');
+  // require('./tests/classes/YamlParser');
   // require('./tests/classes/Project');
   // require('./tests/classes/ProviderAws');
   // require('./tests/classes/Function');
