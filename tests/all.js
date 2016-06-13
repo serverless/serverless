5007f3b56486d154fe25bedc3f455edfc35abb68
Philipp Muens <raute1337@gmx.de>
2016-06-13 15:07:34 +0200

    Add compileRestApi functionality

--- tests/all.js
+++ tests/all.js
@@ -23,3 +23,4 @@ require('../lib/plugins/awsInvoke/tests/awsInvoke');
 require('../lib/plugins/awsCompileFunctions/tests/awsCompileFunctions');
 require('../lib/plugins/awsCompileS3Events/tests/awsCompileS3Events');
 require('../lib/plugins/awsCompileScheduledEvents/tests/awsCompileScheduledEvents');
+require('../lib/plugins/awsCompileApigEvents/tests/all');
