6191a38e26b6750baf72646499a79f276b4cb262
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-06-17 10:51:37 +0200

    bring back apig tests

--- tests/all.js
+++ tests/all.js
@@ -23,7 +23,7 @@ require('../lib/plugins/aws/deploy/tests/all');
 require('../lib/plugins/aws/deploy/compile/functions/tests');
 require('../lib/plugins/aws/deploy/compile/events/s3/tests');
 require('../lib/plugins/aws/deploy/compile/events/schedule/tests');
-require('../lib/plugins/aws/deploy/compile/events/apiGateway/tests');
+require('../lib/plugins/aws/deploy/compile/events/apiGateway/tests/all');
 require('../lib/plugins/aws/invoke/tests');
 require('../lib/plugins/aws/remove/tests/all');
 
