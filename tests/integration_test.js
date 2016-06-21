afe2321b11ce56bc94042c8b0c7401c31304feb2
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-06-21 13:03:58 +0200

    fixed integration tests

--- tests/integration_test.js
+++ tests/integration_test.js
@@ -14,23 +14,20 @@ serverless.init();
 const serverlessExec = path.join(serverless.config.serverlessPath, '..', 'bin', 'serverless');
 
 const serviceName = `integration-test-${(new Date).getTime().toString()}`;
-const stageName = 'dev';
-const regionName = 'us-east-1';
+const providerName = 'aws';
 const tmpDir = path.join(os.tmpdir(), (new Date).getTime().toString());
 fse.mkdirSync(tmpDir);
 process.chdir(tmpDir);
 
-const CF = new AWS.CloudFormation({ region: regionName });
+const CF = new AWS.CloudFormation({ region: 'us-east-1' });
 BbPromise.promisifyAll(CF, { suffix: 'Promised' });
 
 describe('Service Lifecyle Integration Test', () => {
   it('should create service in tmp directory', () => {
     execSync(`${serverlessExec} create --name ${
       serviceName
-      } --stage ${
-      stageName
-      } --region ${
-      regionName
+      } --provider ${
+      providerName
       }`, { stdio: 'inherit' });
 
     process.chdir(path.join(tmpDir, serviceName));
@@ -40,29 +37,19 @@ describe('Service Lifecyle Integration Test', () => {
       .fileExistsSync(path.join(tmpDir, serviceName, 'serverless.env.yaml'))).to.be.equal(true);
     expect(serverless.utils
       .fileExistsSync(path.join(tmpDir, serviceName, 'handler.js'))).to.be.equal(true);
-    expect(serverless.utils
-      .fileExistsSync(path.join(tmpDir, serviceName, 'package.json'))).to.be.equal(true);
   });
 
   it('should deploy service to aws', function () {
     this.timeout(0);
-    execSync(`${serverlessExec} deploy --stage ${
-      stageName
-      } --region ${
-      regionName
-      }`, { stdio: 'inherit' });
+    execSync(`${serverlessExec} deploy`, { stdio: 'inherit' });
 
-    return CF.describeStacksPromised({ StackName: `${serviceName}-${stageName}` })
+    return CF.describeStacksPromised({ StackName: `${serviceName}-dev` })
       .then(d => expect(d.Stacks[0].StackStatus).to.be.equal('UPDATE_COMPLETE'));
   });
 
   it('should invoke function from aws', function () {
     this.timeout(0);
-    const invoked = execSync(`${serverlessExec} invoke --function hello --stage ${
-      stageName
-      } --region ${
-      regionName
-      }`);
+    const invoked = execSync(`${serverlessExec} invoke --function hello`);
     const result = JSON.parse(new Buffer(invoked, 'base64').toString());
     expect(result.message).to.be.equal('Go Serverless v1.0! Your function executed successfully!');
   });
@@ -79,33 +66,21 @@ describe('Service Lifecyle Integration Test', () => {
 
     serverless.utils.writeFileSync(path.join(tmpDir, serviceName, 'handler.js'), newHandler);
     this.timeout(0);
-    execSync(`${serverlessExec} deploy --stage ${
-      stageName
-      } --region ${
-      regionName
-      }`, { stdio: 'inherit' });
+    execSync(`${serverlessExec} deploy`, { stdio: 'inherit' });
   });
 
   it('should invoke updated function from aws', function () {
     this.timeout(0);
-    const invoked = execSync(`${serverlessExec} invoke --function hello --stage ${
-      stageName
-      } --region ${
-      regionName
-      }`);
+    const invoked = execSync(`${serverlessExec} invoke --function hello`);
     const result = JSON.parse(new Buffer(invoked, 'base64').toString());
     expect(result.message).to.be.equal('Service Update Succeeded');
   });
 
   it('should remove service from aws', function () {
     this.timeout(0);
-    execSync(`${serverlessExec} remove --stage ${
-      stageName
-      } --region ${
-      regionName
-      }`, { stdio: 'inherit' });
+    execSync(`${serverlessExec} remove`, { stdio: 'inherit' });
 
-    return CF.describeStacksPromised({ StackName: `${serviceName}-${stageName}` })
+    return CF.describeStacksPromised({ StackName: `${serviceName}-dev` })
       .then(d => expect(d.Stacks[0].StackStatus).to.be.equal('DELETE_COMPLETE'))
       .catch(e => {
         if (e.message.indexOf('does not exist') > -1) return BbPromise.resolve();
