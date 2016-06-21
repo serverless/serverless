389cbd7de26f9f2cfd5874687b5aaac0ee882e21
Philipp Muens <raute1337@gmx.de>
2016-06-21 11:28:44 +0200

    Fix AwsCompileApigEvents plugin and tests

--- tests/all.js
+++ tests/all.js
@@ -16,10 +16,10 @@ require('../lib/plugins/invoke/tests/invoke');
 require('../lib/plugins/remove/tests/remove');
 
 // AWS Plugins Tests
-// require('../lib/plugins/aws/invoke/tests');
-// require('../lib/plugins/aws/remove/tests/all');
-// require('../lib/plugins/aws/deploy/tests/all');
-// require('../lib/plugins/aws/deploy/compile/functions/tests');
-// require('../lib/plugins/aws/deploy/compile/events/s3/tests');
-// require('../lib/plugins/aws/deploy/compile/events/schedule/tests');
-// require('../lib/plugins/aws/deploy/compile/events/apiGateway/tests/all');
+require('../lib/plugins/aws/invoke/tests');
+require('../lib/plugins/aws/remove/tests/all');
+require('../lib/plugins/aws/deploy/tests/all');
+require('../lib/plugins/aws/deploy/compile/functions/tests');
+require('../lib/plugins/aws/deploy/compile/events/s3/tests');
+require('../lib/plugins/aws/deploy/compile/events/schedule/tests');
+require('../lib/plugins/aws/deploy/compile/events/apiGateway/tests/all');
