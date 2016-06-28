aa3ed17ab78179d4b543b324ebf0e24a48b66695
Eslam A. Hefnawy <eahefnawy@gmail.com>
2016-06-28 13:24:21 +0200

    fix getVersion test

--- tests/classes/Serverless.js
+++ tests/classes/Serverless.js
@@ -2,12 +2,13 @@
 
 const expect = require('chai').expect;
 const Serverless = require('../../lib/Serverless');
+const semverRegex = require('semver-regex');
 
 describe('Serverless', () => {
   describe('#getVersion()', () => {
     it('should get the correct serverless version', () => {
       const serverless = new Serverless();
-      expect(serverless.getVersion()).to.match(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+      expect(semverRegex().test(serverless.getVersion())).to.equal(true);
     });
   });
 });
