41f2af817ae203a209d2a5c10cd2769b7842d8d0
Cyril Scetbon <cscetbon@gmail.com>
2016-06-28 11:09:20 -0400

    Add vendored folder to sys.modules

--- lib/templates/python2.7/handler.py
+++ lib/templates/python2.7/handler.py
@@ -6,6 +6,12 @@ import logging
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
+# this adds the component-level `lib` directory to the Python import path
+import sys, os
+# get this file's directory independent of where it's run from
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "<%= fnRootPath %>vendored"))
+
 def handler(event, context):
     log.debug("Received event {}".format(json.dumps(event)))
     return {}
