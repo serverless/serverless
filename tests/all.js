6a2ef2752be41e7264d01d17516706321449e0a6
Philipp Muens <raute1337@gmx.de>
2016-07-12 13:31:44 +0200

    Add tests for AWS SNS plugin

--- tests/all.js
+++ tests/all.js
@@ -25,3 +25,4 @@ require('../lib/plugins/aws/deploy/compile/functions/tests');
 require('../lib/plugins/aws/deploy/compile/events/s3/tests');
 require('../lib/plugins/aws/deploy/compile/events/schedule/tests');
 require('../lib/plugins/aws/deploy/compile/events/apiGateway/tests/all');
+require('../lib/plugins/aws/deploy/compile/events/sns/tests');
