60aff96d3ae49206f33ec597080db1669d814a34
Philipp Muens <raute1337@gmx.de>
2016-10-05 14:58:38 -0700

    Update aws-python handler file

--- lib/plugins/create/templates/aws-python/handler.py
+++ lib/plugins/create/templates/aws-python/handler.py
@@ -1,5 +1,27 @@
+import json
+
 def hello(event, context):
+    # This code is used so that your function can repond to HTTP events
+    # which use the LAMBDA-PROXY integration
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "custom-header": "Custom header value"
+        },
+        "body": json.dumps(body)
+    };
+
+    return response
+
+    # Use the following code if you're not using the LAMBDA-PROXY integration
+    """
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "event": event
     }
+    """
