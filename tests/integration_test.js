05ec3e809280093596ee38808298caa16dd98c22
Philipp Muens <raute1337@gmx.de>
2016-10-11 19:26:42 -0700

    Update handler files for better onboarding

--- tests/integration_test.js
+++ tests/integration_test.js
@@ -47,7 +47,9 @@ describe('Service Lifecyle Integration Test', () => {
     this.timeout(0);
     const invoked = execSync(`${serverlessExec} invoke --function hello --noGreeting true`);
     const result = JSON.parse(new Buffer(invoked, 'base64').toString());
-    expect(result.message).to.be.equal('Go Serverless v1.0! Your function executed successfully!');
+    // parse it once again because the body is stringified to be LAMBDA-PROXY ready
+    const message = JSON.parse(result.body).message;
+    expect(message).to.be.equal('Go Serverless v1.0! Your function executed successfully!');
   });
 
   it('should deploy updated service to aws', function () {
