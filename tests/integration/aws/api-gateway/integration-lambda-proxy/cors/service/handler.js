bf8f9ad98d57f8635cd93c0e512daeba99504b2c
Philipp Muens <raute1337@gmx.de>
2016-10-19 10:57:04 +0200

    Add additional API Gateway tests for lambda proxy integration

--- tests/integration/aws/api-gateway/integration-lambda-proxy/cors/service/handler.js
+++ tests/integration/aws/api-gateway/integration-lambda-proxy/cors/service/handler.js
@@ -3,6 +3,9 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify({
       message: 'Hello from API Gateway!',
       input: event,
