01f5f943e7d2b8fad89397eed47373bfb253defd
Philipp Muens <raute1337@gmx.de>
2016-11-21 10:38:12 +0100

    Add tests for overwriting environment variables

--- tests/integration/aws/general/environment-variables/service/handler.js
+++ tests/integration/aws/general/environment-variables/service/handler.js
@@ -2,7 +2,6 @@
 
 module.exports.hello = (event, context, callback) => {
   callback(null, {
-    provider_level_variable: process.env.provider_level_variable,
-    function_level_variable: process.env.function_level_variable,
+    environment_variables: process.env,
   });
 };
--- tests/integration/aws/general/environment-variables/tests.js
+++ tests/integration/aws/general/environment-variables/tests.js
@@ -19,8 +19,14 @@ describe('AWS - General: Environment variables test', function () {
 
     const result = JSON.parse(new Buffer(invoked, 'base64').toString());
 
-    expect(result.provider_level_variable).to.be.equal('provider_level');
-    expect(result.function_level_variable).to.be.equal('function_level');
+    expect(result.environment_variables.provider_level_variable_1)
+      .to.be.equal('provider_level_1');
+    expect(result.environment_variables.function_level_variable_1)
+      .to.be.equal('function_level_1');
+    expect(result.environment_variables.function_level_variable_2)
+      .to.be.equal('function_level_2');
+    expect(result.environment_variables.provider_level_variable_2)
+      .to.be.equal('overwritten_by_function');
   });
 
   after(() => {
