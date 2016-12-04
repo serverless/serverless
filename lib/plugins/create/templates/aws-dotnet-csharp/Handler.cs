c9d2fafbbc5b4770ed772f6249a8a2fd16c52fef
Jim Pelletier <mail@thejimmyp.com>
2016-12-04 13:32:11 +1100

    add a dotnet core csharp template

---
+++ lib/plugins/create/templates/aws-dotnet-csharp/Handler.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace AwsDotnetCsharp
+{
+    public class Handler
+    {
+       public Response Hello(Request request)
+       {
+           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
+       }
+    }
+
+    public class Response
+    {
+      public string Message {get; set;}
+      public Request Request {get; set;}
+
+      public Response(string message, Request request){
+        Message = message;
+        Request = request;
+      }
+    }
+
+    public class Request
+    {
+      public string Key1 {get; set;}
+      public string Key2 {get; set;}
+      public string Key3 {get; set;}
+
+      public Request(string key1, string key2, string key3){
+        Key1 = key1;
+        Key2 = key2;
+        Key3 = key3;
+      }
+    }
+}
