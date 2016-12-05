afd839767f836c74cb249e5a23cd2caedd55f479
davidwells <david@inboundnow.com>
2016-12-05 13:18:07 -0800

    fix linting.... ⊂◉‿◉つ

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -1,14 +1,9 @@
 /**
- * This example generated adds content to the repos README.md file
+ * adds content to the repos README.md file
  */
-const fs = require('fs');
 const path = require('path');
 const markdownMagic = require('markdown-magic');
 const remoteRequest = require('markdown-magic/lib/transforms/remote').remoteRequest;
-
-function formatPluginName(string) {
-  return toTitleCase(string.replace(/-/g, ' '));
-}
 
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, txt =>
@@ -16,16 +11,20 @@ function toTitleCase(str) {
   );
 }
 
+function formatPluginName(string) {
+  return toTitleCase(string.replace(/-/g, ' '));
+}
+
 const config = {
   transforms: {
-    GENERATE_SERVERLESS_PLUGIN_TABLE(content, options) {
+    GENERATE_SERVERLESS_PLUGIN_TABLE(content, options) { // eslint-disable-line
       const pluginUrl = 'https://raw.githubusercontent.com/serverless/community-plugins/master/plugins.json';
       const remoteContent = remoteRequest(pluginUrl);
       let md = '| Plugin name | description  |\n';
       md += '|:--------------------------- |:-----|\n';
-      JSON.parse(remoteContent).plugins.sort((a, b) =>
-         a.name < b.name ? -1 : 1
-      ).forEach((data) => {
+      JSON.parse(remoteContent).plugins.sort((a, b) => { // eslint-disable-line 
+        return a.name < b.name ? -1 : 1;
+      }).forEach((data) => {
         md += `| [${formatPluginName(data.name)}](${data.githubUrl}) | ${data.description} |\n`;
       });
       return md.replace(/^\s+|\s+$/g, '');
