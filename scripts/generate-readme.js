1f1b8a91338a3c0613652e5a4eb55c362c9bc115
davidwells <david@inboundnow.com>
2016-12-05 11:55:17 -0800

    fix linting

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -1,42 +1,41 @@
 /**
  * This example generated adds content to the repos README.md file
  */
-const fs = require('fs')
-const path = require('path')
-const fetch = require('node-fetch')
-const markdownMagic = require('markdown-magic')
-const remoteRequest = require('markdown-magic/lib/transforms/remote').remoteRequest
+const fs = require('fs');
+const path = require('path');
+const markdownMagic = require('markdown-magic');
+const remoteRequest = require('markdown-magic/lib/transforms/remote').remoteRequest;
 
 const config = {
   transforms: {
-    GENERATE_SERVERLESS_PLUGIN_TABLE: function(content, options) {
-      const pluginUrl = 'https://raw.githubusercontent.com/serverless/community-plugins/master/plugins.json'
-      const remoteContent = remoteRequest(pluginUrl)
-      let md =  '| Plugin name | description  |\n'
-          md += '|:--------------------------- |:-----|\n'
-      JSON.parse(remoteContent).plugins.sort(function (a, b) {
-          return a.name < b.name ? -1 : 1;
-      }).forEach(function(data) {
-          md += `| [${formatPluginName(data.name)}](${data.githubUrl}) | ${data.description} |\n`
+    GENERATE_SERVERLESS_PLUGIN_TABLE(content, options) {
+      const pluginUrl = 'https://raw.githubusercontent.com/serverless/community-plugins/master/plugins.json';
+      const remoteContent = remoteRequest(pluginUrl);
+      let md = '| Plugin name | description  |\n';
+      md += '|:--------------------------- |:-----|\n';
+      JSON.parse(remoteContent).plugins.sort((a, b) =>
+         a.name < b.name ? -1 : 1
+      ).forEach((data) => {
+        md += `| [${formatPluginName(data.name)}](${data.githubUrl}) | ${data.description} |\n`;
       });
-      return md.replace(/^\s+|\s+$/g, '')
-    }
-  }
-}
+      return md.replace(/^\s+|\s+$/g, '');
+    },
+  },
+};
 
-function formatPluginName (string) {
-  return toTitleCase(string.replace(/-/g, ' '))
+function formatPluginName(string) {
+  return toTitleCase(string.replace(/-/g, ' '));
 }
 
 function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  )
+  return str.replace(/\w\S*/g, txt =>
+     txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+
+  );
 }
 
-const markdownPath = path.join(__dirname, '..', 'README.md')
+const markdownPath = path.join(__dirname, '..', 'README.md');
 // const markdownPath = path.join(__dirname, '..', 'test/fixtures/test.md')
-markdownMagic(markdownPath, config, function() {
-  console.log(`${markdownPath} updated!`)
-})
+markdownMagic(markdownPath, config, () => {
+  console.log(`${markdownPath} updated!`);
+});
