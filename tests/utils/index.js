e41c643ee1d7f667bb1551b1cbde8c45f31ccd7a
Nik Graf <nik@nikgraf.com>
2016-12-16 15:51:11 +0100

    simplify finding the path

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -8,12 +8,9 @@ const BbPromise = require('bluebird');
 const fse = require('fs-extra');
 const execSync = require('child_process').execSync;
 const AWS = require('aws-sdk');
-const Serverless = require('../../lib/Serverless');
 
-const serverless = new Serverless();
-serverless.init();
-const serverlessExec = path.join(serverless.config.serverlessPath, '..', 'bin', 'serverless');
-
+const serverlessExec = path.join(__dirname, '..', '..', 'bin', 'serverless');
+//
 const getTmpDirPath = () => path.join(os.tmpdir(),
   'tmpdirs-serverless', 'serverless', crypto.randomBytes(8).toString('hex'));
 
