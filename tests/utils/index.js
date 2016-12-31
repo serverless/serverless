5c6bdf4b2737a545a8000129958e6f4a3f5256c1
Nik Graf <nik@nikgraf.com>
2016-12-31 09:13:38 +0100

    print out service name

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -6,9 +6,7 @@ const path = require('path');
 const crypto = require('crypto');
 const BbPromise = require('bluebird');
 const fse = require('fs-extra');
-// const execSync = require('child_process').execSync;
-const spawnSync = require('child_process').spawnSync;
-
+const execSync = require('child_process').execSync;
 const AWS = require('aws-sdk');
 
 const serverlessExec = path.join(__dirname, '..', '..', 'bin', 'serverless');
@@ -22,68 +20,6 @@ const replaceTextInFile = (filePath, subString, newSubString) => {
   const fileContent = fs.readFileSync(filePath).toString();
   fs.writeFileSync(filePath, fileContent.replace(subString, newSubString));
 };
-
-function normalizeExecArgs(command /*, options, callback*/) {
-  let options;
-  let callback;
-
-  if (typeof arguments[1] === 'function') {
-    options = undefined;
-    callback = arguments[1];
-  } else {
-    options = arguments[1];
-    callback = arguments[2];
-  }
-
-  // Make a shallow copy so we don't clobber the user's options object.
-  options = Object.assign({}, options);
-  options.shell = typeof options.shell === 'string' ? options.shell : true;
-
-  return {
-    file: command,
-    options: options,
-    callback: callback
-  };
-}
-
-function checkExecSyncError(ret) {
-  console.log('ret: ', ret);
-  if (ret.error || ret.status !== 0) {
-    var err = ret.error;
-    ret.error = null;
-
-    if (!err) {
-      var msg = 'Command failed: ';
-      msg += ret.cmd || ret.args.join(' ');
-      if (ret.stderr && ret.stderr.length > 0)
-        msg += '\n' + ret.stderr.toString();
-      err = new Error(msg);
-    }
-
-    util._extend(err, ret);
-    return err;
-  }
-
-  return false;
-}
-
-function execSync(command /*, options*/) {
-  var opts = normalizeExecArgs.apply(null, arguments);
-  var inheritStderr = !opts.options.stdio;
-
-  var ret = spawnSync(opts.file, opts.options);
-  ret.cmd = command;
-
-  if (inheritStderr && ret.stderr)
-    process.stderr.write(ret.stderr);
-
-  var err = checkExecSyncError(ret);
-
-  if (err)
-    throw err;
-  else
-    return ret.stdout;
-}
 
 module.exports = {
   serverlessExec,
@@ -163,19 +99,8 @@ module.exports = {
   },
 
   deployService() {
-
-
-    console.log('serverlessExec ', serverlessExec);
-    try {
-      execSync(`${serverlessExec} deploy`, { stdio: 'inherit' });
-
-      var opts = normalizeExecArgs.apply(null, arguments);
-      var inheritStderr = !opts.options.stdio;
-
-    } catch (err) {
-      console.log('err ', err);
-      throw err;
-    }
+    console.log('SERVICE NAME: ', fs.readFileSync('serverless.yml').toString().split('\n')[0]);
+    execSync(`${serverlessExec} deploy`, { stdio: 'inherit' });
   },
 
   removeService() {
