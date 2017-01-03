542c95908040fe755239f1a15023c2bc60014c44
Stuart Lang <stuart.b.lang@gmail.com>
2017-01-03 00:26:48 +0000

    add aws-fsharp integration tests

--- lib/plugins/create/templates/aws-fsharp/build.sh
+++ lib/plugins/create/templates/aws-fsharp/build.sh
@@ -3,5 +3,4 @@
 #build handlers
 dotnet restore
 dotnet publish -c release
-
 dotnet lambda package --configuration Release --framework netcoreapp1.0  --output-package bin/release/netcoreapp1.0/deploy-package.zip
