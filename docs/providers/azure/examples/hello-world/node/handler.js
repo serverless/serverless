b3659a45d2537db43336afd08437ad5334dec5d2
Christopher Anderson <christopheranderson@Christophers-MacBook-Pro.local>
2017-02-20 01:03:41 -0800

    Adding initial Azure Docs :tada:

---
+++ docs/providers/azure/examples/hello-world/node/handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Your function handler
+module.exports.hello = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done(null, res);
+};
---
+++ docs/providers/azure/guide/quickstart.md
@@ -0,0 +1,15 @@
+<!--
+title: Serverless Framework - Azure Functions Guide - Quickstart
+menuText: Quickstart
+menuOrder: 2
+description: Get started with Azure Functions in 5 minutes or less
+layout: Doc
+-->
+
+<!-- DOCS-SITE-LINK:START automatically generated  -->
+### [Read this on the main serverless docs site](https://www.serverless.com/framework/docs/providers/azure/guide/intro)
+<!-- DOCS-SITE-LINK:END -->
+
+# Introduction
+
+[!TODO]
