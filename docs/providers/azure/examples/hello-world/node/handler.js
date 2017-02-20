81ff6c80eab1716da5b919247825d4fc2d095eba
Christopher Anderson <christopheranderson@Christophers-MacBook-Pro.local>
2017-02-20 02:10:20 -0800

    Fix eslint issues

--- docs/providers/azure/examples/hello-world/node/handler.js
+++ docs/providers/azure/examples/hello-world/node/handler.js
@@ -1,20 +1,15 @@
 'use strict';
 
-// Your function handler
-module.exports.hello = function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
-    context.done(null, res);
+module.exports.hello = (context, req) => {
+  context.log('JavaScript HTTP trigger function processed a request.');
+  const res = {};
+  if (req.query.name || (req.body && req.body.name)) {
+    // status: 200, /* Defaults to 200 */
+    const name = req.query.name || req.body.name;
+    res.body = `Hello ${name}`;
+  } else {
+    res.status = 400;
+    res.body = 'Please pass a name on the query string or in the request body';
+  }
+  context.done(null, res);
 };
