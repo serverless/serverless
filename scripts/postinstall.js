eb656d6decb92294ac2671b374c3d5419ed4f07e
Eslam A. Hefnawy <eahefnawy@gmail.com>
2017-06-08 02:01:50 +0700

    added sls to autocomplete

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -10,8 +10,10 @@ const serverless = new Serverless();
   serverless.init().then(() => {
     try {
       execSync('./node_modules/tabtab/bin/tabtab install --name serverless --auto');
+      execSync('./node_modules/tabtab/bin/tabtab install --name sls --auto');
     } catch (e) {
       execSync('./node_modules/tabtab/bin/tabtab install --name serverless --stdout');
+      execSync('./node_modules/tabtab/bin/tabtab install --name sls --stdout');
       serverless.cli.consoleLog('Could not auto-install serverless autocomplete script.');
       serverless.cli.consoleLog('Please copy/paste the script above into your shell.');
     }
