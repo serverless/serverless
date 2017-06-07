671049a6c94b08c8fbc4a7a293008ef63f84c82c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2017-06-07 23:23:03 +0700

    postinstalling autocomplete

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,18 @@
 'use strict';
 
+const execSync = require('child_process').execSync;
+
 const Serverless = require('../lib/Serverless');
 
 const serverless = new Serverless();
 
 (() => {
   serverless.init().then(() => {
+    try {
+      execSync('./node_modules/tabtab/bin/tabtab install --name serverless --auto')
+    } catch (e) {
+      console.log('Could not auto-install serverless autocompletion script');
+    }
     serverless.utils.logStat(serverless, 'install').catch(() => Promise.resolve());
   });
 })();
