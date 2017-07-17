7e33670748ef14bea3b7bc522db6eaac7464e28c
Alex DeBrie <alexdebrie1@gmail.com>
2017-07-17 21:32:13 +0000

    Send to new tracking endpoint. Add serviceName in tracking.

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -6,15 +6,12 @@ const path = require('path');
 
 const trackingConfigFilePath = path.join(process.cwd(), getTrackingConfigFileName());
 
-// don't release without tracking keys!
-if (!process.env.SEGMENT_WRITE_KEY) {
-  throw new Error('SEGMENT_WRITE_KEY env var not set');
-} else if (!process.env.SENTRY_DSN) {
+// don't release without Sentry key!
+if (!process.env.SENTRY_DSN) {
   throw new Error('SENTRY_DSN env var not set');
 }
 
 const trackingConfig = {
-  segmentWriteKey: process.env.SEGMENT_WRITE_KEY,
   sentryDSN: process.env.SENTRY_DSN,
 };
 
