5a61f6c75611415ddbfce27cdacb697858160c21
Tommy Brunn <tommy.brunn@klarna.com>
2017-08-05 19:47:46 +0200

    Test python implementation of get_remaining_time_in_millis

--- lib/plugins/aws/invokeLocal/invoke.py
+++ lib/plugins/aws/invokeLocal/invoke.py
@@ -12,7 +12,7 @@ class FakeLambdaContext(object):
         self.timeout = timeout
 
     def get_remaining_time_in_millis(self):
-        return max((self.timeout * 1000) - (time() - self.created), 0)
+        return int(max((self.timeout * 1000) - (int(round(time() * 1000)) - int(round(self.created * 1000))), 0))
 
     @property
     def function_name(self):
