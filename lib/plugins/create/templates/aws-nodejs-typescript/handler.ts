43a2ce2822a34544bc9f19f25431aa44cf6330b4
Philipp Muens <raute1337@gmx.de>
2017-09-14 12:17:00 +0200

    Update Lambda function response

--- lib/plugins/create/templates/aws-nodejs-typescript/handler.ts
+++ lib/plugins/create/templates/aws-nodejs-typescript/handler.ts
@@ -1,3 +1,11 @@
-export const hello = (event, context, cb) => cb(null,
-  { message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!', event }
-);
+export const hello = (event, context, cb) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  cb(null, response);
+}
