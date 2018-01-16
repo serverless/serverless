35ec7251dc6f5cb250973c2164201aa739b1bff4
Stuart Lang <stuart.b.lang@gmail.com>
2018-01-16 00:52:40 +0000

    Uprade aws-csharp to .NET Core 2.0

--- lib/plugins/create/templates/aws-csharp/build.sh
+++ lib/plugins/create/templates/aws-csharp/build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-#install zip
-apt-get -qq update
-apt-get -qq -y install zip
-
-dotnet restore
-
-#create deployment package
-dotnet lambda package --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/deploy-package.zip
+dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
