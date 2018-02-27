67cbabc4c97741b1c1be2c18da63e305f56663e9
Yunspace <yun@yunspace.com>
2018-02-27 22:23:59 +1100

    removed redudant code from docs.

--- lib/plugins/create/templates/aws-csharp/build.cmd
+++ lib/plugins/create/templates/aws-csharp/build.cmd
@@ -1,1 +1,2 @@
+dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
--- lib/plugins/create/templates/aws-csharp/build.sh
+++ lib/plugins/create/templates/aws-csharp/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
