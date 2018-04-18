2adefacbc33f02c6c001b82a13d3c3fc052c2088
Jason Axelson <jason@hobnob.io>
2018-04-17 14:57:34 -1000

    Remove tab completion from postinstall
    Tab completion can later be added back with a command like `serverless
    install completion` or `serverless install completion --remove` but
    those can be created in a separate PR.
    
    Fixes #4069

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -13,29 +13,8 @@ try {
 
   (() => serverless.init()
     .then(() => serverless.utils.logStat(serverless, 'install'))
-    .then(() => setupAutocomplete())
     .catch(() => Promise.resolve())
   )();
 } catch (error) {
   // fail silently
-}
-
-function setupAutocomplete() {
-  return new Promise((resolve, reject) => {
-    const indexRegex = new RegExp(path.join(path.sep, 'index.js'));
-    const tabtabPath = require.resolve('tabtab').replace(indexRegex, '');
-    const tabtabCliPath = path.join(tabtabPath, 'src', 'cli.js');
-
-    try {
-      execSync(`node "${tabtabCliPath}" install --name serverless --auto`);
-      execSync(`node "${tabtabCliPath}" install --name sls --auto`);
-      return resolve();
-    } catch (error) {
-      execSync(`node "${tabtabCliPath}" install --name serverless --stdout`);
-      execSync(`node "${tabtabCliPath}" install --name sls --stdout`);
-      console.log('Could not auto-install serverless autocomplete script.');
-      console.log('Please copy / paste the script above into your shell.');
-      return reject(error);
-    }
-  });
 }
