396febb306e3e0950d9103fed4bf2fad7ddd90b2
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-06-13 16:33:11 +0300

    publish, archive, access keys and login updated

---
+++ test/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
