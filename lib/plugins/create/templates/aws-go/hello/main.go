b48b0581dabcc8db9a5577c39a194d7a712c0731
Sebastian Borza <sebito91@gmail.com>
2018-07-21 09:26:53 -0500

    updating golang templates to match examples

--- lib/plugins/create/templates/aws-go/hello/main.go
+++ lib/plugins/create/templates/aws-go/hello/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// Response is the default struct required to reply to an AWS Lambda request
 type Response struct {
-	Message string `json:"message"`
+	StatusCode      int               `json:"statusCode"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	Body            string            `json:"body"`
+	Headers         map[string]string `json:"headers"`
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context) (Response, error) {
+	var buf bytes.Buffer
+
+	body, err := json.Marshal(map[string]interface{}{
+		"message": "Go Serverless v1.0! Your function executed successfully!",
+	})
+	if err != nil {
+		return Response{StatusCode: 404}, err
+	}
+	json.HTMLEscape(&buf, body)
+
+	resp := Response{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":           "application/json",
+			"X-MyCompany-Func-Reply": "hello-handler",
+		},
+	}
+
+	return resp, nil
 }
 
 func main() {
