52fe216487b5da0f52d0a4464e72f036ec76a896
Daniel Schep <dschep@gmail.com>
2018-11-30 12:47:08 -0500

    Remove sentry dsn check from pre-release script
    we dont actually use this and its blocking release now.
    
    Releases just happened to work in the past because node 4 would do the deploy which doesnt
    run the prerelease script

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -7,9 +7,11 @@ const path = require('path');
 const trackingConfigFilePath = path.join(process.cwd(), getTrackingConfigFileName());
 
 // don't release without Sentry key!
+/*
 if (!process.env.SENTRY_DSN) {
   throw new Error('SENTRY_DSN env var not set');
 }
+*/
 
 const trackingConfig = {
   sentryDSN: process.env.SENTRY_DSN,
