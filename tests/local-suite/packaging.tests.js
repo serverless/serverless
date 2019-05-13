a981d264f2636673ff2b949fdf7e4578e5f18e91
Daniel Schep <dschep@gmail.com>
2019-05-13 15:40:33 -0400

    delint

--- tests/local-suite/packaging.tests.js
+++ tests/local-suite/packaging.tests.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const execSync = require('child_process').execSync;
-const BbPromise = require('bluebird');
 const fse = require('fs-extra');
 const testUtils = require('../utils/index');
 
 const serverlessExec = path.join(__dirname, '..', '..', 'bin', 'serverless');
 
 describe('simple packaging', () => {
-  let cwd
+  let cwd;
   beforeEach(() => {
     cwd = testUtils.getTmpDirPath();
     fse.mkdirsSync(cwd);
-  })
+  });
 
   it('packages the default aws template correctly', () => {
     const templateName = 'aws-nodejs';
@@ -22,98 +21,101 @@ describe('simple packaging', () => {
       .then(zipfiles => {
         expect(zipfiles).toEqual(['handler.js']);
       });
-  })
+  });
 
   it('packages the default aws template with an npm dep correctly', () => {
     const templateName = 'aws-nodejs';
     execSync(`${serverlessExec} create --template ${templateName}`, { cwd });
-    execSync(`npm init --yes`, { cwd });
-    execSync(`npm i lodash`, { cwd });
+    execSync('npm init --yes', { cwd });
+    execSync('npm i lodash', { cwd });
     execSync(`${serverlessExec} package`, { cwd });
     return testUtils.listZipFiles(path.join(cwd, '.serverless/aws-nodejs.zip'))
       .then(zipfiles => {
-        const nodeModules = new Set(zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
+        const nodeModules = new Set(
+          zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
         const nonNodeModulesFiles = zipfiles.filter(f => !f.startsWith('node_modules'));
         expect(nodeModules).toEqual(new Set(['lodash']));
         expect(nonNodeModulesFiles).toEqual(['handler.js', 'package-lock.json', 'package.json']);
       });
-  })
+  });
 
   it('doesn\'t package a dev dependency', () => {
     const templateName = 'aws-nodejs';
     execSync(`${serverlessExec} create --template ${templateName}`, { cwd });
-    execSync(`npm init --yes`, { cwd });
-    execSync(`npm i --save-dev lodash`, { cwd });
+    execSync('npm init --yes', { cwd });
+    execSync('npm i --save-dev lodash', { cwd });
     execSync(`${serverlessExec} package`, { cwd });
     return testUtils.listZipFiles(path.join(cwd, '.serverless/aws-nodejs.zip'))
       .then(zipfiles => {
-        const nodeModules = new Set(zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
+        const nodeModules = new Set(
+          zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
         const nonNodeModulesFiles = zipfiles.filter(f => !f.startsWith('node_modules'));
         expect(nodeModules).toEqual(new Set([]));
         expect(nonNodeModulesFiles).toEqual(['handler.js', 'package-lock.json', 'package.json']);
       });
-  })
+  });
 
   it('ignores package json files per ignore directive', () => {
     const templateName = 'aws-nodejs';
     execSync(`${serverlessExec} create --template ${templateName}`, { cwd });
-    execSync(`npm init --yes`, { cwd });
-    execSync(`echo 'package: {exclude: ["package*.json"]}' >> serverless.yml`, { cwd });
-    execSync(`npm i lodash`, { cwd });
+    execSync('npm init --yes', { cwd });
+    execSync('echo \'package: {exclude: ["package*.json"]}\' >> serverless.yml', { cwd });
+    execSync('npm i lodash', { cwd });
     execSync(`${serverlessExec} package`, { cwd });
     return testUtils.listZipFiles(path.join(cwd, '.serverless/aws-nodejs.zip'))
       .then(zipfiles => {
-        const nodeModules = new Set(zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
+        const nodeModules = new Set(i
+          zipfiles.filter(f => f.startsWith('node_modules')).map(f => f.split(path.sep)[1]));
         const nonNodeModulesFiles = zipfiles.filter(f => !f.startsWith('node_modules'));
         expect(nodeModules).toEqual(new Set(['lodash']));
         expect(nonNodeModulesFiles).toEqual(['handler.js']);
       });
-  })
-})
+  });
+});
 
 describe('cloudformation packaging', () => {
-  let cwd
+  let cwd;
   beforeEach(() => {
     cwd = testUtils.getTmpDirPath();
     fse.mkdirsSync(cwd);
-  })
+  });
 
   it('creates the correct default function resource', () => {
     const templateName = 'aws-nodejs';
     execSync(`${serverlessExec} create --template ${templateName}`, { cwd });
     execSync(`${serverlessExec} package`, { cwd });
     const cfnTemplate = JSON.parse(fs.readFileSync(path.join(
-      cwd, '.serverless/cloudformation-template-update-stack.json')))
-      expect(cfnTemplate.Resources.HelloLambdaFunction.Properties.Code.S3Key).toMatch(/serverless\/aws-nodejs\/dev\/[^]*\/aws-nodejs.zip/)
-      delete cfnTemplate.Resources.HelloLambdaFunction.Properties.Code.S3Key;
-      expect(cfnTemplate.Resources.HelloLambdaFunction).toEqual({
-        "Type": "AWS::Lambda::Function",
-        "Properties": {
-          "Code": {
-            "S3Bucket": {
-              "Ref": "ServerlessDeploymentBucket"
-            }
+      cwd, '.serverless/cloudformation-template-update-stack.json')));
+    expect(cfnTemplate.Resources.HelloLambdaFunction.Properties.Code.S3Key).toMatch(/serverless\/aws-nodejs\/dev\/[^]*\/aws-nodejs.zip/);
+    delete cfnTemplate.Resources.HelloLambdaFunction.Properties.Code.S3Key;
+    expect(cfnTemplate.Resources.HelloLambdaFunction).toEqual({
+      Type: 'AWS::Lambda::Function',
+      Properties: {
+        Code: {
+          S3Bucket: {
+            Ref: 'ServerlessDeploymentBucket',
           },
-          "FunctionName": "aws-nodejs-dev-hello",
-          "Handler": "handler.hello",
-          "MemorySize": 1024,
-          "Role": {
-            "Fn::GetAtt": [
-              "IamRoleLambdaExecution",
-              "Arn"
-            ]
-          },
-          "Runtime": "nodejs8.10",
-          "Timeout": 6
         },
-        "DependsOn": [
-          "HelloLogGroup",
-          "IamRoleLambdaExecution"
-        ]
-      })
+        FunctionName: 'aws-nodejs-dev-hello',
+        Handler: 'handler.hello',
+        MemorySize: 1024,
+        Role: {
+          'Fn::GetAtt': [
+            'IamRoleLambdaExecution',
+            'Arn',
+          ],
+        },
+        Runtime: 'nodejs8.10',
+        Timeout: 6,
+      },
+      DependsOn: [
+        'HelloLogGroup',
+        'IamRoleLambdaExecution',
+      ],
+    });
     return testUtils.listZipFiles(path.join(cwd, '.serverless/aws-nodejs.zip'))
       .then(zipfiles => {
         expect(zipfiles).toEqual(['handler.js']);
       });
-  })
-})
+  });
+});
