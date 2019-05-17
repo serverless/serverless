fb896887f5b8668335103467c2017679dd63e3a8
Mariusz Nowak <medyk@medikoo.com>
2019-05-17 14:56:45 +0200

    Move all mocha patches into mocha-reporter

--- tests/mocha-reporter.js
+++ tests/mocha-reporter.js
@@ -1,5 +1,21 @@
 'use strict';
 
+// Unahdled rejections are not exposed in mocha, we need to force it
+// https://github.com/mochajs/mocha/issues/2640
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+// Workaround Mocha v5 issue: https://github.com/mochajs/mocha/issues/3226
+// Fixed in v6, but not really: https://github.com/mochajs/mocha/issues/3917
+process.on('uncaughtException', err => {
+  if (!process.listenerCount('exit')) return;
+  // Mocha done it's report, and registered process.exit listener which silences any further
+  // eventual crashes. Recover by unregistering the listener
+  process.removeAllListeners('exit');
+  throw err;
+});
+
 const Spec = require('mocha/lib/reporters/spec');
 const Runner = require('mocha/lib/runner');
 
@@ -9,4 +25,3 @@ const Runner = require('mocha/lib/runner');
 Runner.immediately = process.nextTick;
 
 module.exports = Spec;
-
