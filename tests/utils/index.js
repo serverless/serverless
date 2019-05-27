262905768294f29517c76e41cf00600099db0a4e
Mariusz Nowak <mariusz@medikoo.com>
2019-05-27 13:20:52 +0200

    execSync test util

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -240,11 +240,11 @@ module.exports = {
   execSync(command, options = null) {
     // Same as native but outputs std in case of error
     try {
-      execSync(command, options);
+      return execSync(command, options);
     } catch (error) {
       if (error.stdout) process.stdout.write(error.stdout);
       if (error.stderr) process.stderr.write(error.stderr);
       throw error;
     }
-  }
+  },
 };
