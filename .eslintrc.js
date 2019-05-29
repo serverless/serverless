d8288d057c12d0fd595b074a1b16ccc0ccc2f60d
Mariusz Nowak <mariusz@medikoo.com>
2019-05-29 13:34:27 +0200

    Fix eslint configuration of 'strict' option

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,9 @@ module.exports = {
   "rules": {
     "func-names": "off",
     "global-require": "off", // Interfers with optional and eventual circular references
+    "strict": ["error", "safe"],
 
     // doesn't work in node v4 :(
-    "strict": "off",
     "prefer-rest-params": "off",
     "react/require-extension": "off",
     "import/no-extraneous-dependencies": "off"
