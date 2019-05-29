8182e9e0c848f8c06013f9c4cae0f2121dd53da0
Mariusz Nowak <mariusz@medikoo.com>
2019-05-29 13:58:24 +0200

    Configure "import/no-extraneous-dependencies" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,9 @@ module.exports = {
   "rules": {
     "func-names": "off",
     "global-require": "off", // Interfers with optional and eventual circular references
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.js",  "**/scripts/**", "**/tests/**"]}],
     "react/require-extension": "off", // Forced by airbnb, not applicable (also deprecated)
     "strict": ["error", "safe"], // airbnb implies we're transpiling with babel, we're not
-
-    // doesn't work in node v4 :(
-    "import/no-extraneous-dependencies": "off"
   },
   "parserOptions": {
     "sourceType": "script", // airbnb assumes ESM, while we're CJS
