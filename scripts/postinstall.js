dab6cc62d9d768426fd9937764de755dc52ef776
Neil Locketz <neil@nlocketz.com>
2019-07-05 15:35:40 -0400

    Tell user what to do to install auto complete themselves.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,8 +5,6 @@ const chalk = require('chalk');
 
 /* eslint-disable no-console */
 
-const execSync = require('child_process').execSync;
-
 const truthyStr = val => val && !['0', 'false', 'f', 'n', 'no'].includes(val.toLowerCase());
 const { CI, ADBLOCK, SILENT } = process.env;
 if (!truthyStr(CI) && !truthyStr(ADBLOCK) && !truthyStr(SILENT)) {
@@ -37,23 +35,14 @@ try {
 }
 
 function setupAutocomplete() {
-  return new Promise((resolve, reject) => {
-    const indexRegex = new RegExp(path.join(path.sep, 'index.js'));
-    const tabtabPath = require.resolve('tabtab').replace(indexRegex, '');
-    const tabtabCliPath = path.join(tabtabPath, 'src', 'cli.js');
-
-    try {
-      execSync(`node "${tabtabCliPath}" install --name serverless --auto`);
-      execSync(`node "${tabtabCliPath}" install --name sls --auto`);
-      execSync(`node "${tabtabCliPath}" install --name slss --auto`);
-      return resolve();
-    } catch (error) {
-      execSync(`node "${tabtabCliPath}" install --name serverless --stdout`);
-      execSync(`node "${tabtabCliPath}" install --name sls --stdout`);
-      execSync(`node "${tabtabCliPath}" install --name slss --stdout`);
-      console.log('Could not auto-install serverless autocomplete script.');
-      console.log('Please copy / paste the script above into your shell.');
-      return reject(error);
-    }
-  });
+  const indexRegex = new RegExp(path.join(path.sep, 'index.js'));
+  const tabtabPath = require.resolve('tabtab').replace(indexRegex, '');
+  const tabtabCliPath = path.join(tabtabPath, 'src', 'cli.js');
+  console.log(
+    "If you'd like autocomplete for serverless, you can run the following commands to add it:\n"
+  );
+  console.log(`\tnode "${tabtabCliPath}" install --name serverless --auto`);
+  console.log(`\tnode "${tabtabCliPath}" install --name sls --auto`);
+  console.log(`\tnode "${tabtabCliPath}" install --name slss --auto`);
+  console.log();
 }
