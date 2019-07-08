49f6e1e8a57929862c79b6fea90c7515469bca7c
Philipp Muens <raute1337@gmx.de>
2019-07-08 14:43:32 +0200

    feat: Configure binaries generation

---
+++ scripts/pkg/config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  // Open license ensures JS files are accessible as is
+  // which is important for local templates installation
+  license: 'MIT',
+  files: [
+    // Custom resources
+    '../../lib/plugins/aws/customResources/resources',
+    // Basic CF templates
+    '../../lib/plugins/aws/package/lib/*.json',
+    // Service templates
+    '../../lib/plugins/create/templates',
+    // Ensure npm is bundled as a dependency
+    '../../node_modules/npm/bin/npm-cli.js',
+    // Below module is not automatically traced by pkg, we need to point it manually
+    // See: https://github.com/npm/npm-lifecycle/pull/41
+    '../../node_modules/node-gyp/bin/node-gyp.js',
+    '../../node_modules/npm/node_modules/node-gyp/bin/node-gyp.js',
+    '../../node_modules/npm/node_modules/npm-lifecycle/node_modules/node-gyp/bin/node-gyp.js',
+  ],
+};
