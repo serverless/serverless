5140282f5b8d997181147bbfcc5681c8752cfb57
Mariusz Nowak <mariusz@medikoo.com>
2019-08-20 13:56:31 +0200

    Fix websockets service setup

--- tests/integration-all/api-gateway-websocket/service/core.js
+++ tests/integration-all/api-gateway-websocket/service/core.js
@@ -1,13 +1,9 @@
 'use strict';
 
-function minimal(event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from API Gateway! - (websocket)',
-      event,
-    }),
-  };
+function minimal(event, context, callback) {
+  console.info('Event type', event.requestContext.eventType);
+  if (event.body) console.info('Event body', event.body);
+  return callback(null, { statusCode: 200 });
 }
 
 module.exports = {
