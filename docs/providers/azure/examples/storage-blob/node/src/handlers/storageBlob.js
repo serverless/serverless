d988bfa61917759392ec3882c6a1c43a51515814
My Ho <mydiemho@gmail.com>
2019-09-03 17:21:38 -0700

    add new azure nodejs trigger samples
    - [x] storage blob
    - [x] storage queue
    - [x] service bus queue
    - [x] service bus topic
    - [x] event hub
    - [x] timer

---
+++ docs/providers/azure/examples/storage-blob/node/src/handlers/storageBlob.js
@@ -0,0 +1,14 @@
+module.exports.printMessage = async function (context, blob) {
+  const content = JSON.parse(blob);
+
+  context.log('================= MESSAGE START =================');
+  context.log('Blob name: ', context.bindingData.blobName);
+  context.log('Blob length: ', blob.length, ' bytes');
+
+  // NOTE: expecting blob to be a json file
+  context.log('Blob content:', JSON.stringify(content, null, 2));
+
+  context.log('================= MESSAGE END =================');
+
+  context.done();
+};
