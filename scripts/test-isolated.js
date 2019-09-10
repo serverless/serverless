a5b3f1b80f7307e12426ad92a35df3111f4640cb
Mariusz Nowak <mariusz@medikoo.com>
2019-09-10 15:24:29 +0200

    Recognize SERVERLESS_ACCESS_KEY as creds env var

--- scripts/test-isolated.js
+++ scripts/test-isolated.js
@@ -118,6 +118,9 @@ const run = path => {
     for (const envVarName of Object.keys(process.env)) {
       if (envVarName.startsWith('AWS_')) env[envVarName] = process.env[envVarName];
     }
+    if (process.env.SERVERLESS_ACCESS_KEY) {
+      env.SERVERLESS_ACCESS_KEY = process.env.SERVERLESS_ACCESS_KEY;
+    }
   }
   return spawn('npx', ['mocha', path], {
     stdio: isMultiProcessRun ? null : 'inherit',
