f6dafb4e9aba780a6614a67ffe098f982182a7e0
Daniel Schep <dschep@gmail.com>
2019-09-24 14:22:05 -0400

    Automate keeping the sfe-next branch upto date

---
+++ scripts/update-sfe-dep.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const pkgJson = JSON.parse(fs.readFileSync('package.json'));
+
+pkgJson.dependencies['@serverless/enterprise-plugin'] = 'next';
+
+fs.writeFileSync('package.json', JSON.stringify(pkgJson, null, 2));
