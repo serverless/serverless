f67ca3901d73ac7235e565f44d0ff6cb2fdb619c
Mariusz Nowak <mariusz@medikoo.com>
2019-09-27 12:00:17 +0200

    Clear eslint workaround

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,12 +2,10 @@
 
 const chalk = require('chalk');
 
-/* eslint-disable no-console */
-
 const truthyStr = val => val && !['0', 'false', 'f', 'n', 'no'].includes(val.toLowerCase());
 const { CI, ADBLOCK, SILENT } = process.env;
 if (!truthyStr(CI) && !truthyStr(ADBLOCK) && !truthyStr(SILENT)) {
-  console.log(
+  process.stdout.write(
     chalk.yellow(`\
  +--------------------------------------------------+
  |                                                  |
@@ -15,6 +13,7 @@ if (!truthyStr(CI) && !truthyStr(ADBLOCK) && !truthyStr(SILENT)) {
  |  To start your first project, run “serverless”.  |
  |                                                  |
  +--------------------------------------------------+
+
 `)
   );
 }
