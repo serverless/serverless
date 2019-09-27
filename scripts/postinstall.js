289eba79e09a6c34c3610c2ad678456bed46982d
Mariusz Nowak <mariusz@medikoo.com>
2019-09-27 11:59:49 +0200

    Simplify logic

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -23,11 +23,10 @@ try {
   const Serverless = require('../lib/Serverless');
   const serverless = new Serverless();
 
-  (() =>
-    serverless
-      .init()
-      .then(() => serverless.utils.logStat(serverless, 'install'))
-      .catch(() => Promise.resolve()))();
+  serverless
+    .init()
+    .then(() => serverless.utils.logStat(serverless, 'install'))
+    .catch(() => {});
 } catch (error) {
   // fail silently
 }
