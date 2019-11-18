86e21846c818ed4fd3716159245157019ebf0a01
Mariusz Nowak <mariusz@medikoo.com>
2019-11-18 13:45:09 +0100

    feat(Standalone): Windows Chocolatey PM integration

---
+++ scripts/pkg/choco-package-template/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$fileLocation = Join-Path $toolsDir 'serverless.exe'
+
+Install-BinFile -Name serverless -Path $fileLocation
+Install-BinFile -Name sls -Path $fileLocation
+
+& 'serverless' 'binary-postinstall'
