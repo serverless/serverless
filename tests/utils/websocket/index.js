0970cf9c9c595f4f6765811f8717f8795333d2d8
Mariusz Nowak <mariusz@medikoo.com>
2019-12-19 12:38:13 +0100

    test: Replace `persistentRequest` usage with `awsRequest`

--- tests/utils/websocket/index.js
+++ tests/utils/websocket/index.js
@@ -1,58 +1,45 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-const { region, persistentRequest } = require('../misc');
+const { awsRequest } = require('../misc');
 
 function createApi(name) {
-  const APIG = new AWS.ApiGatewayV2({ region });
-
-  return APIG.createApi({
+  return awsRequest('ApiGatewayV2', 'createApi', {
     Name: name,
     ProtocolType: 'WEBSOCKET',
     RouteSelectionExpression: '$request.body.action',
-  }).promise();
+  });
 }
 
 function createStage(apiId, stageName) {
-  const APIG = new AWS.ApiGatewayV2({ region });
-
   const params = {
     ApiId: apiId,
     StageName: stageName,
   };
-  return APIG.createStage(params).promise();
+  return awsRequest('ApiGatewayV2', 'createStage', params);
 }
 
 function deleteApi(id) {
-  const APIG = new AWS.ApiGatewayV2({ region });
-
-  return APIG.deleteApi({
+  return awsRequest('ApiGatewayV2', 'deleteApi', {
     ApiId: id,
-  }).promise();
+  });
 }
 
 function deleteStage(apiId, stageName) {
-  const APIG = new AWS.ApiGatewayV2({ region });
-
   const params = {
     ApiId: apiId,
     StageName: stageName,
   };
-  return APIG.deleteStage(params).promise();
+  return awsRequest('ApiGatewayV2', 'deleteStage', params);
 }
 
 function getRoutes(apiId) {
-  const APIG = new AWS.ApiGatewayV2({ region });
-  APIG.getRoutes;
-  return APIG.getRoutes({ ApiId: apiId })
-    .promise()
-    .then(data => data.Items);
+  return awsRequest('ApiGatewayV2', 'getRoutes', { ApiId: apiId }).then(data => data.Items);
 }
 
 module.exports = {
-  createApi: persistentRequest.bind(this, createApi),
-  deleteApi: persistentRequest.bind(this, deleteApi),
-  getRoutes: persistentRequest.bind(this, getRoutes),
-  createStage: persistentRequest.bind(this, createStage),
-  deleteStage: persistentRequest.bind(this, deleteStage),
+  createApi,
+  deleteApi,
+  getRoutes,
+  createStage,
+  deleteStage,
 };
