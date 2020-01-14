serverless/serverless

commit e662a91d92651111c86b6e72eed57075be95decb

fix(AWS APIGW): Fix handling of removal of `resourcePolicy` setting
Fixes #6789

lib/plugins/aws/package/compile/events/apiGateway/lib/restApi.js
@@ -3,6 +3,23 @@
 const _ = require('lodash');
 const BbPromise = require('bluebird');
 
+const defaultPolicy = {
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Effect: 'Allow',
+      Principal: '*',
+      Action: 'execute-api:Invoke',
+      Resource: ['execute-api:/*/*/*'],
+      Condition: {
+        IpAddress: {
+          'aws:SourceIp': ['0.0.0.0/0', '::/0'],
+        },
+      },
+    },
+  ],
+};
+
 module.exports = {
   compileRestApi() {
     const apiGateway = this.serverless.service.provider.apiGateway || {};
@@ -61,6 +78,17 @@ module.exports = {
         ].Properties,
         {
           Policy: policy,
+        }
+      );
+    } else {
+      // setting up a policy with no restrictions in cases where no policy is specified
+      // this ensures that a policy is always present
+      _.merge(
+        this.serverless.service.provider.compiledCloudFormationTemplate.Resources[
+          this.apiGatewayRestApiLogicalId
+        ].Properties,
+        {
+          Policy: defaultPolicy,
         }
       );
     }

lib/plugins/aws/package/compile/events/apiGateway/lib/restApi.test.js
@@ -49,6 +49,22 @@ describe('#compileRestApi()', () => {
           EndpointConfiguration: {
             Types: ['EDGE'],
           },
+          Policy: {
+            Statement: [
+              {
+                Action: 'execute-api:Invoke',
+                Condition: {
+                  IpAddress: {
+                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
+                  },
+                },
+                Effect: 'Allow',
+                Principal: '*',
+                Resource: ['execute-api:/*/*/*'],
+              },
+            ],
+            Version: '2012-10-17',
+          },
         },
       });
     }));
@@ -100,6 +116,40 @@ describe('#compileRestApi()', () => {
     });
   });
 
+  it('should provide open policy if no policy specified', () => {
+    const resources =
+      awsCompileApigEvents.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+
+    return awsCompileApigEvents.compileRestApi().then(() => {
+      expect(resources.ApiGatewayRestApi).to.deep.equal({
+        Type: 'AWS::ApiGateway::RestApi',
+        Properties: {
+          Name: 'dev-new-service',
+          BinaryMediaTypes: undefined,
+          EndpointConfiguration: {
+            Types: ['EDGE'],
+          },
+          Policy: {
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Effect: 'Allow',
+                Principal: '*',
+                Action: 'execute-api:Invoke',
+                Resource: ['execute-api:/*/*/*'],
+                Condition: {
+                  IpAddress: {
+                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+    });
+  });
+
   it('should ignore REST API resource creation if there is predefined restApi config', () => {
     awsCompileApigEvents.serverless.service.provider.apiGateway = {
       restApiId: '6fyzt1pfpk',
@@ -128,6 +178,22 @@ describe('#compileRestApi()', () => {
             Types: ['EDGE'],
           },
           Name: 'dev-new-service',
+          Policy: {
+            Statement: [
+              {
+                Action: 'execute-api:Invoke',
+                Condition: {
+                  IpAddress: {
+                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
+                  },
+                },
+                Effect: 'Allow',
+                Principal: '*',
+                Resource: ['execute-api:/*/*/*'],
+              },
+            ],
+            Version: '2012-10-17',
+          },
         },
       });
     });

commit 63e21a2bd891073fdb1f5ff6e2c10244efbd3b9d

fix(AWS APIGW):  Fix default resource policy configuration
Regression introduced with #7138
Fixes #7194 #7211

lib/plugins/aws/package/compile/events/apiGateway/lib/restApi.js
@@ -3,23 +3,6 @@
 const _ = require('lodash');
 const BbPromise = require('bluebird');
 
-const defaultPolicy = {
-  Version: '2012-10-17',
-  Statement: [
-    {
-      Effect: 'Allow',
-      Principal: '*',
-      Action: 'execute-api:Invoke',
-      Resource: ['execute-api:/*/*/*'],
-      Condition: {
-        IpAddress: {
-          'aws:SourceIp': ['0.0.0.0/0', '::/0'],
-        },
-      },
-    },
-  ],
-};
-
 module.exports = {
   compileRestApi() {
     const apiGateway = this.serverless.service.provider.apiGateway || {};
@@ -87,9 +70,7 @@ module.exports = {
         this.serverless.service.provider.compiledCloudFormationTemplate.Resources[
           this.apiGatewayRestApiLogicalId
         ].Properties,
-        {
-          Policy: defaultPolicy,
-        }
+        { Policy: '' }
       );
     }
 

lib/plugins/aws/package/compile/events/apiGateway/lib/restApi.test.js
@@ -49,22 +49,7 @@ describe('#compileRestApi()', () => {
           EndpointConfiguration: {
             Types: ['EDGE'],
           },
-          Policy: {
-            Statement: [
-              {
-                Action: 'execute-api:Invoke',
-                Condition: {
-                  IpAddress: {
-                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
-                  },
-                },
-                Effect: 'Allow',
-                Principal: '*',
-                Resource: ['execute-api:/*/*/*'],
-              },
-            ],
-            Version: '2012-10-17',
-          },
+          Policy: '',
         },
       });
     }));
@@ -129,22 +114,7 @@ describe('#compileRestApi()', () => {
           EndpointConfiguration: {
             Types: ['EDGE'],
           },
-          Policy: {
-            Version: '2012-10-17',
-            Statement: [
-              {
-                Effect: 'Allow',
-                Principal: '*',
-                Action: 'execute-api:Invoke',
-                Resource: ['execute-api:/*/*/*'],
-                Condition: {
-                  IpAddress: {
-                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
-                  },
-                },
-              },
-            ],
-          },
+          Policy: '',
         },
       });
     });
@@ -178,22 +148,7 @@ describe('#compileRestApi()', () => {
             Types: ['EDGE'],
           },
           Name: 'dev-new-service',
-          Policy: {
-            Statement: [
-              {
-                Action: 'execute-api:Invoke',
-                Condition: {
-                  IpAddress: {
-                    'aws:SourceIp': ['0.0.0.0/0', '::/0'],
-                  },
-                },
-                Effect: 'Allow',
-                Principal: '*',
-                Resource: ['execute-api:/*/*/*'],
-              },
-            ],
-            Version: '2012-10-17',
-          },
+          Policy: '',
         },
       });
     });