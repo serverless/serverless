e0d28b33ca3b9cf263ec7576f857f9122afd8bce
Mariusz Nowak <mariusz@medikoo.com>
2020-02-28 11:55:26 +1300

    chore: Do not restrict lines length in body

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,9 +3,7 @@
 module.exports = {
   rules: {
     'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [2, 'always', 72],
     'footer-leading-blank': [2, 'always'],
-    'footer-max-line-length': [2, 'always', 72],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'start-case'],
     'scope-enum': [
