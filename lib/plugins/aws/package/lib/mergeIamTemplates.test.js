serverless/serverless

commit 9a0aaa843b19cb5bc0ddfe9a25b96e3c64d82749

fix: Ensure consistency in IAM role and policy names (#7357)

lib/plugins/aws/lib/naming.js
@@ -89,15 +89,7 @@ module.exports = {
   },
   getRoleName() {
     return {
-      'Fn::Join': [
-        '-',
-        [
-          this.provider.serverless.service.service,
-          this.provider.getStage(),
-          { Ref: 'AWS::Region' },
-          'lambdaRole',
-        ],
-      ],
+      'Fn::Join': ['-', [this.getStackName(), { Ref: 'AWS::Region' }, 'lambdaRole']],
     };
   },
   getRoleLogicalId() {
@@ -107,16 +99,7 @@ module.exports = {
   // Policy
   getPolicyName() {
     return {
-      // TODO should probably have name ordered and altered as above - see AWS docs
-      'Fn::Join': [
-        '-',
-        [
-          this.provider.getStage(),
-          // TODO is it a bug here in that the role is not specific to the region?
-          this.provider.serverless.service.service,
-          'lambda',
-        ],
-      ],
+      'Fn::Join': ['-', [this.getStackName(), 'lambda']],
     };
   },
 

lib/plugins/aws/lib/naming.test.js
@@ -111,15 +111,7 @@ describe('#naming()', () => {
     it('uses the service name, stage, and region to generate a role name', () => {
       serverless.service.service = 'myService';
       expect(sdk.naming.getRoleName()).to.eql({
-        'Fn::Join': [
-          '-',
-          [
-            serverless.service.service,
-            sdk.naming.provider.getStage(),
-            { Ref: 'AWS::Region' },
-            'lambdaRole',
-          ],
-        ],
+        'Fn::Join': ['-', [sdk.naming.getStackName(), { Ref: 'AWS::Region' }, 'lambdaRole']],
       });
     });
   });
@@ -134,7 +126,7 @@ describe('#naming()', () => {
     it('should use the stage and service name', () => {
       serverless.service.service = 'myService';
       expect(sdk.naming.getPolicyName()).to.eql({
-        'Fn::Join': ['-', [sdk.naming.provider.getStage(), serverless.service.service, 'lambda']],
+        'Fn::Join': ['-', [sdk.naming.getStackName(), 'lambda']],
       });
     });
   });

lib/plugins/aws/package/lib/mergeIamTemplates.test.js
@@ -76,10 +76,7 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': [
-                  '-',
-                  [awsPackage.provider.getStage(), awsPackage.serverless.service.service, 'lambda'],
-                ],
+                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -114,8 +111,7 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.serverless.service.service,
-                awsPackage.provider.getStage(),
+                awsPackage.provider.naming.getStackName(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -162,10 +158,7 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': [
-                  '-',
-                  [awsPackage.provider.getStage(), awsPackage.serverless.service.service, 'lambda'],
-                ],
+                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -200,8 +193,7 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.serverless.service.service,
-                awsPackage.provider.getStage(),
+                awsPackage.provider.naming.getStackName(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -251,10 +243,7 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': [
-                  '-',
-                  [awsPackage.provider.getStage(), awsPackage.serverless.service.service, 'lambda'],
-                ],
+                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -289,8 +278,7 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.serverless.service.service,
-                awsPackage.provider.getStage(),
+                awsPackage.provider.naming.getStackName(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -346,10 +334,7 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': [
-                  '-',
-                  [awsPackage.provider.getStage(), awsPackage.serverless.service.service, 'lambda'],
-                ],
+                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -394,8 +379,7 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.serverless.service.service,
-                awsPackage.provider.getStage(),
+                awsPackage.provider.naming.getStackName(),
                 {
                   Ref: 'AWS::Region',
                 },

commit 6c7c35651daed89ad1c3d6084098c02dc634eef1

fix(AWS IAM): Fix role and policy name resolution
Custom stack name should be used only for CloudFormation stack.

Regression introduced with #7357

lib/plugins/aws/lib/naming.js
@@ -89,7 +89,15 @@ module.exports = {
   },
   getRoleName() {
     return {
-      'Fn::Join': ['-', [this.getStackName(), { Ref: 'AWS::Region' }, 'lambdaRole']],
+      'Fn::Join': [
+        '-',
+        [
+          this.provider.serverless.service.service,
+          this.provider.getStage(),
+          { Ref: 'AWS::Region' },
+          'lambdaRole',
+        ],
+      ],
     };
   },
   getRoleLogicalId() {
@@ -99,7 +107,10 @@ module.exports = {
   // Policy
   getPolicyName() {
     return {
-      'Fn::Join': ['-', [this.getStackName(), 'lambda']],
+      'Fn::Join': [
+        '-',
+        [this.provider.serverless.service.service, this.provider.getStage(), 'lambda'],
+      ],
     };
   },
 

lib/plugins/aws/lib/naming.test.js
@@ -111,7 +111,15 @@ describe('#naming()', () => {
     it('uses the service name, stage, and region to generate a role name', () => {
       serverless.service.service = 'myService';
       expect(sdk.naming.getRoleName()).to.eql({
-        'Fn::Join': ['-', [sdk.naming.getStackName(), { Ref: 'AWS::Region' }, 'lambdaRole']],
+        'Fn::Join': [
+          '-',
+          [
+            serverless.service.service,
+            sdk.naming.provider.getStage(),
+            { Ref: 'AWS::Region' },
+            'lambdaRole',
+          ],
+        ],
       });
     });
   });
@@ -126,7 +134,7 @@ describe('#naming()', () => {
     it('should use the stage and service name', () => {
       serverless.service.service = 'myService';
       expect(sdk.naming.getPolicyName()).to.eql({
-        'Fn::Join': ['-', [sdk.naming.getStackName(), 'lambda']],
+        'Fn::Join': ['-', [serverless.service.service, sdk.naming.provider.getStage(), 'lambda']],
       });
     });
   });

lib/plugins/aws/package/lib/mergeIamTemplates.test.js
@@ -76,7 +76,10 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
+                'Fn::Join': [
+                  '-',
+                  [awsPackage.serverless.service.service, awsPackage.provider.getStage(), 'lambda'],
+                ],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -111,7 +114,8 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.provider.naming.getStackName(),
+                awsPackage.serverless.service.service,
+                awsPackage.provider.getStage(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -158,7 +162,10 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
+                'Fn::Join': [
+                  '-',
+                  [awsPackage.serverless.service.service, awsPackage.provider.getStage(), 'lambda'],
+                ],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -193,7 +200,8 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.provider.naming.getStackName(),
+                awsPackage.serverless.service.service,
+                awsPackage.provider.getStage(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -243,7 +251,10 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
+                'Fn::Join': [
+                  '-',
+                  [awsPackage.serverless.service.service, awsPackage.provider.getStage(), 'lambda'],
+                ],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -278,7 +289,8 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.provider.naming.getStackName(),
+                awsPackage.serverless.service.service,
+                awsPackage.provider.getStage(),
                 {
                   Ref: 'AWS::Region',
                 },
@@ -334,7 +346,10 @@ describe('#mergeIamTemplates()', () => {
           Policies: [
             {
               PolicyName: {
-                'Fn::Join': ['-', [awsPackage.provider.naming.getStackName(), 'lambda']],
+                'Fn::Join': [
+                  '-',
+                  [awsPackage.serverless.service.service, awsPackage.provider.getStage(), 'lambda'],
+                ],
               },
               PolicyDocument: {
                 Version: '2012-10-17',
@@ -379,7 +394,8 @@ describe('#mergeIamTemplates()', () => {
             'Fn::Join': [
               '-',
               [
-                awsPackage.provider.naming.getStackName(),
+                awsPackage.serverless.service.service,
+                awsPackage.provider.getStage(),
                 {
                   Ref: 'AWS::Region',
                 },