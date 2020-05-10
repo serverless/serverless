bab0778a85e2c3ef6e9b676fcc7bee9181cb6c1c
Joseph Woodward <joseph.woodward@xeuse.com>
2020-05-10 01:54:09 +0100

    Remove constructor

--- lib/plugins/create/templates/aws-csharp/Handler.cs
+++ lib/plugins/create/templates/aws-csharp/Handler.cs
@@ -27,11 +27,5 @@ namespace AwsDotnetCsharp
       public string Key1 {get; set;}
       public string Key2 {get; set;}
       public string Key3 {get; set;}
-
-      public Request(string key1, string key2, string key3){
-        Key1 = key1;
-        Key2 = key2;
-        Key3 = key3;
-      }
     }
 }
