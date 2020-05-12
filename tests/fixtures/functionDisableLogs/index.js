f698d0ec5a4d55a680d5d551153f827147a3a3d7
Ahmed Abdelwahab <ahmed.abdelwahab@prophecylabs.com>
2020-05-12 10:12:21 +0200

    Exclude functions with disableLogs from log group creation
    Change disableLogs flag tests to use runServerless
    
    Ignore test fixtures .serverless dir
    
    Modify docs to include disableLogs flag

---
+++ tests/fixtures/functionDisableLogs/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'disableLogs?', input: event }, null, 2),
+  });
+};
