serverless/serverless

commit 4c6f8be5ccae88034e19f72a53996208dd4a56d5

refactor: Replace `_.map` with `array.map` (#7827)

lib/plugins/aws/package/compile/events/apiGateway/lib/apiKeys.js
@@ -51,7 +51,7 @@ module.exports = {
         if (
           _.isObject(apiKeyDefinition) &&
           _.flatten(
-            _.map(this.serverless.service.provider.usagePlan, item => Object.keys(item))
+            (this.serverless.service.provider.usagePlan || []).map(item => Object.keys(item))
           ).includes(name)
         ) {
           keyNumber = 0;

lib/classes/PluginManager.js
@@ -250,7 +250,7 @@ class PluginManager {
     _.forEach(pluginInstance.commands, (details, key) => {
       const command = this.loadCommand(pluginName, details, key);
       // Grab and extract deprecated events
-      command.lifecycleEvents = _.map(command.lifecycleEvents, event => {
+      command.lifecycleEvents = (command.lifecycleEvents || []).map(event => {
         if (event.startsWith('deprecated#')) {
           // Extract event and optional redirect
           const transformedEvent = /^deprecated#(.*?)(?:->(.*?))?$/.exec(event);

lib/classes/Variables.js
@@ -261,7 +261,7 @@ class Variables {
     const addContext = (value, key) =>
       this.getProperties(root, false, value, context.concat(key), results);
     if (Array.isArray(current)) {
-      _.map(current, addContext);
+      current.map(addContext);
     } else if (
       _.isObject(current) &&
       !_.isDate(current) &&
@@ -291,7 +291,7 @@ class Variables {
     const variables = properties.filter(
       property => typeof property.value === 'string' && property.value.match(this.variableSyntax)
     );
-    return _.map(variables, variable =>
+    return variables.map(variable =>
       this.populateValue(variable.value, false).then(populated =>
         Object.assign({}, variable, { populated })
       )
@@ -367,7 +367,7 @@ class Variables {
     if (!matches || !matches.length) {
       return property;
     }
-    return _.map(matches, match => ({
+    return matches.map(match => ({
       match,
       variable: this.cleanVariable(match),
     }));
@@ -379,7 +379,7 @@ class Variables {
    * @returns {Promise[]} Promises for the eventual populated values of the given matches
    */
   populateMatches(matches, property) {
-    return _.map(matches, match => this.splitAndGet(match, property));
+    return matches.map(match => this.splitAndGet(match, property));
   }
   /**
    * Render the given matches and their associated results to the given value

lib/plugins/aws/deploy/lib/uploadArtifacts.js
@@ -83,7 +83,7 @@ module.exports = {
 
     const functionNames = this.serverless.service.getAllFunctions();
     let artifactFilePaths = _.uniq(
-      _.map(functionNames, name => {
+      functionNames.map(name => {
         const functionArtifactFileName = this.provider.naming.getFunctionArtifactName(name);
         const functionObject = this.serverless.service.getFunction(name);
         functionObject.package = functionObject.package || {};
@@ -107,19 +107,21 @@ module.exports = {
 
     const layerNames = this.serverless.service.getAllLayers();
     artifactFilePaths = artifactFilePaths.concat(
-      _.map(layerNames, name => {
-        const layerObject = this.serverless.service.getLayer(name);
-        if (layerObject.artifactAlreadyUploaded) {
-          this.serverless.cli.log(`Skip uploading ${name}`);
-          return null;
-        }
-        return getLambdaLayerArtifactPath(
-          this.packagePath,
-          name,
-          this.provider.serverless.service,
-          this.provider.naming
-        );
-      }).filter(Boolean)
+      layerNames
+        .map(name => {
+          const layerObject = this.serverless.service.getLayer(name);
+          if (layerObject.artifactAlreadyUploaded) {
+            this.serverless.cli.log(`Skip uploading ${name}`);
+            return null;
+          }
+          return getLambdaLayerArtifactPath(
+            this.packagePath,
+            name,
+            this.provider.serverless.service,
+            this.provider.naming
+          );
+        })
+        .filter(Boolean)
     );
 
     return BbPromise.map(

lib/plugins/aws/deployList/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const BbPromise = require('bluebird');
-const _ = require('lodash');
 const validate = require('../lib/validate');
 const findAndGroupDeployments = require('../utils/findAndGroupDeployments');
 const setBucketName = require('../lib/setBucketName');
@@ -78,7 +77,7 @@ class AwsDeployList {
       };
 
       return this.provider.request('Lambda', 'getFunction', params);
-    }).then(result => _.map(result, item => item.Configuration));
+    }).then(result => result.map(item => item.Configuration));
   }
 
   getFunctionPaginatedVersions(params, totalVersions) {

lib/plugins/aws/info/getApiKeyValues.js
@@ -39,7 +39,7 @@ module.exports = {
             .map(resource => resource.PhysicalResourceId)
             .value();
           return Promise.all(
-            _.map(apiKeys, apiKey =>
+            apiKeys.map(apiKey =>
               this.provider.request('APIGateway', 'getApiKey', {
                 apiKey,
                 includeValue: true,
@@ -49,7 +49,7 @@ module.exports = {
         })
         .then(apiKeys => {
           if (apiKeys && apiKeys.length) {
-            info.apiKeys = _.map(apiKeys, apiKey =>
+            info.apiKeys = apiKeys.map(apiKey =>
               _.pick(apiKey, ['name', 'value', 'description', 'customerId'])
             );
           }

lib/plugins/aws/lib/cloudformationSchema.js
@@ -42,8 +42,8 @@ const yamlType = (name, kind) => {
 
 const createSchema = () => {
   const types = _.flatten(
-    _.map(functionNames, functionName =>
-      _.map(['mapping', 'scalar', 'sequence'], kind => yamlType(functionName, kind))
+    functionNames.map(functionName =>
+      ['mapping', 'scalar', 'sequence'].map(kind => yamlType(functionName, kind))
     )
   );
   return YAML.Schema.create(types);

lib/plugins/aws/package/compile/events/apiGateway/lib/method/index.js
@@ -65,7 +65,7 @@ module.exports = {
       let extraCognitoPoolClaims;
       if (event.http.authorizer) {
         const claims = event.http.authorizer.claims || [];
-        extraCognitoPoolClaims = _.map(claims, claim => {
+        extraCognitoPoolClaims = claims.map(claim => {
           if (typeof claim === 'string') {
             const colonIndex = claim.indexOf(':');
             if (colonIndex !== -1) {

commit 25f846b286300522885c5bd9c97fec0519dbf5b4

fix(AWS API Gateway): Fix handling of usagePlan array
Improves leaky logic introduces with https://github.com/serverless/serverless/pull/5982
Exposed by regression introduced with https://github.com/serverless/serverless/pull/7827

lib/plugins/aws/package/compile/events/apiGateway/lib/apiKeys.js
@@ -50,8 +50,9 @@ module.exports = {
         const name = Object.keys(apiKeyDefinition)[0];
         if (
           _.isObject(apiKeyDefinition) &&
+          Array.isArray(this.serverless.service.provider.usagePlan) &&
           _.flatten(
-            (this.serverless.service.provider.usagePlan || []).map(item => Object.keys(item))
+            this.serverless.service.provider.usagePlan.map(item => Object.keys(item))
           ).includes(name)
         ) {
           keyNumber = 0;

lib/plugins/aws/package/compile/events/apiGateway/lib/apiKeys.test.js
@@ -39,6 +39,11 @@ describe('#compileApiKeys()', () => {
       },
     ];
 
+    // Added purely to test https://github.com/serverless/serverless/issues/7844 regression
+    awsCompileApigEvents.serverless.service.provider.usagePlan = {
+      quota: { limit: 5000 },
+    };
+
     return awsCompileApigEvents.compileApiKeys().then(() => {
       const expectedApiKeys = [
         {