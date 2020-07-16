b7fbad92a38bb3bf0f550871173a96e850ed1e21
Mariusz Nowak <medyk@medikoo.com>
2020-07-16 11:16:14 +0200

    test(AWS S3): Reconfigure to rely on more reliable CloudWatch logs resolution

--- tests/integration-all/s3/tests.js
+++ tests/integration-all/s3/tests.js
@@ -6,13 +6,8 @@ const { expect } = require('chai');
 
 const { getTmpDirPath } = require('../../utils/fs');
 const { createBucket, createAndRemoveInBucket, deleteBucket } = require('../../utils/s3');
-const {
-  createTestService,
-  deployService,
-  removeService,
-  waitForFunctionLogs,
-} = require('../../utils/integration');
-const { getMarkers } = require('../shared/utils');
+const { createTestService, deployService, removeService } = require('../../utils/integration');
+const { confirmCloudWatchLogs } = require('../../utils/misc');
 
 describe('AWS - S3 Integration Test', function() {
   this.timeout(1000 * 60 * 10); // Involves time-taking deploys
@@ -77,48 +72,48 @@ describe('AWS - S3 Integration Test', function() {
   describe('Minimal Setup', () => {
     it('should invoke function when an object is created', () => {
       const functionName = 'minimal';
-      const markers = getMarkers(functionName);
       const expectedMessage = `Hello from S3! - (${functionName})`;
 
-      return createAndRemoveInBucket(bucketMinimalSetup)
-        .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-        .then(logs => {
-          expect(/aws:s3/g.test(logs)).to.equal(true);
-          expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
-          expect(logs.includes(expectedMessage)).to.equal(true);
-        });
+      return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+        createAndRemoveInBucket(bucketMinimalSetup)
+      ).then(events => {
+        const logs = events.reduce((data, event) => data + event.message, '');
+        expect(/aws:s3/g.test(logs)).to.equal(true);
+        expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
+        expect(logs.includes(expectedMessage)).to.equal(true);
+      });
     });
   });
 
   describe('Extended Setup', () => {
     it('should invoke function when an object is removed', () => {
       const functionName = 'extended';
-      const markers = getMarkers(functionName);
       const expectedMessage = `Hello from S3! - (${functionName})`;
 
-      return createAndRemoveInBucket(bucketExtendedSetup, { prefix: 'photos/', suffix: '.jpg' })
-        .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-        .then(logs => {
-          expect(/aws:s3/g.test(logs)).to.equal(true);
-          expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
-          expect(logs.includes(expectedMessage)).to.equal(true);
-        });
+      return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+        createAndRemoveInBucket(bucketExtendedSetup, { prefix: 'photos/', suffix: '.jpg' })
+      ).then(events => {
+        const logs = events.reduce((data, event) => data + event.message, '');
+        expect(/aws:s3/g.test(logs)).to.equal(true);
+        expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
+        expect(logs.includes(expectedMessage)).to.equal(true);
+      });
     });
   });
 
   describe('Custom Setup', () => {
     it('should invoke function when an object is created', () => {
       const functionName = 'custom';
-      const markers = getMarkers(functionName);
       const expectedMessage = `Hello from S3! - (${functionName})`;
 
-      return createAndRemoveInBucket(bucketCustomName)
-        .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-        .then(logs => {
-          expect(/aws:s3/g.test(logs)).to.equal(true);
-          expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
-          expect(logs.includes(expectedMessage)).to.equal(true);
-        });
+      return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+        createAndRemoveInBucket(bucketCustomName)
+      ).then(events => {
+        const logs = events.reduce((data, event) => data + event.message, '');
+        expect(/aws:s3/g.test(logs)).to.equal(true);
+        expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
+        expect(logs.includes(expectedMessage)).to.equal(true);
+      });
     });
   });
 
@@ -126,86 +121,86 @@ describe('AWS - S3 Integration Test', function() {
     describe('Single function / single bucket setup', () => {
       it('should invoke function when an object is created', () => {
         const functionName = 'existing';
-        const markers = getMarkers(functionName);
         const expectedMessage = `Hello from S3! - (${functionName})`;
 
-        return createAndRemoveInBucket(bucketExistingSimpleSetup, {
-          prefix: 'Files/',
-          suffix: '.TXT',
-        })
-          .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-          .then(logs => {
-            expect(/aws:s3/g.test(logs)).to.equal(true);
-            expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
-            expect(logs.includes(expectedMessage)).to.equal(true);
-          });
+        return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+          createAndRemoveInBucket(bucketExistingSimpleSetup, {
+            prefix: 'Files/',
+            suffix: '.TXT',
+          })
+        ).then(events => {
+          const logs = events.reduce((data, event) => data + event.message, '');
+          expect(/aws:s3/g.test(logs)).to.equal(true);
+          expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
+          expect(logs.includes(expectedMessage)).to.equal(true);
+        });
       });
     });
 
     describe('Multi function / multi bucket setup', () => {
       it('should invoke function when a .jpg object is created', () => {
         const functionName = 'existingCreated';
-        const markers = getMarkers(functionName);
         const expectedMessage = `Hello from S3! - (${functionName})`;
 
-        return createAndRemoveInBucket(bucketExistingComplexSetup, {
-          prefix: 'photos',
-          suffix: '.jpg',
-        })
-          .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-          .then(logs => {
-            expect(/aws:s3/g.test(logs)).to.equal(true);
-            expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
-            expect(logs.includes(expectedMessage)).to.equal(true);
-          });
+        return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+          createAndRemoveInBucket(bucketExistingComplexSetup, {
+            prefix: 'photos',
+            suffix: '.jpg',
+          })
+        ).then(events => {
+          const logs = events.reduce((data, event) => data + event.message, '');
+          expect(/aws:s3/g.test(logs)).to.equal(true);
+          expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
+          expect(logs.includes(expectedMessage)).to.equal(true);
+        });
       });
       it('should invoke function when a .jpg object is removed', () => {
         const functionName = 'existingRemoved';
-        const markers = getMarkers(functionName);
         const expectedMessage = `Hello from S3! - (${functionName})`;
 
-        return createAndRemoveInBucket(bucketExistingComplexSetup, {
-          prefix: 'photos',
-          suffix: '.jpg',
-        })
-          .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-          .then(logs => {
-            expect(/aws:s3/g.test(logs)).to.equal(true);
-            expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
-            expect(logs.includes(expectedMessage)).to.equal(true);
-          });
+        return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+          createAndRemoveInBucket(bucketExistingComplexSetup, {
+            prefix: 'photos',
+            suffix: '.jpg',
+          })
+        ).then(events => {
+          const logs = events.reduce((data, event) => data + event.message, '');
+          expect(/aws:s3/g.test(logs)).to.equal(true);
+          expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
+          expect(logs.includes(expectedMessage)).to.equal(true);
+        });
       });
       it('should invoke function when a .png object is created', () => {
         const functionName = 'existingCreated';
-        const markers = getMarkers(functionName);
         const expectedMessage = `Hello from S3! - (${functionName})`;
 
-        return createAndRemoveInBucket(bucketExistingComplexSetup, {
-          prefix: 'photos',
-          suffix: '.png',
-        })
-          .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-          .then(logs => {
-            expect(/aws:s3/g.test(logs)).to.equal(true);
-            expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
-            expect(logs.includes(expectedMessage)).to.equal(true);
-          });
+        return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+          createAndRemoveInBucket(bucketExistingComplexSetup, {
+            prefix: 'photos',
+            suffix: '.png',
+          })
+        ).then(events => {
+          const logs = events.reduce((data, event) => data + event.message, '');
+          expect(/aws:s3/g.test(logs)).to.equal(true);
+          expect(/ObjectCreated:Put/g.test(logs)).to.equal(true);
+          expect(logs.includes(expectedMessage)).to.equal(true);
+        });
       });
       it('should invoke function when a .png object is removed', () => {
         const functionName = 'existingRemoved';
-        const markers = getMarkers(functionName);
         const expectedMessage = `Hello from S3! - (${functionName})`;
 
-        return createAndRemoveInBucket(bucketExistingComplexSetup, {
-          prefix: 'photos',
-          suffix: '.png',
-        })
-          .then(() => waitForFunctionLogs(tmpDirPath, functionName, markers.start, markers.end))
-          .then(logs => {
-            expect(/aws:s3/g.test(logs)).to.equal(true);
-            expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
-            expect(logs.includes(expectedMessage)).to.equal(true);
-          });
+        return confirmCloudWatchLogs(`/aws/lambda/${stackName}-${functionName}`, () =>
+          createAndRemoveInBucket(bucketExistingComplexSetup, {
+            prefix: 'photos',
+            suffix: '.png',
+          })
+        ).then(events => {
+          const logs = events.reduce((data, event) => data + event.message, '');
+          expect(/aws:s3/g.test(logs)).to.equal(true);
+          expect(/ObjectRemoved:Delete/g.test(logs)).to.equal(true);
+          expect(logs.includes(expectedMessage)).to.equal(true);
+        });
       });
     });
   });
