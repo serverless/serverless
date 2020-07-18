9626d585c19b15466e4601c282c96af353912333
Yuji Yamano <Yamano_Yuji@ogis-ri.co.jp>
2020-07-18 13:16:18 +0900

    Add test for RequestHandler.

--- lib/plugins/aws/invokeLocal/java/src/test/java/com/serverless/InvokeBridgeTest.java
+++ lib/plugins/aws/invokeLocal/java/src/test/java/com/serverless/InvokeBridgeTest.java
@@ -2,19 +2,27 @@ package com.serverless;
 
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class InvokeBridgeTest {
-  @Before
-  public void before() {
-    System.setProperty("artifactPath", "target/test-classes/com/serverless/RequestStreamHandler.class");
-    System.setProperty("className", "com.serverless.RequestStreamHandler");
+
+  @Test
+  public void verifyInvokeRequestHandler() {
+    System.setProperty("artifactPath", "target/test-classes/com/serverless/RequestHandler.class");
+    System.setProperty("className", "com.serverless.RequestHandler");
     System.setProperty("handlerName", "handleRequest");
+
+    System.setIn(getClass().getResourceAsStream("/test.json"));
+    InvokeBridge.main(new String[] {});
+    assertNotNull(RequestStreamHandler.input);
   }
 
   @Test
-  public void verifyInvoke() {
+  public void verifyInvokeRequestStreamHandler() {
+    System.setProperty("artifactPath", "target/test-classes/com/serverless/RequestStreamHandler.class");
+    System.setProperty("className", "com.serverless.RequestStreamHandler");
+    System.setProperty("handlerName", "handleRequest");
+
     System.setIn(getClass().getResourceAsStream("/test.json"));
     InvokeBridge.main(new String[] {});
     assertNotNull(RequestStreamHandler.input);
