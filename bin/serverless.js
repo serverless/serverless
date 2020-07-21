cf1d51dbb9b218dfc1cebfa1bf3c5f6eb1ab248b
Mariusz Nowak <medyk@medikoo.com>
2020-07-21 17:07:42 +0200

    refactor(CLI): Cleanup components resolution logic

--- bin/serverless.js
+++ bin/serverless.js
@@ -12,20 +12,30 @@ if (require('../lib/utils/isStandaloneExecutable')) {
 
 const nodeVersion = Number(process.version.split('.')[0].slice(1));
 
-// only check for components if user is running Node 8
+// CLI Triage
+// Serverless Components work only in Node.js v8+
 if (nodeVersion >= 8) {
-  const componentsV1 = require('../lib/components-v1');
-  const componentsV2 = require('../lib/components-v2');
+  try {
+    const componentsV1 = require('@serverless/cli');
+    const componentsV2 = require('@serverless/components');
 
-  if (componentsV1 && componentsV1.runningComponents()) {
-    componentsV1.runComponents();
-    return;
-  }
+    if (componentsV1.runningComponents()) {
+      // Serverless Components v1 CLI (deprecated)
+      componentsV1.runComponents();
+      return;
+    }
 
-  if (componentsV2 && componentsV2.runningComponents()) {
-    componentsV2.runComponents();
-    return;
+    if (componentsV2.runningComponents()) {
+      // Serverless Components CLI
+      componentsV2.runComponents();
+      return;
+    }
+  } catch (error) {
+    if (process.env.SLS_DEBUG) {
+      require('../lib/classes/Error').logWarning(`CLI triage crashed with: ${error.stack}`);
+    }
   }
 }
 
+// Serverless Framework CLI
 require('../scripts/serverless');
