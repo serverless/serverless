c872c143ba11f58fdc0f77420437f4a6e6205464
Mariusz Nowak <medyk@medikoo.com>
2020-07-22 16:51:55 +0200

    refactor: Seclude generic version artifacts handlers

--- tests/mocha-reporter.js
+++ tests/mocha-reporter.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const disableServerlessStatsRequests = require('@serverless/test/disable-serverless-stats-requests');
-const customResourceZipGenerator = require('../lib/plugins/aws/customResources/generateZip');
+const { _ensureArtifact } = require('../lib/utils/getEnsureArtifact');
 
 disableServerlessStatsRequests(path.resolve(__dirname, '..'));
 
@@ -12,9 +12,9 @@ module.exports.deferredRunner.then(runner => {
   runner.on('suite end', suite => {
     if (!suite.parent || !suite.parent.root) return;
 
-    // Ensure to reset memoization on custom resource zip generator after each test file run.
+    // Ensure to reset memoization on artifacts generation after each test file run.
     // Reason is that homedir is automatically cleaned for each test,
     // therefore eventually built custom resource file is also removed
-    customResourceZipGenerator.cache.clear();
+    _ensureArtifact.clear();
   });
 });
