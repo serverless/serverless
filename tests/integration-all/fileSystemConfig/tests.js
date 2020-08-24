8c8cfeaa6f758aa6e3b8c45155795fa072a83414
Piotr Grzesik <pj.grzesik@gmail.com>
2020-08-24 19:53:22 +0200

    Add integration test

---
+++ tests/integration-all/fileSystemConfig/tests.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const path = require('path');
+const { expect } = require('chai');
+
+const awsRequest = require('@serverless/test/aws-request');
+const fs = require('fs');
+const { getTmpDirPath } = require('../../utils/fs');
+const crypto = require('crypto');
+const { createTestService, deployService, removeService } = require('../../utils/integration');
+
+describe('AWS - FileSystemConfigs Integration Test', function() {
+  this.timeout(1000 * 60 * 100); // Involves time-taking deploys
+  let serviceName;
+  let stackName;
+  let tmpDirPath;
+  const stage = 'dev';
+  const resourcesStackName = `efs-integration-tests-deps-stack-${crypto
+    .randomBytes(8)
+    .toString('hex')}`;
+
+  before(async () => {
+    tmpDirPath = getTmpDirPath();
+    console.info(`Temporary path: ${tmpDirPath}`);
+    const cfnTemplate = fs.readFileSync(path.join(__dirname, 'cloudformation.yml'), 'utf8');
+
+    console.info('Deploying CloudFormation stack with required resources...');
+    await awsRequest('CloudFormation', 'createStack', {
+      StackName: resourcesStackName,
+      TemplateBody: cfnTemplate,
+    });
+    const waitForResult = await awsRequest('CloudFormation', 'waitFor', 'stackCreateComplete', {
+      StackName: resourcesStackName,
+    });
+
+    const outputMap = waitForResult.Stacks[0].Outputs.reduce((map, output) => {
+      map[output.OutputKey] = output.OutputValue;
+      return map;
+    }, {});
+
+    const serverlessConfig = await createTestService(tmpDirPath, {
+      templateDir: path.join(__dirname, 'service'),
+      filesToAdd: [path.join(__dirname, '..', 'shared')],
+      serverlessConfigHook: config => {
+        const fileSystemConfig = {
+          localMountPath: '/mnt/testing',
+          arn: outputMap.AccessPointARN,
+        };
+        config.provider.vpc = {
+          subnetIds: [outputMap.Subnet],
+          securityGroupIds: [outputMap.SecurityGroup],
+        };
+        config.functions.writer.fileSystemConfigs = [fileSystemConfig];
+        config.functions.reader.fileSystemConfigs = [fileSystemConfig];
+      },
+    });
+    serviceName = serverlessConfig.service;
+    stackName = `${serviceName}-${stage}`;
+    console.info(`Deploying "${stackName}" service...`);
+    return deployService(tmpDirPath);
+  });
+
+  after(async () => {
+    console.info('Removing service...');
+    await removeService(tmpDirPath);
+    console.info('Removing CloudFormation stack with required resources...');
+    await awsRequest('CloudFormation', 'deleteStack', { StackName: resourcesStackName });
+    return awsRequest('CloudFormation', 'waitFor', 'stackDeleteComplete', {
+      StackName: resourcesStackName,
+    });
+  });
+
+  describe('Basic Setup', () => {
+    it('should be able to write to efs and read from it in a separate function', async () => {
+      await awsRequest('Lambda', 'invoke', {
+        FunctionName: `${stackName}-writer`,
+        InvocationType: 'RequestResponse',
+      });
+
+      const readerResult = await awsRequest('Lambda', 'invoke', {
+        FunctionName: `${stackName}-reader`,
+        InvocationType: 'RequestResponse',
+      });
+      const payload = JSON.parse(readerResult.Payload);
+
+      expect(payload).to.deep.equal({ result: 'fromlambda' });
+    });
+  });
+});
