24b2c9451b35d385d54f4a62a56f40f5236d77e1
Mariusz Nowak <medyk@medikoo.com>
2020-08-25 16:49:43 +0200

    feat: Drop support for Node.js versions below v10
    BREAKING CHANGE:
    Node.js versions below v10 are no longer supported

--- bin/serverless.js
+++ bin/serverless.js
@@ -10,30 +10,25 @@ if (require('../lib/utils/isStandaloneExecutable')) {
   }
 }
 
-const nodeVersion = Number(process.version.split('.')[0].slice(1));
-
 // CLI Triage
-// Serverless Components work only in Node.js v8+
-if (nodeVersion >= 8) {
-  try {
-    const componentsV1 = require('@serverless/cli');
-    const componentsV2 = require('@serverless/components');
+try {
+  const componentsV1 = require('@serverless/cli');
+  const componentsV2 = require('@serverless/components');
 
-    if (componentsV1.runningComponents()) {
-      // Serverless Components v1 CLI (deprecated)
-      componentsV1.runComponents();
-      return;
-    }
+  if (componentsV1.runningComponents()) {
+    // Serverless Components v1 CLI (deprecated)
+    componentsV1.runComponents();
+    return;
+  }
 
-    if (componentsV2.runningComponents()) {
-      // Serverless Components CLI
-      componentsV2.runComponents();
-      return;
-    }
-  } catch (error) {
-    if (process.env.SLS_DEBUG) {
-      require('../lib/classes/Error').logWarning(`CLI triage crashed with: ${error.stack}`);
-    }
+  if (componentsV2.runningComponents()) {
+    // Serverless Components CLI
+    componentsV2.runComponents();
+    return;
+  }
+} catch (error) {
+  if (process.env.SLS_DEBUG) {
+    require('../lib/classes/Error').logWarning(`CLI triage crashed with: ${error.stack}`);
   }
 }
 
