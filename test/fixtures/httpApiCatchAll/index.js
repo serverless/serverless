57108cdf197718d99c8363fbbead4c80e3d9f175
Mariusz Nowak <medyk@medikoo.com>
2020-08-28 16:30:27 +0200

    test(AWS HTTP API): FIx after default payload mode change

--- test/fixtures/httpApiCatchAll/index.js
+++ test/fixtures/httpApiCatchAll/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-module.exports.handler = (event, context, callback) =>
+module.exports.handler = (event, context, callback) => {
+  const httpData = event.requestContext.http;
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
-      path: event.path,
-      method: event.httpMethod,
+      path: httpData.path,
+      method: httpData.method,
     }),
   });
+};
