04641c2c917774d3af7d97823cd7d21e10679403
Frédéric Barthelet <fredericb@theodo.fr>
2020-10-06 18:59:59 +0200

    Add integration test

--- test/fixtures/iotFleetProvisioning/index.js
+++ test/fixtures/iotFleetProvisioning/index.js
@@ -1,9 +1,133 @@
 'use strict';
 
-function registerThing() {}
+// NOTE: `aws-iot-device-sdk-v2` is bundled into the deployment package
+// eslint-disable-next-line import/no-unresolved
+const { mqtt, io, iot, iotidentity } = require('aws-iot-device-sdk-v2');
 
-function iotFleetPreProvisioningHook(event, context, callback) {
-  return callback(null, event);
+async function executeKeys(identity) {
+  return new Promise((resolve, reject) => {
+    try {
+      const keysAccepted = (error, response) => {
+        if (response) {
+          resolve(response.certificateOwnershipToken);
+        }
+        reject(error);
+      };
+
+      const keysRejected = (error, response) => {
+        if (response) {
+          reject(response);
+        }
+        reject(error);
+      };
+
+      const subscsribeAccepted = identity.subscribeToCreateKeysAndCertificateAccepted(
+        {},
+        mqtt.QoS.AtLeastOnce,
+        keysAccepted
+      );
+      const subscsribeRejected = identity.subscribeToCreateKeysAndCertificateRejected(
+        {},
+        mqtt.QoS.AtLeastOnce,
+        keysRejected
+      );
+
+      Promise.all([subscsribeAccepted, subscsribeRejected]).then(() => {
+        identity.publishCreateKeysAndCertificate(
+          {
+            toJSON() {
+              return {};
+            },
+          },
+          mqtt.QoS.AtLeastOnce
+        );
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
-module.exports = { registerThing, iotFleetPreProvisioningHook };
+async function executeRegisterThing(identity, token) {
+  return new Promise((resolve, reject) => {
+    try {
+      const registerAccepted = (error, response) => {
+        if (response) {
+          resolve();
+        }
+        reject(error);
+      };
+
+      const registerRejected = (error, response) => {
+        if (response) {
+          reject(response);
+        }
+        reject(error);
+      };
+
+      const subscribeAccepted = identity.subscribeToRegisterThingAccepted(
+        { templateName: process.env.TEMPLATE_NAME },
+        mqtt.QoS.AtLeastOnce,
+        registerAccepted
+      );
+      const subscribeRejected = identity.subscribeToRegisterThingRejected(
+        { templateName: process.env.TEMPLATE_NAME },
+        mqtt.QoS.AtLeastOnce,
+        registerRejected
+      );
+      Promise.all([subscribeAccepted, subscribeRejected]).then(() => {
+        identity.publishRegisterThing(
+          {
+            parameters: {},
+            templateName: process.env.TEMPLATE_NAME,
+            certificateOwnershipToken: token,
+          },
+          mqtt.QoS.AtLeastOnce
+        );
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function connect(config) {
+  return new Promise((resolve, reject) => {
+    try {
+      const clientBootstrap = new io.ClientBootstrap();
+      const client = new mqtt.MqttClient(clientBootstrap);
+      const connection = client.new_connection(config);
+      const identity = new iotidentity.IotIdentityClient(connection);
+      connection.connect().then(() => {
+        resolve(identity);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function registerDevice(event) {
+  io.enable_logging(io.LogLevel.TRACE);
+
+  const config = iot.AwsIotMqttConnectionConfigBuilder.new_mtls_builder(
+    event.certificatePem,
+    event.keyPair.PrivateKey
+  )
+    .with_clean_session(false)
+    .with_client_id('test')
+    .with_endpoint(event.iotEndpoint)
+    .build();
+
+  const identity = await connect(config);
+  const token = await executeKeys(identity);
+  await executeRegisterThing(identity, token);
+
+  return 'success';
+}
+
+function iotFleetPreProvisioningHook(event) {
+  return event;
+}
+
+module.exports = { registerDevice, iotFleetPreProvisioningHook };
--- test/integration/iotFleetProvisioning.test.js
+++ test/integration/iotFleetProvisioning.test.js
@@ -1,14 +1,27 @@
 'use strict';
 
+const awsRequest = require('@serverless/test/aws-request');
 const fixtures = require('../fixtures');
 const { deployService, removeService } = require('../utils/integration');
+const { resolveIotEndpoint } = require('../utils/iot');
 
 describe('AWS - IoT Fleet Provisioning Integration Test', function() {
   this.timeout(1000 * 60 * 100); // Involves time-taking deploys
+  let stackName;
   let servicePath;
+  const stage = 'dev';
+
+  const resolveTemplateName = async () => {
+    const result = await awsRequest('CloudFormation', 'describeStacks', { StackName: stackName });
+    return result.Stacks[0].Outputs.find(output => output.OutputKey === 'ProvisioningTemplateName')
+      .OutputValue;
+  };
 
   before(async () => {
-    ({ servicePath } = await fixtures.setup('iotFleetProvisioning'));
+    let serviceConfig;
+    ({ serviceConfig, servicePath } = await fixtures.setup('iotFleetProvisioning'));
+    const serviceName = serviceConfig.service;
+    stackName = `${serviceName}-${stage}`;
     await deployService(servicePath);
   });
 
@@ -16,5 +29,20 @@ describe('AWS - IoT Fleet Provisioning Integration Test', function() {
     await removeService(servicePath);
   });
 
-  it('setup a new IoT Thing with the provisioning template', async () => {});
+  it('setup a new IoT Thing with the provisioning template', async () => {
+    const [certificates, iotEndpoint] = await Promise.all([
+      awsRequest('Iot', 'createProvisioningClaim', {
+        templateName: await resolveTemplateName(),
+      }),
+      resolveIotEndpoint(),
+    ]);
+    await awsRequest('Lambda', 'invoke', {
+      FunctionName: `${stackName}-registerDevice`,
+      InvocationType: 'RequestResponse',
+      Payload: JSON.stringify({
+        iotEndpoint,
+        ...certificates,
+      }),
+    });
+  });
 });
