a60fe25a84d5799b6fcf4943ad0eb0a48a756dce
Frédéric Barthelet <fredericb@theodo.fr>
2020-10-06 13:28:39 +0200

    Remove unecessary log

--- test/fixtures/iotFleetProvisioning/index.js
+++ test/fixtures/iotFleetProvisioning/index.js
@@ -1,12 +1,6 @@
 'use strict';
 
-// NOTE: the `utils.js` file is bundled into the deployment package
-// eslint-disable-next-line
-const { log } = require('./utils');
-
 function iotFleetPreProvisioningHook(event, context, callback) {
-  const functionName = 'iotFleetPreProvisioningHook';
-  log(functionName, JSON.stringify(event));
   return callback(null, event);
 }
 
