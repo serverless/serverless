f56d1b3d01499f1e5aedb73d01a8f113f78f9cce
Mariusz Nowak <medyk@medikoo.com>
2020-11-02 11:35:32 +0100

    test: Refactor to async/await

--- test/integration/iot.test.js
+++ test/integration/iot.test.js
@@ -30,17 +30,15 @@ describe('AWS - IoT Integration Test', function() {
   });
 
   describe('Basic Setup', () => {
-    it('should invoke on a topic message matching the rule', () => {
+    it('should invoke on a topic message matching the rule', async () => {
       const functionName = 'iotBasic';
       const markers = getMarkers(functionName);
       const message = JSON.stringify({ message: 'Hello from IoT!' });
 
       // NOTE: This test may fail on fresh accounts where the IoT endpoint has not completed provisioning
-      return publishIotData(iotTopic, message)
-        .then(() => waitForFunctionLogs(servicePath, functionName, markers.start, markers.end))
-        .then(logs => {
-          expect(logs).to.include(message);
-        });
+      await publishIotData(iotTopic, message);
+      const logs = await waitForFunctionLogs(servicePath, functionName, markers.start, markers.end);
+      expect(logs).to.include(message);
     });
   });
 });
