13d94bb778a548c6a31e8ef85cc3063aa5bcc041
Mariusz Nowak <medyk@medikoo.com>
2020-11-18 12:33:06 +0100

    refactor: Refactor to async/await

--- test/utils/run-serverless.js
+++ test/utils/run-serverless.js
@@ -2,26 +2,22 @@
 
 const path = require('path');
 const runServerless = require('@serverless/test/run-serverless');
-const BbPromise = require('bluebird');
 const fixtures = require('../fixtures');
 
 const serverlessPath = path.join(__dirname, '../../');
 
-module.exports = options =>
-  BbPromise.try(() => {
-    if (options.fixture && options.cwd) {
-      throw new Error('Either "fixture" or "cwd" should be provided');
-    }
-    const runServerlessOptions = Object.assign({}, options);
-    delete runServerlessOptions.serverlessPath;
-    delete runServerlessOptions.fixture;
-    delete runServerlessOptions.configExt;
-    const fixturePromise = options.fixture
-      ? fixtures
-          .setup(options.fixture, { configExt: options.configExt })
-          .then(({ servicePath }) => (runServerlessOptions.cwd = servicePath))
-      : BbPromise.resolve();
-    return fixturePromise.then(() =>
-      runServerless(options.serverlessPath || serverlessPath, runServerlessOptions)
-    );
-  });
+module.exports = async options => {
+  if (options.fixture && options.cwd) {
+    throw new Error('Either "fixture" or "cwd" should be provided');
+  }
+  const runServerlessOptions = Object.assign({}, options);
+  delete runServerlessOptions.serverlessPath;
+  delete runServerlessOptions.fixture;
+  delete runServerlessOptions.configExt;
+  if (options.fixture) {
+    const fixtureData = await fixtures.setup(options.fixture, { configExt: options.configExt });
+    runServerlessOptions.cwd = fixtureData.servicePath;
+  }
+
+  return runServerless(options.serverlessPath || serverlessPath, runServerlessOptions);
+};
