ff7ecde3b06e8c26e3ba282c0938efbc6718aabd
Mariusz Nowak <medyk@medikoo.com>
2020-12-07 13:06:33 +0100

    test(AWS Lambda): Pave path for layers tests refactor

--- test/unit/lib/plugins/aws/package/compile/layers/index.test.js
+++ test/unit/lib/plugins/aws/package/compile/layers/index.test.js
@@ -1,19 +1,32 @@
 'use strict';
 
-const crypto = require('crypto');
 const path = require('path');
 const chai = require('chai');
-const fs = require('fs');
 const sinon = require('sinon');
-const _ = require('lodash');
 const AwsProvider = require('../../../../../../../../lib/plugins/aws/provider/awsProvider');
 const AwsCompileLayers = require('../../../../../../../../lib/plugins/aws/package/compile/layers/index');
 const Serverless = require('../../../../../../../../lib/Serverless');
 const { getTmpDirPath } = require('../../../../../../../utils/fs');
+const runServerless = require('../../../../../../../utils/run-serverless');
 
 chai.use(require('chai-as-promised'));
 
 const expect = chai.expect;
+
+const awsRequestStubMap = {
+  CloudFormation: {
+    describeStacks: {
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'LayerLambdaLayerHash', OutputValue: '1qaz' },
+            { OutputKey: 'LayerLambdaLayerS3Key', OutputValue: 'a/b/c/foo.zip' },
+          ],
+        },
+      ],
+    },
+  },
+};
 
 describe('AwsCompileLayers', () => {
   let serverless;
@@ -81,11 +94,6 @@ describe('AwsCompileLayers', () => {
     });
   });
 
-  describe('#constructor()', () => {
-    it('should set the provider variable to an instance of AwsProvider', () =>
-      expect(awsCompileLayers.provider).to.be.instanceof(AwsProvider));
-  });
-
   describe('#compileLayers()', () => {
     it('should use layer artifact if individually', () => {
       awsCompileLayers.serverless.service.package.individually = true;
@@ -141,190 +149,6 @@ describe('AwsCompileLayers', () => {
           awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
             .TestLambdaLayerQualifiedArn
         ).to.deep.equal(compiledLayerOutput);
-      });
-    });
-
-    it('should create a layer resource with a retention policy', () => {
-      const s3Folder = awsCompileLayers.serverless.service.package.artifactDirectoryName;
-      const s3FileName = awsCompileLayers.serverless.service.layers.test.package.artifact
-        .split(path.sep)
-        .pop();
-      awsCompileLayers.serverless.service.layers = {
-        test: {
-          path: 'layer',
-          retain: true,
-        },
-      };
-      const compiledLayer = {
-        Type: 'AWS::Lambda::LayerVersion',
-        Properties: {
-          Content: {
-            S3Bucket: { Ref: 'ServerlessDeploymentBucket' },
-            S3Key: `${s3Folder}/${s3FileName}`,
-          },
-          LayerName: 'test',
-        },
-      };
-      const layerArtifactBinary = fs.readFileSync(
-        path.join(awsCompileLayers.packagePath, 'test.zip')
-      );
-
-      const sha = crypto
-        .createHash('sha1')
-        .update(JSON.stringify(_.omit(compiledLayer, ['Properties.Content.S3Key'])))
-        .update(layerArtifactBinary)
-        .digest('hex');
-      compiledLayer.DeletionPolicy = 'Retain';
-      const compiledLayerOutput = {
-        Description: 'Current Lambda layer version',
-        Value: {
-          Ref: `TestLambdaLayer${sha}`,
-        },
-      };
-
-      return expect(awsCompileLayers.compileLayers()).to.be.fulfilled.then(() => {
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Resources[
-            `TestLambdaLayer${sha}`
-          ]
-        ).to.deep.equal(compiledLayer);
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerQualifiedArn
-        ).to.deep.equal(compiledLayerOutput);
-      });
-    });
-
-    it('should create a layer resource with a retention policy, has the same logical id when not modify', () => {
-      awsCompileLayers.serverless.service.layers = {
-        test: {
-          path: 'layer',
-          retain: true,
-        },
-      };
-      const secondAwsCompileLayers = _.cloneDeep(awsCompileLayers);
-      secondAwsCompileLayers.serverless.service.package.artifactDirectoryName = 'somedir2';
-
-      const secondProviderRequestStub = sinon.stub(secondAwsCompileLayers.provider, 'request');
-      secondProviderRequestStub.withArgs('CloudFormation', 'describeStacks').resolves({
-        Stacks: [
-          {
-            Outputs: [
-              {
-                OutputKey: 'TestLambdaLayerHash',
-                OutputValue: '18c9a02dcb78d62bb202964f0da15366b9730863',
-              },
-              { OutputKey: 'TestLambdaLayerS3Key', OutputValue: 'somedir/test.zip' },
-            ],
-          },
-        ],
-      });
-
-      return expect(
-        Promise.all([awsCompileLayers.compileLayers(), secondAwsCompileLayers.compileLayers()])
-      ).to.be.fulfilled.then(() => {
-        const resources =
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Resources;
-        const secondResources =
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate
-            .Resources;
-        expect(
-          Object.keys(resources).find(key => resources[key].Type === 'AWS::Lambda::LayerVersion')
-        ).to.deep.equal(
-          Object.keys(secondResources).find(
-            key => secondResources[key].Type === 'AWS::Lambda::LayerVersion'
-          )
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerQualifiedArn
-        ).to.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerQualifiedArn
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerHash
-        ).to.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerHash,
-          'Hash should be same if layer is NOT modified'
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerS3Key.Value
-        ).to.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerS3Key.Value,
-          'S3Key should be same if layer is NOT modified'
-        );
-      });
-    });
-
-    it('should create a layer resource with a retention policy, has different logical id when modify', () => {
-      awsCompileLayers.serverless.service.layers = {
-        test: {
-          path: 'layer',
-          retain: true,
-        },
-      };
-      const secondAwsCompileLayers = _.cloneDeep(awsCompileLayers);
-      secondAwsCompileLayers.serverless.service.layers.test.description = 'modified description';
-      secondAwsCompileLayers.serverless.service.package.artifactDirectoryName = 'somedir2';
-
-      const secondProviderRequestStub = sinon.stub(secondAwsCompileLayers.provider, 'request');
-      secondProviderRequestStub.withArgs('CloudFormation', 'describeStacks').resolves({
-        Stacks: [
-          {
-            Outputs: [
-              {
-                OutputKey: 'TestLambdaLayerHash',
-                OutputValue: '18c9a02dcb78d62bb202964f0da15366b9730863',
-              },
-              { OutputKey: 'TestLambdaLayerS3Key', OutputValue: 'a/b/c/foo.zip' },
-            ],
-          },
-        ],
-      });
-
-      return expect(
-        Promise.all([awsCompileLayers.compileLayers(), secondAwsCompileLayers.compileLayers()])
-      ).to.be.fulfilled.then(() => {
-        const resources =
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Resources;
-        const secondResources =
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate
-            .Resources;
-        expect(
-          Object.keys(resources).find(key => resources[key].Type === 'AWS::Lambda::LayerVersion')
-        ).to.not.deep.equal(
-          Object.keys(secondResources).find(
-            key => secondResources[key].Type === 'AWS::Lambda::LayerVersion'
-          )
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerQualifiedArn
-        ).to.not.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerQualifiedArn
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerHash
-        ).to.not.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerHash,
-          'Hash should be different if layer is modified'
-        );
-        expect(
-          awsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerS3Key.Value
-        ).to.not.deep.equal(
-          secondAwsCompileLayers.serverless.service.provider.compiledCloudFormationTemplate.Outputs
-            .TestLambdaLayerS3Key.Value,
-          'S3Key should be different if layer is modified'
-        );
       });
     });
 
@@ -495,5 +319,107 @@ describe('AwsCompileLayers', () => {
         ).to.deep.equal(compiledLayerOutput);
       });
     });
+  });
+});
+
+describe('lib/plugins/aws/package/compile/layers/index.test.js', () => {
+  let cfResources;
+  let naming;
+  let updateConfig;
+  let servicePath;
+
+  before(async () => {
+    const { awsNaming, cfTemplate, fixtureData } = await runServerless({
+      fixture: 'layer',
+      cliArgs: ['package'],
+      configExt: {
+        layers: {
+          layerRetain: {
+            path: 'layer',
+            retain: true,
+          },
+        },
+      },
+      awsRequestStubMap,
+    });
+    cfResources = cfTemplate.Resources;
+    naming = awsNaming;
+    ({ updateConfig, servicePath } = fixtureData);
+  });
+
+  it.skip('TODO: should support `layers[].package.artifact` with `package.individually`', () => {
+    // Replaces
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L90-L106
+  });
+
+  it.skip('TODO: should generate expected layer version resource', () => {
+    // Replaces
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L108-L145
+  });
+
+  describe('`layers[].retain` property', () => {
+    it('should ensure expected deletion policy', () => {
+      const layerResourceNamePrefix = naming.getLambdaLayerLogicalId('layerRetain');
+      const layerResourceName = Object.keys(cfResources).find(resourceName =>
+        resourceName.startsWith(layerResourceNamePrefix)
+      );
+      expect(layerResourceName).to.not.equal(layerResourceNamePrefix);
+      const layerResource = cfResources[layerResourceName];
+      expect(layerResource.DeletionPolicy).to.equal('Retain');
+    });
+
+    it('should ensure unique resource id per layer version', async () => {
+      const layerResourceNamePrefix = naming.getLambdaLayerLogicalId('layerRetain');
+      const firstLayerResourceName = Object.keys(cfResources).find(resourceName =>
+        resourceName.startsWith(layerResourceNamePrefix)
+      );
+
+      await updateConfig({ layers: { layerRetain: { description: 'foo' } } });
+      const {
+        cfTemplate: { Resources: secondCfResources },
+      } = await runServerless({
+        cwd: servicePath,
+        cliArgs: ['package'],
+        awsRequestStubMap,
+      });
+      expect(secondCfResources).to.not.have.property(firstLayerResourceName);
+
+      await updateConfig({ layers: { layerRetain: { description: null } } });
+      const {
+        cfTemplate: { Resources: firstCfResources },
+      } = await runServerless({
+        cwd: servicePath,
+        cliArgs: ['package'],
+        awsRequestStubMap,
+      });
+      expect(firstCfResources).to.have.property(firstLayerResourceName);
+      // Replaces
+      // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L147-L329
+    });
+  });
+
+  it.skip('TODO: should generate expected permissions resource', () => {
+    // Replaces
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L331-L383
+  });
+
+  it.skip('TODO: should support `layers[].allowedAccounts`', () => {
+    // Replaces
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L331-L452
+  });
+
+  it.skip('TODO: should support `layers[].description`', () => {
+    // Replaces partially
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L454-L497
+  });
+
+  it.skip('TODO: should support `layers[].compatibleRuntimes`', () => {
+    // Replaces partially
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L454-L497
+  });
+
+  it.skip('TODO: should support `layers[].licenseInfo`', () => {
+    // Replaces partially
+    // https://github.com/serverless/serverless/blob/e78f695004bf292c4163daf9705e5e0c6cbe2592/test/unit/lib/plugins/aws/package/compile/layers/index.test.js#L454-L497
   });
 });
