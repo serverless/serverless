3632e0ee09945ed5f293779a68409cb297c7d0cc
Jin <3448324+jinhong-@users.noreply.github.com>
2020-12-11 21:04:11 +0800

    feat(AWS ALB): Recognize `path` as optional condition (#8571)

--- test/unit/lib/plugins/aws/package/compile/events/alb/index.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/alb/index.test.js
@@ -1,18 +1,26 @@
 'use strict';
 
 const runServerless = require('../../../../../../../../utils/run-serverless');
-const { expect } = require('chai');
+const { ServerlessError } = require('../../../../../../../../../lib/classes/Error');
+const { use: chaiUse, expect } = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chaiUse(chaiAsPromised);
 
 describe('lib/plugins/aws/package/compile/eents/alb/index.test.js', () => {
   let cfResources;
   let naming;
 
+  const baseEventConfig = {
+    listenerArn:
+      'arn:aws:elasticloadbalancing:' +
+      'us-east-1:123456789012:listener/app/my-load-balancer/' +
+      '50dc6c495c0c9188/f2f7dc8efc522ab2',
+  };
+
   before(async () => {
-    const baseEventConfig = {
-      listenerArn:
-        'arn:aws:elasticloadbalancing:' +
-        'us-east-1:123456789012:listener/app/my-load-balancer/' +
-        '50dc6c495c0c9188/f2f7dc8efc522ab2',
+    const validBaseEventConfig = {
+      ...baseEventConfig,
       conditions: {
         path: '/',
       },
@@ -43,15 +51,25 @@ describe('lib/plugins/aws/package/compile/eents/alb/index.test.js', () => {
         functions: {
           fnAuthorizerOnUnauthenticatedRequestDeny: {
             handler: 'index.handler',
-            events: [{ alb: { ...baseEventConfig, priority: 1, authorizer: 'deny' } }],
+            events: [{ alb: { ...validBaseEventConfig, priority: 1, authorizer: 'deny' } }],
           },
           fnAuthorizerOnUnauthenticatedRequestAllow: {
             handler: 'index.handler',
-            events: [{ alb: { ...baseEventConfig, priority: 2, authorizer: 'allow' } }],
+            events: [{ alb: { ...validBaseEventConfig, priority: 2, authorizer: 'allow' } }],
           },
           fnAuthorizerOnUnauthenticatedRequestAuthenticate: {
             handler: 'index.handler',
-            events: [{ alb: { ...baseEventConfig, priority: 3, authorizer: 'authenticate' } }],
+            events: [{ alb: { ...validBaseEventConfig, priority: 3, authorizer: 'authenticate' } }],
+          },
+          fnConditionsHostOnly: {
+            handler: 'index.handler',
+            events: [
+              { alb: { ...baseEventConfig, priority: 4, conditions: { host: 'example.com' } } },
+            ],
+          },
+          fnConditionsPathOnly: {
+            handler: 'index.handler',
+            events: [{ alb: { ...baseEventConfig, priority: 5, conditions: { path: '/' } } }],
           },
         },
       },
@@ -114,6 +132,64 @@ describe('lib/plugins/aws/package/compile/eents/alb/index.test.js', () => {
       expect(rule.Properties.Actions[0].AuthenticateCognitoConfig).to.deep.equal(
         baseAuthenticateCognitoConfig({ OnUnauthenticatedRequest: 'authenticate' })
       );
+    });
+  });
+
+  describe('alb rule conditions', () => {
+    it('should support rule without path', () => {
+      const albListenerRuleLogicalId = naming.getAlbListenerRuleLogicalId(
+        'fnConditionsHostOnly',
+        4
+      );
+      const rule = cfResources[albListenerRuleLogicalId];
+
+      expect(rule.Type).to.equal('AWS::ElasticLoadBalancingV2::ListenerRule');
+      expect(rule.Properties.Conditions).to.have.length(1);
+      expect(rule.Properties.Conditions[0].Field).to.equal('host-header');
+      expect(rule.Properties.Conditions[0].Values).to.have.length(1);
+      expect(rule.Properties.Conditions[0].Values[0]).to.equal('example.com');
+    });
+
+    it('should should support rule with path', () => {
+      const albListenerRuleLogicalId = naming.getAlbListenerRuleLogicalId(
+        'fnConditionsPathOnly',
+        5
+      );
+      const rule = cfResources[albListenerRuleLogicalId];
+
+      expect(rule.Type).to.equal('AWS::ElasticLoadBalancingV2::ListenerRule');
+      expect(rule.Properties.Conditions).to.have.length(1);
+      expect(rule.Properties.Conditions[0].Field).to.equal('path-pattern');
+      expect(rule.Properties.Conditions[0].Values).to.have.length(1);
+      expect(rule.Properties.Conditions[0].Values[0]).to.equal('/');
+    });
+
+    it('should fail validation if no conditions are set', async () => {
+      const runServerlessAction = () =>
+        runServerless({
+          fixture: 'function',
+          cliArgs: ['package'],
+          configExt: {
+            functions: {
+              fnConditionsHostOnly: {
+                handler: 'index.handler',
+                events: [
+                  {
+                    alb: {
+                      ...baseEventConfig,
+                      priority: 1,
+                      conditions: {},
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        });
+
+      await expect(runServerlessAction())
+        .to.eventually.be.rejectedWith(ServerlessError)
+        .and.have.property('code', 'ALB_NO_CONDITIONS');
     });
   });
 });
