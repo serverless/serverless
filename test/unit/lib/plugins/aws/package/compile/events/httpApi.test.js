f9c8677eccdfe14382c7e90079abce9f7bfed866
Santhos Baala, Ramalingam Santhanakrishnan <1725062+captainsano@users.noreply.github.com>
2020-12-23 11:32:19 +0100

    feat(AWS API Gateway): Add HttpApiId to httpApi CF Outputs (#8664)

--- test/unit/lib/plugins/aws/package/compile/events/httpApi.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/httpApi.test.js
@@ -58,7 +58,12 @@ describe('lib/plugins/aws/package/compile/events/httpApi.test.js', () => {
       expect(resource.Properties.AutoDeploy).to.equal(true);
     });
 
-    it('should configure output', () => {
+    it('should configure output for HttpApiId', () => {
+      const output = cfOutputs.HttpApiId;
+      expect(output).to.have.property('Value');
+    });
+
+    it('should configure output for HttpApiUrl', () => {
       const output = cfOutputs.HttpApiUrl;
       expect(output).to.have.property('Value');
     });
@@ -481,6 +486,7 @@ describe('lib/plugins/aws/package/compile/events/httpApi.test.js', () => {
         expect(cfResources).to.not.have.property(naming.getHttpApiStageLogicalId());
       });
       it('should not configure output', () => {
+        expect(cfOutputs).to.not.have.property('HttpApiId');
         expect(cfOutputs).to.not.have.property('HttpApiUrl');
       });
       it('should configure endpoint that attaches to external API', () => {
