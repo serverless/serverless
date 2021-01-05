9d783482895d82a1bfdb627c4cc0debb32123d56
Mariusz Nowak <medyk@medikoo.com>
2021-01-05 22:47:02 +0100

    refactor: Seclude `cli/resolve-local-serverless-path` util

--- test/mochaPatch.js
+++ test/mochaPatch.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const disableServerlessStatsRequests = require('@serverless/test/disable-serverless-stats-requests');
 const { _ensureArtifact } = require('../lib/utils/getEnsureArtifact');
+const resolveLocalServerless = require('../lib/cli/resolve-local-serverless-path');
 
 disableServerlessStatsRequests(path.resolve(__dirname, '..'));
 
@@ -18,6 +19,9 @@ runnerEmitter.on('runner', (runner) => {
   runner.on('suite end', (suite) => {
     if (!suite.parent || !suite.parent.root) return;
 
+    // Ensure to reset cache for local serverless installation resolution
+    // Leaking it across test files may introduce wrong assumptions when result is used for testing
+    resolveLocalServerless.clear();
     // Ensure to reset memoization on artifacts generation after each test file run.
     // Reason is that homedir is automatically cleaned for each test,
     // therefore eventually built custom resource file is also removed
