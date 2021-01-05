1af73bacdf01e5dc855da59387ab36085b2b78a1
Frédéric Barthelet <fredericb@theodo.fr>
2021-01-05 11:14:12 +0100

    refactor: Upgrade "ajv" to v7 and "ajv-keywords" to v4 (#8703)

--- test/unit/lib/classes/ConfigSchemaHandler/normalizeAjvErrors.test.js
+++ test/unit/lib/classes/ConfigSchemaHandler/normalizeAjvErrors.test.js
@@ -2,7 +2,7 @@
 
 const { expect } = require('chai');
 
-const Ajv = require('ajv');
+const Ajv = require('ajv').default;
 const memoize = require('memoizee');
 const normalizeAjvErrors = require('../../../../../lib/classes/ConfigSchemaHandler/normalizeAjvErrors');
 
@@ -185,7 +185,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report error for unrecognized deep level property', () =>
       expect(
         errors.some((error) => {
-          if (error.dataPath !== '.package') return false;
+          if (error.dataPath !== '/package') return false;
           if (error.keyword !== 'additionalProperties') return false;
           error.isExpected = true;
           return true;
@@ -194,7 +194,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report error for invalid function name', () =>
       expect(
         errors.some((error) => {
-          if (error.dataPath !== '.functions') return false;
+          if (error.dataPath !== '/functions') return false;
           if (error.keyword !== 'additionalProperties') return false;
           error.isExpected = true;
           return true;
@@ -203,7 +203,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report error for unrecognized event', () =>
       expect(
         errors.some((error) => {
-          if (error.dataPath !== ".functions['foo'].events[0]") return false;
+          if (error.dataPath !== '/functions/foo/events/0') return false;
           if (error.keyword !== 'anyOf') return false;
           error.isExpected = true;
           return true;
@@ -212,7 +212,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report error for unrecognized property at event type configuration level', () =>
       expect(
         errors.some((error) => {
-          if (error.dataPath !== ".functions['foo'].events[1]") return false;
+          if (error.dataPath !== '/functions/foo/events/1') return false;
           if (error.keyword !== 'additionalProperties') return false;
           error.isExpected = true;
           return true;
@@ -231,7 +231,7 @@ describe('#normalizeAjvErrors', () => {
         //         method: GET # Should be additionally indented
         expect(
           errors.some((error) => {
-            if (error.dataPath !== ".functions['foo'].events[2]") return false;
+            if (error.dataPath !== '/functions/foo/events/2') return false;
             if (error.keyword !== 'additionalProperties') return false;
             error.isExpected = true;
             return true;
@@ -244,7 +244,7 @@ describe('#normalizeAjvErrors', () => {
       () =>
         expect(
           errors.some((error) => {
-            if (error.dataPath !== ".functions['foo'].events[3].http") return false;
+            if (error.dataPath !== '/functions/foo/events/3/http') return false;
             if (error.keyword !== 'additionalProperties') return false;
             error.isExpected = true;
             return true;
@@ -257,7 +257,7 @@ describe('#normalizeAjvErrors', () => {
       () =>
         expect(
           errors.some((error) => {
-            if (error.dataPath !== ".functions['foo'].events[4].http") return false;
+            if (error.dataPath !== '/functions/foo/events/4/http') return false;
             if (error.keyword !== 'pattern') return false;
             error.isExpected = true;
             return true;
@@ -270,7 +270,7 @@ describe('#normalizeAjvErrors', () => {
       () =>
         expect(
           errors.some((error) => {
-            if (error.dataPath !== ".functions['foo'].events[5].http") return false;
+            if (error.dataPath !== '/functions/foo/events/5/http') return false;
             if (error.keyword !== 'required') return false;
             error.isExpected = true;
             return true;
@@ -283,7 +283,7 @@ describe('#normalizeAjvErrors', () => {
       () =>
         expect(
           errors.some((error) => {
-            if (error.dataPath !== '.provider.deploymentBucket.maxPreviousDeploymentArtifacts') {
+            if (error.dataPath !== '/provider/deploymentBucket/maxPreviousDeploymentArtifacts') {
               return false;
             }
             if (error.keyword !== 'type') return false;
@@ -298,7 +298,7 @@ describe('#normalizeAjvErrors', () => {
       () =>
         expect(
           errors.some((error) => {
-            if (error.dataPath !== '.custom.someCustom') {
+            if (error.dataPath !== '/custom/someCustom') {
               return false;
             }
             if (error.keyword !== 'anyOf') return false;
@@ -315,7 +315,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report "additionalProperties" error with meaningful message', () =>
       expect(
         errors.find((error) => {
-          if (error.dataPath !== '.package') return false;
+          if (error.dataPath !== '/package') return false;
           if (error.keyword !== 'additionalProperties') return false;
           return true;
         }).message
@@ -323,7 +323,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report invalid function name error with meaningful message', () =>
       expect(
         errors.find((error) => {
-          if (error.dataPath !== '.functions') return false;
+          if (error.dataPath !== '/functions') return false;
           if (error.keyword !== 'additionalProperties') return false;
           return true;
         }).message
@@ -331,7 +331,7 @@ describe('#normalizeAjvErrors', () => {
     it('should report unrecognized event error with a meaningful message', () =>
       expect(
         errors.find((error) => {
-          if (error.dataPath !== ".functions['foo'].events[0]") return false;
+          if (error.dataPath !== '/functions/foo/events/0') return false;
           if (error.keyword !== 'anyOf') return false;
           return true;
         }).message
