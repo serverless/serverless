33ba9cb1e63fef6ea19ed2e4511aed92735ef234
Lukasz Jendrysik <scadu9@gmail.com>
2021-01-06 14:37:06 +0100

    test(Packaging): Remove old servide-wide test

--- test/unit/lib/plugins/package/lib/packageService.test.js
+++ test/unit/lib/plugins/package/lib/packageService.test.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const _ = require('lodash');
-const BbPromise = require('bluebird');
 const path = require('path');
-const fs = require('fs');
-const fse = require('fs-extra');
 const chai = require('chai');
-const sinon = require('sinon');
-const Package = require('../../../../../../lib/plugins/package/package');
-const Serverless = require('../../../../../../lib/Serverless');
-const serverlessConfigFileUtils = require('../../../../../../lib/utils/getServerlessConfigFile');
-const { createTmpDir, listZipFiles, listFilePermissions } = require('../../../../../utils/fs');
+const { listZipFiles, listFilePermissions } = require('../../../../../utils/fs');
 const runServerless = require('../../../../../utils/run-serverless');
 const fixtures = require('../../../../../fixtures');
 
@@ -18,382 +10,6 @@ const fixtures = require('../../../../../fixtures');
 chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
 const { expect } = require('chai');
-
-describe('#packageService()', () => {
-  let serverless;
-  let packagePlugin;
-
-  beforeEach(() => {
-    serverless = new Serverless();
-    serverless.processedInput = { options: {} };
-    packagePlugin = new Package(serverless, {});
-    packagePlugin.serverless.cli = new serverless.classes.CLI();
-    packagePlugin.serverless.service.functions = {
-      first: {
-        handler: 'foo',
-      },
-    };
-    packagePlugin.serverless.service.provider = { runtime: 'nodejs:8.10' };
-  });
-
-  describe('#getIncludes()', () => {
-    it('should merge package includes', () => {
-      const packageIncludes = ['dir', 'file.js'];
-
-      serverless.service.package.include = packageIncludes;
-
-      const include = packagePlugin.getIncludes();
-      expect(include).to.deep.equal(['dir', 'file.js']);
-    });
-
-    it('should merge package and func includes', () => {
-      const funcIncludes = ['lib', 'other.js'];
-      const packageIncludes = ['dir', 'file.js'];
-
-      serverless.service.package.include = packageIncludes;
-
-      const include = packagePlugin.getIncludes(funcIncludes);
-      expect(include).to.deep.equal(['dir', 'file.js', 'lib', 'other.js']);
-    });
-  });
-
-  describe('#getExcludes()', () => {
-    const serverlessConfigFileName = 'serverless.xyz';
-
-    before(() => {
-      sinon
-        .stub(serverlessConfigFileUtils, 'getServerlessConfigFilePath')
-        .returns(BbPromise.resolve(`/path/to/${serverlessConfigFileName}`));
-    });
-
-    after(() => {
-      serverlessConfigFileUtils.getServerlessConfigFilePath.restore();
-    });
-
-    it('should exclude plugins localPath defaults', () => {
-      const localPath = './myplugins';
-      serverless.service.plugins = { localPath };
-
-      return expect(packagePlugin.getExcludes()).to.be.fulfilled.then((exclude) =>
-        expect(exclude).to.deep.equal(
-          _.union(packagePlugin.defaultExcludes, [serverlessConfigFileName], [localPath])
-        )
-      );
-    });
-
-    it('should merge defaults with plugin localPath and excludes', () => {
-      const localPath = './myplugins';
-      serverless.service.plugins = { localPath };
-
-      const packageExcludes = ['dir', 'file.js'];
-      serverless.service.package.exclude = packageExcludes;
-
-      return expect(packagePlugin.getExcludes()).to.be.fulfilled.then((exclude) =>
-        expect(exclude).to.deep.equal(
-          _.union(
-            packagePlugin.defaultExcludes,
-            [serverlessConfigFileName],
-            [localPath],
-            packageExcludes
-          )
-        )
-      );
-    });
-
-    it('should merge defaults with plugin localPath package and func excludes', () => {
-      const localPath = './myplugins';
-      serverless.service.plugins = { localPath };
-
-      const packageExcludes = ['dir', 'file.js'];
-      serverless.service.package.exclude = packageExcludes;
-
-      const funcExcludes = ['lib', 'other.js'];
-
-      return expect(packagePlugin.getExcludes(funcExcludes)).to.be.fulfilled.then((exclude) =>
-        expect(exclude).to.deep.equal(
-          _.union(
-            packagePlugin.defaultExcludes,
-            [serverlessConfigFileName],
-            [localPath],
-            packageExcludes,
-            funcExcludes
-          )
-        )
-      );
-    });
-  });
-
-  describe('#packageService()', () => {
-    it('should package functions individually', () => {
-      serverless.service.package.individually = true;
-      serverless.service.functions = {
-        'test-one': {
-          name: 'test-one',
-        },
-        'test-two': {
-          name: 'test-two',
-        },
-      };
-
-      const packageFunctionStub = sinon
-        .stub(packagePlugin, 'packageFunction')
-        .resolves((func) => func.name);
-
-      return expect(packagePlugin.packageService()).to.be.fulfilled.then(
-        () => expect(packageFunctionStub).to.be.calledTwice
-      );
-    });
-
-    it('should package single function individually', () => {
-      serverless.service.functions = {
-        'test-one': {
-          name: 'test-one',
-          package: {
-            individually: true,
-          },
-        },
-        'test-two': {
-          name: 'test-two',
-        },
-      };
-
-      const packageFunctionStub = sinon
-        .stub(packagePlugin, 'packageFunction')
-        .resolves((func) => func.name);
-      const packageAllStub = sinon.stub(packagePlugin, 'packageAll').resolves((func) => func.name);
-
-      return expect(packagePlugin.packageService()).to.be.fulfilled.then(() =>
-        BbPromise.join(
-          expect(packageFunctionStub).to.be.calledOnce,
-          expect(packageAllStub).to.be.calledOnce
-        )
-      );
-    });
-
-    it('should not package functions if package artifact specified', () => {
-      serverless.service.package.artifact = 'some/file.zip';
-
-      const packageAllStub = sinon.stub(packagePlugin, 'packageAll').resolves();
-
-      return expect(packagePlugin.packageService()).to.be.fulfilled.then(
-        () => expect(packageAllStub).to.not.be.called
-      );
-    });
-
-    it('should package functions individually if package artifact specified', () => {
-      serverless.service.package.artifact = 'some/file.zip';
-      serverless.service.package.individually = true;
-      serverless.service.functions = {
-        'test-one': {
-          name: 'test-one',
-        },
-        'test-two': {
-          name: 'test-two',
-        },
-      };
-
-      const packageFunctionStub = sinon
-        .stub(packagePlugin, 'packageFunction')
-        .resolves((func) => func.name);
-      const packageAllStub = sinon.stub(packagePlugin, 'packageAll').resolves((func) => func.name);
-
-      return expect(packagePlugin.packageService()).to.be.fulfilled.then(() =>
-        BbPromise.join(
-          expect(packageFunctionStub).to.be.calledTwice,
-          expect(packageAllStub).to.not.be.called
-        )
-      );
-    });
-
-    it('should package single functions individually if package artifact specified', () => {
-      serverless.service.package.artifact = 'some/file.zip';
-      serverless.service.functions = {
-        'test-one': {
-          name: 'test-one',
-          package: {
-            individually: true,
-          },
-        },
-        'test-two': {
-          name: 'test-two',
-        },
-      };
-
-      const packageFunctionStub = sinon
-        .stub(packagePlugin, 'packageFunction')
-        .resolves((func) => func.name);
-      const packageAllStub = sinon.stub(packagePlugin, 'packageAll').resolves((func) => func.name);
-
-      return expect(packagePlugin.packageService()).to.be.fulfilled.then(() =>
-        BbPromise.join(
-          expect(packageFunctionStub).to.be.calledOnce,
-          expect(packageAllStub).to.not.be.called
-        )
-      );
-    });
-  });
-
-  describe('#packageAll()', () => {
-    const exclude = ['test-exclude'];
-    const include = ['test-include'];
-    const files = [];
-    const artifactFilePath = '/some/fake/path/test-artifact.zip';
-    let getExcludesStub;
-    let getIncludesStub;
-    let resolveFilePathsFromPatternsStub;
-    let zipFilesStub;
-
-    beforeEach(() => {
-      getExcludesStub = sinon
-        .stub(packagePlugin, 'getExcludes')
-        .returns(BbPromise.resolve(exclude));
-      getIncludesStub = sinon.stub(packagePlugin, 'getIncludes').returns(include);
-      resolveFilePathsFromPatternsStub = sinon
-        .stub(packagePlugin, 'resolveFilePathsFromPatterns')
-        .returns(files);
-      zipFilesStub = sinon.stub(packagePlugin, 'zipFiles').resolves(artifactFilePath);
-    });
-
-    afterEach(() => {
-      packagePlugin.getExcludes.restore();
-      packagePlugin.getIncludes.restore();
-      packagePlugin.resolveFilePathsFromPatterns.restore();
-      packagePlugin.zipFiles.restore();
-    });
-
-    (process.platform === 'win32' ? it : it.skip)(
-      'should call zipService with settings & binaries to chmod on win32',
-      () => {
-        const servicePath = 'test';
-        const zipFileName = `${serverless.service.service}.zip`;
-
-        serverless.config.servicePath = servicePath;
-
-        return expect(packagePlugin.packageService()).to.be.fulfilled.then(() =>
-          BbPromise.all([
-            expect(getExcludesStub).to.be.calledOnce,
-            expect(getIncludesStub).to.be.calledOnce,
-            expect(resolveFilePathsFromPatternsStub).to.be.calledOnce,
-            expect(zipFilesStub).to.be.calledOnce,
-            expect(zipFilesStub).to.have.been.calledWithExactly(files, zipFileName),
-          ])
-        );
-      }
-    );
-  });
-
-  describe('#packageLayer()', () => {
-    const exclude = ['test-exclude'];
-    const include = ['test-include'];
-    const files = [];
-    const artifactFilePath = '/some/fake/path/test-artifact.zip';
-    let getExcludesStub;
-    let getIncludesStub;
-    let resolveFilePathsFromPatternsStub;
-    let zipFilesStub;
-
-    beforeEach(() => {
-      getExcludesStub = sinon
-        .stub(packagePlugin, 'getExcludes')
-        .returns(BbPromise.resolve(exclude));
-      getIncludesStub = sinon.stub(packagePlugin, 'getIncludes').returns(include);
-      resolveFilePathsFromPatternsStub = sinon
-        .stub(packagePlugin, 'resolveFilePathsFromPatterns')
-        .returns(files);
-      zipFilesStub = sinon.stub(packagePlugin, 'zipFiles').resolves(artifactFilePath);
-    });
-
-    afterEach(() => {
-      packagePlugin.getExcludes.restore();
-      packagePlugin.getIncludes.restore();
-      packagePlugin.resolveFilePathsFromPatterns.restore();
-      packagePlugin.zipFiles.restore();
-    });
-
-    it('should call zipService with settings', () => {
-      const servicePath = 'test';
-      const layerName = 'test-layer';
-
-      const zipFileName = 'test-layer.zip';
-
-      serverless.config.servicePath = servicePath;
-      serverless.service.layers = {};
-      serverless.service.layers[layerName] = { path: './foobar' };
-
-      return expect(packagePlugin.packageLayer(layerName))
-        .to.eventually.equal(artifactFilePath)
-        .then(() =>
-          BbPromise.all([
-            expect(getExcludesStub).to.be.calledOnce,
-            expect(getIncludesStub).to.be.calledOnce,
-            expect(resolveFilePathsFromPatternsStub).to.be.calledOnce,
-
-            expect(zipFilesStub).to.be.calledOnce,
-            expect(zipFilesStub).to.have.been.calledWithExactly(
-              files,
-              zipFileName,
-              path.resolve('./foobar')
-            ),
-          ])
-        );
-    });
-  });
-
-  describe('#resolveFilePathsFromPatterns()', () => {
-    // NOTE: the path.join in `beforeEach` will take care of OS
-    // independent file paths
-    const handlerFile = 'src/function/handler.js';
-    const utilsFile = 'src/utils/utils.js';
-    const dotsFile = 'src/dots/[...file].js';
-    let servicePath;
-
-    beforeEach(() => {
-      servicePath = createTmpDir();
-      fse.ensureFileSync(path.join(servicePath, handlerFile));
-      fse.ensureFileSync(path.join(servicePath, utilsFile));
-      fse.ensureFileSync(path.join(servicePath, dotsFile));
-    });
-
-    it('should exclude all and include function/handler.js', () => {
-      const params = {
-        exclude: ['**'],
-        include: [handlerFile],
-      };
-      serverless.config.servicePath = servicePath;
-
-      return expect(
-        packagePlugin.resolveFilePathsFromPatterns(params)
-      ).to.be.fulfilled.then((actual) => expect(actual).to.deep.equal([handlerFile]));
-    });
-
-    it('should include file specified with `!` in exclude params', () => {
-      const params = {
-        exclude: ['**', `!${utilsFile}`],
-        include: [handlerFile],
-      };
-      serverless.config.servicePath = servicePath;
-
-      return expect(
-        packagePlugin.resolveFilePathsFromPatterns(params)
-      ).to.be.fulfilled.then((actual) =>
-        expect(actual.sort()).to.deep.equal([handlerFile, utilsFile].sort())
-      );
-    });
-
-    it('should exclude file specified with `!` in include params', () => {
-      const params = {
-        exclude: [],
-        include: [`!${utilsFile}`],
-      };
-      const expected = [dotsFile, handlerFile];
-      serverless.config.servicePath = servicePath;
-
-      return expect(
-        packagePlugin.resolveFilePathsFromPatterns(params)
-      ).to.be.fulfilled.then((actual) => expect(actual.sort()).to.deep.equal(expected.sort()));
-    });
-  });
-});
 
 describe('lib/plugins/package/lib/packageService.test.js', () => {
   const mockedDescribeStacksResponse = {
@@ -589,19 +205,20 @@ describe('lib/plugins/package/lib/packageService.test.js', () => {
     });
 
     it('TODO: should ignore `package.artifact` if `package.individually`', () => {
-      // Replace
+      // expect(fnIndividualZippedFiles).to.not.include('artifact.zip');
       // https://github.com/serverless/serverless/blob/b12d565ea0ad588445fb120e049db157afc7bf37/test/unit/lib/plugins/package/lib/packageService.test.js#L237-L260
     });
 
     it('should support `package.individually`', () => {
       // Confirm on different artifacts on functions
-      //
+      expect(fnIndividualZippedFiles).to.include('dir1/subdir3/index.js');
+
       // Replaces
       // https://github.com/serverless/serverless/blob/b12d565ea0ad588445fb120e049db157afc7bf37/test/unit/lib/plugins/package/lib/packageService.test.js#L181-L199
     });
 
     it('should support `package.exclude`', () => {
-      expect(fnIndividualZippedFiles).to.include('dir1/subdir3/index.js');
+      expect(fnIndividualZippedFiles).to.not.include('dir1/subdir1/index.js');
     });
 
     it('should support `package.include`', () => {
@@ -612,7 +229,7 @@ describe('lib/plugins/package/lib/packageService.test.js', () => {
     });
   });
 
-  describe.skip('pre-prepared artifact', () => {
+  describe('pre-prepared artifact', () => {
     before(async () => {
       await runServerless({
         fixture: 'packaging',
