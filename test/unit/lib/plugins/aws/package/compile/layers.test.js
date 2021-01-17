13c554490542d6e2e303066b80c630aac96931bb
caiobessa <bessa@hotmail.com.br>
2021-01-17 02:06:16 +0000

    test(*): refactory test

--- test/unit/lib/plugins/aws/package/compile/layers.test.js
+++ test/unit/lib/plugins/aws/package/compile/layers.test.js
@@ -72,7 +72,7 @@ describe('lib/plugins/aws/package/compile/layers/index.test.js', () => {
     const s3Folder = service.package.artifactDirectoryName;
     const s3FileName = service.layers[resourceName].package.artifact.split(path.sep).pop();
 
-    expect(layerResource.Properties.Content.S3Key).to.be.equal(`${s3Folder}/${s3FileName}`);
+    expect(layerResource.Properties.Content.S3Key).to.equal(`${s3Folder}/${s3FileName}`);
   });
 
   it('should generate expected layer version resource', () => {
@@ -81,15 +81,15 @@ describe('lib/plugins/aws/package/compile/layers/index.test.js', () => {
     const s3Folder = service.package.artifactDirectoryName;
     const s3FileName = service.layers[resourceName].package.artifact.split(path.sep).pop();
 
-    expect(layerResource.Type).to.be.equals('AWS::Lambda::LayerVersion');
-    expect(layerResource.Properties.Content.S3Key).to.be.equal(`${s3Folder}/${s3FileName}`);
-    expect(layerResource.Properties.LayerName).to.be.equal('layer');
-    expect(layerResource.Properties.Content.S3Bucket.Ref).to.be.equal('ServerlessDeploymentBucket');
+    expect(layerResource.Type).to.equals('AWS::Lambda::LayerVersion');
+    expect(layerResource.Properties.Content.S3Key).to.equal(`${s3Folder}/${s3FileName}`);
+    expect(layerResource.Properties.LayerName).to.equal('layer');
+    expect(layerResource.Properties.Content.S3Bucket.Ref).to.equal('ServerlessDeploymentBucket');
 
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Description).to.be.equals(
+    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Description).to.equals(
       'Current Lambda layer version'
     );
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Value.Ref).to.be.equals('LayerLambdaLayer');
+    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Value.Ref).to.equals('LayerLambdaLayer');
   });
 
   describe('`layers[].retain` property', () => {
@@ -132,84 +132,44 @@ describe('lib/plugins/aws/package/compile/layers/index.test.js', () => {
   });
 
   it('should generate expected permissions resource', () => {
-    const layerResourceName = naming.getLambdaLayerLogicalId('LayerOne');
-    const layerOne = cfResources[layerResourceName];
-    const s3Folder = service.package.artifactDirectoryName;
-    const s3FileName = service.layers.layerOne.package.artifact.split(path.sep).pop();
-
-    expect(layerOne.Type).to.be.equals('AWS::Lambda::LayerVersion');
-    expect(layerOne.Properties.Content.S3Key).to.be.equals(`${s3Folder}/${s3FileName}`);
-    expect(layerOne.Properties.Content.S3Bucket.Ref).to.be.equals('ServerlessDeploymentBucket');
-    expect(layerOne.Properties.LayerName).to.be.equals('layerOne');
-
-    const description = 'Current Lambda layer version';
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Description).to.be.equals(description);
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Value.Ref).to.be.equals('LayerLambdaLayer');
-
     const layerNamePermission = naming.getLambdaLayerPermissionLogicalId('LayerOne', 'Wild');
     const layerPermission = cfResources[layerNamePermission];
 
-    expect(layerPermission.Type).to.be.equals('AWS::Lambda::LayerVersionPermission');
-    expect(layerPermission.Properties.Action).to.be.equals('lambda:GetLayerVersion');
-    expect(layerPermission.Properties.LayerVersionArn.Ref).to.be.equals('LayerOneLambdaLayer');
-    expect(layerPermission.Properties.Principal).to.be.equals('*');
+    expect(layerPermission.Type).to.equals('AWS::Lambda::LayerVersionPermission');
+    expect(layerPermission.Properties.Action).to.equals('lambda:GetLayerVersion');
+    expect(layerPermission.Properties.LayerVersionArn.Ref).to.equals('LayerOneLambdaLayer');
+    expect(layerPermission.Properties.Principal).to.equals('*');
   });
 
   it('should support `layers[].allowedAccounts`', () => {
-    const layerResourceName = naming.getLambdaLayerLogicalId('LayerTwo');
-    const layerTwo = cfResources[layerResourceName];
-    const s3Folder = service.package.artifactDirectoryName;
-    const s3FileName = service.layers.layerTwo.package.artifact.split(path.sep).pop();
-
-    expect(layerTwo.Type).to.be.equals('AWS::Lambda::LayerVersion');
-    expect(layerTwo.Properties.Content.S3Key).to.be.equals(`${s3Folder}/${s3FileName}`);
-    expect(layerTwo.Properties.Content.S3Bucket.Ref).to.be.equals('ServerlessDeploymentBucket');
-    expect(layerTwo.Properties.LayerName).to.be.equals('layerTwo');
-
-    const description = 'Current Lambda layer version';
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Description).to.be.equals(description);
-    expect(cfOutputs.LayerLambdaLayerQualifiedArn.Value.Ref).to.be.equals('LayerLambdaLayer');
-
     const layerNamePermissionFirstUser = naming.getLambdaLayerPermissionLogicalId(
       'layerTwo',
       '123456789012'
     );
     const layerPermissionFirstUser = cfResources[layerNamePermissionFirstUser];
-
-    expect(layerPermissionFirstUser.Type).to.be.equals('AWS::Lambda::LayerVersionPermission');
-    expect(layerPermissionFirstUser.Properties.Action).to.be.equals('lambda:GetLayerVersion');
-    expect(layerPermissionFirstUser.Properties.LayerVersionArn.Ref).to.be.equals(
-      'LayerTwoLambdaLayer'
-    );
-    expect(layerPermissionFirstUser.Properties.Principal).to.be.equals('123456789012');
+    expect(layerPermissionFirstUser.Properties.Principal).to.equals('123456789012');
 
     const layerNamePermissionUserSecond = naming.getLambdaLayerPermissionLogicalId(
       'layerTwo',
       '123456789013'
     );
     const layerPermissionSecondUser = cfResources[layerNamePermissionUserSecond];
-
-    expect(layerPermissionSecondUser.Type).to.be.equals('AWS::Lambda::LayerVersionPermission');
-    expect(layerPermissionSecondUser.Properties.Action).to.be.equals('lambda:GetLayerVersion');
-    expect(layerPermissionSecondUser.Properties.LayerVersionArn.Ref).to.be.equals(
-      'LayerTwoLambdaLayer'
-    );
-    expect(layerPermissionSecondUser.Properties.Principal).to.be.equals('123456789013');
+    expect(layerPermissionSecondUser.Properties.Principal).to.equals('123456789013');
   });
 
   it('should support `layers[].description`', () => {
     const layerResourceName = naming.getLambdaLayerLogicalId('LayerTwo');
     const layerOne = cfResources[layerResourceName];
 
-    expect(layerOne.Type).to.be.equals('AWS::Lambda::LayerVersion');
-    expect(layerOne.Properties.Description).to.be.equals('Layer two example');
+    expect(layerOne.Type).to.equals('AWS::Lambda::LayerVersion');
+    expect(layerOne.Properties.Description).to.equals('Layer two example');
   });
 
   it('should support `layers[].compatibleRuntimes`', () => {
     const layerResourceName = naming.getLambdaLayerLogicalId('LayerTwo');
     const layerOne = cfResources[layerResourceName];
 
-    expect(layerOne.Type).to.be.equals('AWS::Lambda::LayerVersion');
+    expect(layerOne.Type).to.equals('AWS::Lambda::LayerVersion');
     expect(layerOne.Properties.CompatibleRuntimes).to.be.deep.equals(['nodejs12.x']);
   });
 
@@ -217,7 +177,7 @@ describe('lib/plugins/aws/package/compile/layers/index.test.js', () => {
     const layerResourceName = naming.getLambdaLayerLogicalId('LayerTwo');
     const layerOne = cfResources[layerResourceName];
 
-    expect(layerOne.Type).to.be.equals('AWS::Lambda::LayerVersion');
+    expect(layerOne.Type).to.equals('AWS::Lambda::LayerVersion');
     expect(layerOne.Properties.LicenseInfo).to.be.deep.equals('GPL');
   });
 });
