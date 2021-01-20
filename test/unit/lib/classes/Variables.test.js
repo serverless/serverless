9a3b449dac7e5ede077b8fca862214e244a34f15
sdas13 <s.das001@yahoo.com>
2021-01-20 23:42:20 +0530

    revert fse.existsSync methods

--- test/unit/lib/classes/Variables.test.js
+++ test/unit/lib/classes/Variables.test.js
@@ -14,7 +14,6 @@ const _ = require('lodash');
 const overrideEnv = require('process-utils/override-env');
 
 const AwsProvider = require('../../../../lib/plugins/aws/provider');
-const fs = require('fs');
 const fse = require('fs-extra');
 const Serverless = require('../../../../lib/Serverless');
 const slsError = require('../../../../lib/classes/Error');
@@ -1849,7 +1848,7 @@ module.exports = {
       const tmpDirPath = getTmpDirPath();
       serverless.config.update({ servicePath: tmpDirPath });
       const realpathSync = sinon.spy(fse, 'realpathSync');
-      const existsSync = sinon.spy(fs, 'existsSync');
+      const existsSync = sinon.spy(fse, 'existsSync');
       return serverless.variables
         .getValueFromFile('file(./non-existing.yml)')
         .should.be.fulfilled.then((valueToPopulate) => {
