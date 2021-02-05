4f3df26faf21e88fdae6b8d1f26080c6507d3565
Frédéric Barthelet <fredericb@theodo.fr>
2021-02-05 13:30:03 +0100

    Update nodejs layer to 14 and ES output to 2020

--- lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
+++ lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
@@ -14,7 +14,7 @@ const serverlessConfiguration: AWS = {
   plugins: ['serverless-webpack'],
   provider: {
     name: 'aws',
-    runtime: 'nodejs12.x',
+    runtime: 'nodejs14.x',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
