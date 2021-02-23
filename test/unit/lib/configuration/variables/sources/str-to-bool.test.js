4d134d828b4bf78c1c128f6db19d835163f37da1
Mariusz Nowak <medyk@medikoo.com>
2021-02-23 10:16:17 +0100

    feat(Variables): "strToBool" source configuration for new resolver

---
+++ test/unit/lib/configuration/variables/sources/str-to-bool.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const resolveMeta = require('../../../../../../lib/configuration/variables/resolve-meta');
+const resolve = require('../../../../../../lib/configuration/variables/resolve');
+const strToBoolSource = require('../../../../../../lib/configuration/variables/sources/str-to-bool');
+
+describe('test/unit/lib/configuration/variables/sources/str-to-bool.test.js', () => {
+  let configuration;
+  let variablesMeta;
+  before(async () => {
+    configuration = {
+      truthy: "${strToBool('true')}",
+      falsy: "${strToBool('false')}",
+      noParam: '${strToBool:}',
+      invalid: '${strToBool(foo)}',
+    };
+    variablesMeta = resolveMeta(configuration);
+    await resolve({
+      servicePath: process.cwd(),
+      configuration,
+      variablesMeta,
+      sources: { strToBool: strToBoolSource },
+      options: {},
+    });
+  });
+
+  it('should resolve truthy input', () => expect(configuration.truthy).to.equal(true));
+  it('should resolve falsy input', () => expect(configuration.falsy).to.equal(false));
+
+  it('should report with an error missing input', () =>
+    expect(variablesMeta.get('noParam').error.code).to.equal('VARIABLE_RESOLUTION_ERROR'));
+
+  it('should report with an error a unexpected string input', () =>
+    expect(variablesMeta.get('invalid').error.code).to.equal('VARIABLE_RESOLUTION_ERROR'));
+});
