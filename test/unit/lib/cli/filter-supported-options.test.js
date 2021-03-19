13610cf0f0b89dc3061ae10254df291fc38c5e57
Mariusz Nowak <medyk@medikoo.com>
2021-03-19 15:23:16 +0100

    refactor(Variables): Recognize only defined CLI options in resolver

---
+++ test/unit/lib/cli/filter-supported-options.test.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const { expect } = require('chai');
+
+const filterSupportedOptions = require('../../../../lib/cli/filter-supported-options');
+
+describe('test/unit/lib/cli/filter-supported-options.test.js', () => {
+  it('should recognize just command schema options if passsed', () => {
+    expect(
+      filterSupportedOptions(
+        {
+          supString: 'string',
+          supBool: false,
+          supMultiple: ['multiple'],
+          unSupString: 'string',
+          unSupBool: false,
+          unSupMultiple: ['multiple'],
+        },
+        {
+          commandSchema: {
+            options: {
+              supString: {},
+              supBool: {},
+              supMultiple: {},
+              extra: {},
+            },
+          },
+        }
+      )
+    ).to.deep.equal({
+      supString: 'string',
+      supBool: false,
+      supMultiple: ['multiple'],
+      extra: null,
+    });
+  });
+
+  it('should recognize just AWS service options when no command schema and AWS provider', () => {
+    expect(
+      filterSupportedOptions(
+        {
+          stage: 'marko',
+          region: 'elo',
+          unSupString: 'string',
+          unSupBool: false,
+          unSupMultiple: ['multiple'],
+        },
+        { providerName: 'aws' }
+      )
+    ).to.deep.equal({
+      'region': 'elo',
+      'help': null,
+      'version': null,
+      'config': null,
+      'stage': 'marko',
+      'app': null,
+      'org': null,
+      'use-local-credentials': null,
+    });
+  });
+
+  it('should recognize just any service options when no command schema and no AWS provider', () => {
+    expect(
+      filterSupportedOptions(
+        {
+          stage: 'marko',
+          region: 'elo',
+          unSupString: 'string',
+          unSupBool: false,
+          unSupMultiple: ['multiple'],
+        },
+        {}
+      )
+    ).to.deep.equal({
+      help: null,
+      version: null,
+      config: null,
+      stage: 'marko',
+    });
+  });
+});
