6c6ada93e47e994dc5775a65542bedf69ec6d412
Mariusz Nowak <medyk@medikoo.com>
2021-03-19 15:16:25 +0100

    fix(Variables): Fix resolution of "false" CLI params

--- test/unit/lib/configuration/variables/sources/opt.test.js
+++ test/unit/lib/configuration/variables/sources/opt.test.js
@@ -13,6 +13,7 @@ describe('test/unit/lib/configuration/variables/sources/opt.test.js', () => {
   before(async () => {
     configuration = {
       opt: '${opt:foobar}',
+      optBool: '${opt:bool}',
       optMissing: "${opt:missing, 'fallback'}",
       noAddress: '${opt:}',
       nonStringAddress: '${opt:${self:someObject}}',
@@ -24,17 +25,18 @@ describe('test/unit/lib/configuration/variables/sources/opt.test.js', () => {
       configuration,
       variablesMeta,
       sources: { opt: optSource, self: selfSource },
-      options: { foobar: 'elo' },
+      options: { foobar: 'elo', bool: false },
       fulfilledSources: new Set(['opt']),
     });
   });
 
-  it('should resolve option', () => expect(configuration.opt).to.equal('elo'));
+  it('should resolve string option', () => expect(configuration.opt).to.equal('elo'));
+  it('should resolve bool option', () => expect(configuration.optBool).to.equal(false));
   it('should resolve null on missing option', () =>
     expect(configuration.optMissing).to.equal('fallback'));
 
   it('should expose all options when no address is providerd', () =>
-    expect(configuration.noAddress).to.deep.equal({ foobar: 'elo' }));
+    expect(configuration.noAddress).to.deep.equal({ foobar: 'elo', bool: false }));
 
   it('should report with an error a non-string address argument', () =>
     expect(variablesMeta.get('nonStringAddress').error.code).to.equal('VARIABLE_RESOLUTION_ERROR'));
