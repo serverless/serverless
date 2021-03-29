ee7687672557125b68a300f0cb1f7d8ec1785ec4
Mariusz Nowak <medyk@medikoo.com>
2021-03-29 17:57:17 +0200

    feat(Variables): Support source extensions from plugins for new resolver

--- scripts/serverless.js
+++ scripts/serverless.js
@@ -445,6 +445,63 @@ const processSpanPromise = (async () => {
           resolverConfiguration.fulfilledSources.add('cf').add('s3').add('ssm');
         }
 
+        const ensurePlainFunction = require('type/plain-function/ensure');
+        const ensurePlainObject = require('type/plain-object/ensure');
+
+        for (const externalPlugin of serverless.pluginManager.externalPlugins) {
+          const pluginName = externalPlugin.constructor.name;
+          if (externalPlugin.configurationVariablesSources != null) {
+            ensurePlainObject(externalPlugin.configurationVariablesSources, {
+              errorMessage:
+                'Invalid "configurationVariablesSources" ' +
+                `configuration on "${pluginName}", expected object, got: %v"`,
+              Error: ServerlessError,
+            });
+
+            for (const [sourceName, sourceConfig] of Object.entries(
+              externalPlugin.configurationVariablesSources
+            )) {
+              if (resolverConfiguration.sources[sourceName]) {
+                throw new ServerlessError(
+                  `Cannot add "${sourceName}" configuration variable source ` +
+                    `(through "${pluginName}" plugin) as resolution rules ` +
+                    'for this source name are already configured'
+                );
+              }
+              ensurePlainFunction(
+                ensurePlainObject(sourceConfig, {
+                  errorMessage:
+                    `Invalid "configurationVariablesSources.${sourceName}" ` +
+                    `configuration on "${pluginName}", expected object, got: %v"`,
+                  Error: ServerlessError,
+                }).resolve,
+                {
+                  errorMessage:
+                    `Invalid "configurationVariablesSources.${sourceName}.resolve" ` +
+                    `value on "${pluginName}", expected function, got: %v"`,
+                  Error: ServerlessError,
+                }
+              );
+
+              resolverConfiguration.sources[sourceName] = sourceConfig;
+              resolverConfiguration.fulfilledSources.add(sourceName);
+            }
+          } else if (
+            externalPlugin.variableResolvers &&
+            configuration.variablesResolutionMode < 20210326
+          ) {
+            logDeprecation(
+              'NEW_VARIABLES_RESOLVER',
+              `Plugin "${pluginName}" attempts to extend old variables resolver. ` +
+                'Ensure to rely on latest version of a plugin and if this warning is ' +
+                'still displayed please report the problem at plugin issue tracker' +
+                'Starting with next major release, ' +
+                'old variables resolver will not be supported.\n',
+              { serviceConfig: configuration }
+            );
+          }
+        }
+
         await resolveVariables(resolverConfiguration);
         if (!variablesMeta.size) return;
         if (
