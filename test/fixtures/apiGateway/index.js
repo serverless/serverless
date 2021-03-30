d6429b9417b0e6cb767b1851fe7af77ae9398d19
Piotr Grzesik <pj.grzesik@gmail.com>
2021-03-30 11:48:17 +0200

    test: Separate external API integration tests for API Gateway

--- test/fixtures/apiGateway/index.js
+++ test/fixtures/apiGateway/index.js
@@ -13,3 +13,13 @@ module.exports.handler = (event, context, callback) => {
   if (event.path === '/bar/timeout') setTimeout(resolve, 2000);
   else resolve();
 };
+
+module.exports.minimal = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Hello from API Gateway! - (minimal)',
+      event,
+    }),
+  };
+};
