efd32d4725cd36e5365aa09f50b75d9ca241d26e
Nicholas Wehr <33910651+wwwehr@users.noreply.github.com>
2021-04-07 01:33:08 -0600

    feat(AWS Lambda): Add support for `buildArgs` to `images` (#9198)

--- test/unit/lib/plugins/aws/provider.test.js
+++ test/unit/lib/plugins/aws/provider.test.js
@@ -1444,6 +1444,67 @@ describe('test/unit/lib/plugins/aws/provider.test.js', () => {
         ]);
       });
 
+      it('should work correctly when image is defined with `buildArgs` set', async () => {
+        const awsRequestStubMap = {
+          ...baseAwsRequestStubMap,
+          ECR: {
+            ...baseAwsRequestStubMap.ECR,
+            describeRepositories: describeRepositoriesStub.resolves({
+              repositories: [{ repositoryUri }],
+            }),
+            createRepository: createRepositoryStub,
+          },
+        };
+        const {
+          awsNaming,
+          cfTemplate,
+          fixtureData: { servicePath },
+        } = await runServerless({
+          fixture: 'ecr',
+          cliArgs: ['package'],
+          awsRequestStubMap,
+          modulesCacheStub,
+          configExt: {
+            provider: {
+              ecr: {
+                images: {
+                  baseimage: {
+                    path: './',
+                    file: 'Dockerfile.dev',
+                    buildArgs: {
+                      TESTKEY: 'TESTVAL',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        const functionCfLogicalId = awsNaming.getLambdaLogicalId('foo');
+        const functionCfConfig = cfTemplate.Resources[functionCfLogicalId].Properties;
+        const versionCfConfig = Object.values(cfTemplate.Resources).find(
+          (resource) =>
+            resource.Type === 'AWS::Lambda::Version' &&
+            resource.Properties.FunctionName.Ref === functionCfLogicalId
+        ).Properties;
+
+        expect(functionCfConfig.Code.ImageUri).to.deep.equal(`${repositoryUri}@sha256:${imageSha}`);
+        expect(versionCfConfig.CodeSha256).to.equal(imageSha);
+        expect(describeRepositoriesStub).to.be.calledOnce;
+        expect(createRepositoryStub.notCalled).to.be.true;
+        expect(spawnExtStub).to.be.calledWith('docker', [
+          'build',
+          '-t',
+          `${awsNaming.getEcrRepositoryName()}:baseimage`,
+          '-f',
+          path.join(servicePath, 'Dockerfile.dev'),
+          '--build-arg',
+          'TESTKEY=TESTVAL',
+          './',
+        ]);
+      });
+
       it('should work correctly when `functions[].image` is defined with explicit name', async () => {
         const awsRequestStubMap = {
           ...baseAwsRequestStubMap,
