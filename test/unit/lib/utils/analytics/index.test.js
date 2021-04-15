8c6be158d76a3fb02827ddc77e2a89118b15a5c4
Piotr Grzesik <pj.grzesik@gmail.com>
2021-04-15 12:42:22 +0200

    refactor: Use `async/await` in `analytics` test

--- test/unit/lib/utils/analytics/index.test.js
+++ test/unit/lib/utils/analytics/index.test.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const path = require('path');
-const BbPromise = require('bluebird');
 const fse = require('fs-extra');
 const proxyquire = require('proxyquire');
 const { expect } = require('chai');
 const { v1: uuid } = require('uuid');
+const wait = require('timers-ext/promise/sleep');
 const cacheDirPath = require('../../../../../lib/utils/analytics/cache-path');
 
 const analyticsUrl = 'https://..';
@@ -17,14 +17,12 @@ describe('analytics', () => {
   let expectedState = 'success';
   let usedUrl;
   let usedOptions;
-  let pendingRequests = 0;
-  let concurrentRequestsMax = 0;
 
-  const sendReport = () => {
+  const sendReport = async () => {
     return report({});
   };
 
-  const cacheEvent = (timestamp = Date.now()) => {
+  const cacheEvent = async (timestamp = Date.now()) => {
     fse.writeJson(path.join(cacheDirPath, uuid()), { payload: {}, timestamp });
   };
 
@@ -33,64 +31,55 @@ describe('analytics', () => {
     ({ report, sendPending } = proxyquire('../../../../../lib/utils/analytics/index.js', {
       '@serverless/utils/analytics-and-notfications-url': analyticsUrl,
       './areDisabled': false,
-      'node-fetch': (url, options) => {
+      'node-fetch': async (url, options) => {
         usedUrl = url;
         usedOptions = options;
-        ++pendingRequests;
-        if (pendingRequests > concurrentRequestsMax) concurrentRequestsMax = pendingRequests;
-        return new BbPromise((resolve, reject) => {
-          setTimeout(() => {
-            switch (expectedState) {
-              case 'success':
-                return resolve({ status: 200, json: () => Promise.resolve([]) });
-              case 'networkError':
-                return reject(Object.assign(new Error('Network error'), { code: 'NETWORK_ERROR' }));
-              case 'responseBodyError':
-                return resolve({
-                  status: 200,
-                  json: () =>
-                    Promise.reject(
-                      Object.assign(new Error('Response body error'), {
-                        code: 'RESPONSE_BODY_ERROR',
-                      })
-                    ),
+        await wait(500);
+        switch (expectedState) {
+          case 'success':
+            return { status: 200, json: async () => [] };
+          case 'networkError':
+            throw Object.assign(new Error('Network error'), { code: 'NETWORK_ERROR' });
+          case 'responseBodyError':
+            return {
+              status: 200,
+              json: async () => {
+                throw Object.assign(new Error('Response body error'), {
+                  code: 'RESPONSE_BODY_ERROR',
                 });
-              default:
-                throw new Error(`Unexpected state: ${expectedState}`);
-            }
-          }, 500);
-        }).finally(() => --pendingRequests);
+              },
+            };
+          default:
+            throw new Error(`Unexpected state: ${expectedState}`);
+        }
       },
     }));
   });
 
-  it('Should ignore missing cacheDirPath', () =>
-    sendPending().then((sendPendingResult) => {
-      expect(sendPendingResult).to.be.null;
-      return sendReport().then(() => {
-        expect(usedUrl).to.equal(analyticsUrl);
-        return fse.readdir(cacheDirPath).then((dirFilenames) => {
-          expect(dirFilenames.filter(isFilename).length).to.equal(0);
-        });
-      });
-    }));
+  it('Should ignore missing cacheDirPath', async () => {
+    const sendPendingResult = await sendPending();
+    expect(sendPendingResult).to.be.null;
+    await sendReport();
+    expect(usedUrl).to.equal(analyticsUrl);
+    const dirFilenames = await fse.readdir(cacheDirPath);
+    expect(dirFilenames.filter(isFilename).length).to.equal(0);
+    expect(JSON.parse(usedOptions.body)).to.have.lengthOf(1);
+  });
 
-  it('Should cache failed requests and rerun then with sendPending', () => {
+  it('Should cache failed requests and rerun then with sendPending', async () => {
     expectedState = 'networkError';
-    return sendReport()
-      .then(() => {
-        expect(usedUrl).to.equal(analyticsUrl);
-        return fse.readdir(cacheDirPath);
-      })
-      .then((dirFilenames) => {
-        expect(dirFilenames.filter(isFilename).length).to.equal(1);
-        expectedState = 'success';
-        return sendPending();
-      })
-      .then(() => fse.readdir(cacheDirPath))
-      .then((dirFilenames) => {
-        expect(dirFilenames.filter(isFilename).length).to.equal(0);
-      });
+    await sendReport();
+    expect(usedUrl).to.equal(analyticsUrl);
+    const dirFilenames = await fse.readdir(cacheDirPath);
+    expect(dirFilenames.filter(isFilename).length).to.equal(1);
+
+    expectedState = 'success';
+    await sendPending();
+    const dirFilenamesAfterSend = await fse.readdir(cacheDirPath);
+    expect(dirFilenamesAfterSend.filter(isFilename).length).to.equal(0);
+
+    // Check that one event was send with request
+    expect(JSON.parse(usedOptions.body)).to.have.lengthOf(1);
   });
 
   it('Should ditch stale events at sendPending', async () => {
@@ -105,14 +94,10 @@ describe('analytics', () => {
     expect(JSON.parse(usedOptions.body)).to.have.lengthOf(2);
   });
 
-  it('Should ignore body procesing error', () => {
+  it('Should ignore body procesing error', async () => {
     expectedState = 'responseBodyError';
-    return sendReport()
-      .then(() => {
-        return fse.readdir(cacheDirPath);
-      })
-      .then((dirFilenames) => {
-        expect(dirFilenames.filter(isFilename).length).to.equal(0);
-      });
+    await sendReport();
+    const dirFilenames = await fse.readdir(cacheDirPath);
+    expect(dirFilenames.filter(isFilename).length).to.equal(0);
   });
 });
