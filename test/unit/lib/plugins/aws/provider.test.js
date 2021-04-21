80eb46c8d310ec1f9f712b6324279485cb2cf908
AlinoeDoctari <75314813+AlinoeDoctari@users.noreply.github.com>
2021-04-21 10:15:39 +0200

    test: Refactor base `aws` provider credentials test (#9328)

--- test/unit/lib/plugins/aws/provider.test.js
+++ test/unit/lib/plugins/aws/provider.test.js
@@ -37,12 +37,6 @@ describe('AwsProvider', () => {
   });
   afterEach(() => restoreEnv());
 
-  describe('#getProviderName()', () => {
-    it('should return the provider name', () => {
-      expect(AwsProvider.getProviderName()).to.equal('aws');
-    });
-  });
-
   describe('#constructor()', () => {
     it('should set Serverless instance', () => {
       expect(typeof awsProvider.serverless).to.not.equal('undefined');
@@ -335,6 +329,66 @@ describe('AwsProvider', () => {
 });
 
 describe('test/unit/lib/plugins/aws/provider.test.js', () => {
+  describe('#getProviderName and #sessionCache', () => {
+    let sls;
+    const expectedToken = '123';
+
+    before(async () => {
+      // Fake service that update credentials
+      class FakeCloudFormation {
+        constructor(credentials) {
+          this.credentials = credentials;
+          this.credentials.credentials.sessionToken = expectedToken;
+        }
+        describeStacks() {
+          return { promise: async () => {} };
+        }
+      }
+      // Stub functions for the credentials creation in the provider
+      class SharedIniFileCredentials {
+        constructor() {
+          this.sessionToken = 'abc';
+          this.accessKeyId = 'keyId';
+          this.secretAccessKey = 'secret';
+        }
+      }
+      class EnvironmentCredentials {
+        constructor() {
+          this.sessionToken = 'env';
+          this.accessKeyId = 'keyId';
+          this.secretAccessKey = 'secret';
+        }
+      }
+      const modulesCacheStub = {
+        'aws-sdk': {
+          SharedIniFileCredentials,
+          EnvironmentCredentials,
+          CloudFormation: FakeCloudFormation,
+        },
+      };
+      const { serverless } = await runServerless({
+        fixture: 'aws',
+        command: 'print',
+        modulesCacheStub,
+      });
+      sls = serverless;
+    });
+
+    it('`AwsProvider.getProviderName()` should resolve provider name', () => {
+      expect(AwsProvider.getProviderName()).to.equal('aws');
+    });
+
+    it('should retain sessionToken eventually updated internally by SDK', async () => {
+      expect(sls.getProvider('aws').getCredentials().credentials.sessionToken).not.to.equal(
+        expectedToken
+      );
+      await sls.getProvider('aws').request('CloudFormation', 'describeStacks');
+      expect(sls.getProvider('aws').getCredentials().credentials.sessionToken).to.equal(
+        expectedToken
+      );
+    });
+  });
+
   describe('#getCredentials()', () => {
     before(async () => {
       // create default aws credentials file in before so that grouped run can use it
