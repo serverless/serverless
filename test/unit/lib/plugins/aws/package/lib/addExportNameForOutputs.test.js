352e988fa2dd24e96c456f9ddc8dfc58ad3f8069
Joseph Cha <josephcha4@gmail.com>
2021-04-22 11:08:20 +1000

    test: update test cases

--- test/unit/lib/plugins/aws/package/lib/addExportNameForOutputs.test.js
+++ test/unit/lib/plugins/aws/package/lib/addExportNameForOutputs.test.js
@@ -7,36 +7,44 @@ const expect = chai.expect;
 
 describe('test/unit/lib/plugins/aws/package/lib/addExportNameForOutputs.test.js', () => {
   let outputs;
-
+  let service;
   before(async () => {
     const result = await runServerless({
-      fixture: 'aws',
+      fixture: 'apiGateway',
       command: 'package',
       configExt: {
-        service: 'myService',
         resources: {
           Outputs: {
-            ServerlessDeploymentBucketName: {
-              Value: 'ServerlessDeploymentBucketName',
-            },
-            ServiceEndpoint: {
-              Value: 'example url',
-            },
-            CustomS3BucketName: {
-              Value: 'example bucket',
+            CustomOutput: {
+              Export: {
+                Name: 'someExportName',
+              },
             },
           },
         },
       },
     });
     outputs = result.cfTemplate.Outputs;
+    service = result.serverless.service.service;
   });
 
-  it('Should add Export.Name for each logical id in the Outputs section', async () => {
-    expect(outputs.ServiceEndpoint.Export.Name).to.equal('sls-myService-dev-ServiceEndpoint');
+  it('Should add Export.Name for each internally generated logical id in the Outputs section', () => {
+    expect(outputs.ServiceEndpoint.Export.Name).to.equal(`sls-${service}-dev-ServiceEndpoint`);
     expect(outputs.ServerlessDeploymentBucketName.Export.Name).to.equal(
-      'sls-myService-dev-ServerlessDeploymentBucketName'
+      `sls-${service}-dev-ServerlessDeploymentBucketName`
     );
-    expect(outputs.CustomS3BucketName.Export.Name).to.equal('sls-myService-dev-CustomS3BucketName');
+    expect(outputs.FooLambdaFunctionQualifiedArn.Export.Name).to.equal(
+      `sls-${service}-dev-FooLambdaFunctionQualifiedArn`
+    );
+    expect(outputs.MinimalLambdaFunctionQualifiedArn.Export.Name).to.equal(
+      `sls-${service}-dev-MinimalLambdaFunctionQualifiedArn`
+    );
+    expect(outputs.OtherLambdaFunctionQualifiedArn.Export.Name).to.equal(
+      `sls-${service}-dev-OtherLambdaFunctionQualifiedArn`
+    );
+  });
+
+  it('Should not override Export.Name for user configured Outputs', () => {
+    expect(outputs.CustomOutput.Export.Name).to.equal('someExportName');
   });
 });
