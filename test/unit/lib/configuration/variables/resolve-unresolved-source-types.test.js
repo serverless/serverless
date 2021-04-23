53a7872f78f51938b409925e052d34f2dc85abbd
Mariusz Nowak <medyk@medikoo.com>
2021-04-23 09:53:00 +0200

    fix(Variables): Fix unresolved sources resolver

--- test/unit/lib/configuration/variables/resolve-unresolved-source-types.test.js
+++ test/unit/lib/configuration/variables/resolve-unresolved-source-types.test.js
@@ -3,44 +3,79 @@
 const { expect } = require('chai');
 
 const resolveMeta = require('../../../../../lib/configuration/variables/resolve-meta');
+const resolve = require('../../../../../lib/configuration/variables/resolve');
 const resolveUnresolvedSourceTypes = require('../../../../../lib/configuration/variables/resolve-unresolved-source-types');
 
 describe('test/unit/lib/configuration/variables/resolve-unresolved-source-types.test.js', () => {
   const configuration = {
-    foo: {
-      params: '${sourceParam(param1, param2)}',
-      varParam: '${sourceParam(${sourceDirect:})}',
+    resolved: 'foo${recognized:}',
+    unrecognized: 'foo${unrecognized:}',
+    unrecognizedInParens: 'foo${recognized(${unrecognized:}, ${unrecognized2:})}',
+    unrecognizedInAddress: 'foo${recognized:${unrecognized:}}',
+    unrecognizedInParensAndAddress:
+      'foo${recognized(${unrecognized:}, ${unrecognized2:}):${unrecognized3:}}',
+    unrecognizedFallback: 'foo${recognized:, unrecognized:}',
+    otherUnrecognizedFallback: 'foo${unrecognized:, unrecognized4:}',
+    deep: {
+      resolved: 'foo${recognized:}',
+      unrecognized: 'foo${unrecognized:}',
+      unrecognizedInParens: 'foo${recognized(${unrecognized:}, ${unrecognized2:})}',
+      unrecognizedInAddress: 'foo${recognized:${unrecognized:}}',
+      unrecognizedInParensAndAddress:
+        'foo${recognized(${unrecognized:}, ${unrecognized2:}):${unrecognized3:}}',
+      unrecognizedFallback: 'foo${recognized:, unrecognized:}',
+      otherUnrecognizedFallback: 'foo${unrecognized:, unrecognized4:}',
     },
-    static: true,
-    address: 'foo${sourceAddress:address-result}',
-    varAddress: 'foo${sourceAddress:${sourceDirect:}}',
+  };
 
-    nonStringStringPart: 'elo${sourceMissing:, null}',
-    notExistingProperty: "${sourceProperty(not, existing), 'notExistingFallback'}",
-    nestUnrecognized: {
-      unrecognized:
-        '${sourceDirect:}|${sourceUnrecognized:}|${sourceDirect(${sourceUnrecognized:})}' +
-        '${sourceDirect:${sourceUnrecognized:}}',
+  const sources = {
+    recognized: {
+      resolve: () => ({ value: 234 }),
     },
   };
   let resultMap;
 
   before(async () => {
-    resultMap = resolveUnresolvedSourceTypes(resolveMeta(configuration));
+    const variablesMeta = resolveMeta(configuration);
+    await resolve({
+      serviceDir: process.cwd(),
+      configuration,
+      variablesMeta,
+      sources,
+      options: {},
+      fulfilledSources: new Set('recognized'),
+    });
+
+    resultMap = resolveUnresolvedSourceTypes(variablesMeta);
   });
 
   it('should resolve all not resolved sources', () => {
     expect(resultMap).to.deep.equal(
       new Map([
-        ['sourceParam', new Set(['foo\0params', 'foo\0varParam'])],
         [
-          'sourceDirect',
-          new Set(['foo\0varParam', 'varAddress', 'nestUnrecognized\0unrecognized']),
+          'unrecognized',
+          new Set([
+            'unrecognized',
+            'unrecognizedInParens',
+            'unrecognizedInAddress',
+            'unrecognizedInParensAndAddress',
+            'otherUnrecognizedFallback',
+            'deep\0unrecognized',
+            'deep\0unrecognizedInParens',
+            'deep\0unrecognizedInAddress',
+            'deep\0unrecognizedInParensAndAddress',
+            'deep\0otherUnrecognizedFallback',
+          ]),
         ],
-        ['sourceAddress', new Set(['address', 'varAddress'])],
-        ['sourceMissing', new Set(['nonStringStringPart'])],
-        ['sourceProperty', new Set(['notExistingProperty'])],
-        ['sourceUnrecognized', new Set(['nestUnrecognized\0unrecognized'])],
+        [
+          'unrecognized2',
+          new Set([
+            'unrecognizedInParens',
+            'unrecognizedInParensAndAddress',
+            'deep\0unrecognizedInParens',
+            'deep\0unrecognizedInParensAndAddress',
+          ]),
+        ],
       ])
     );
   });
