141b76ce688a9c31e506f08d357ef0715c1cf97c
Alinoe Abrassart <alinoe.abrassart@doctari.de>
2021-04-27 11:18:56 +0200

    test(aws): Improve setup/teardown position in custom bucket integration

--- test/integration/custom-deployment-bucket.test.js
+++ test/integration/custom-deployment-bucket.test.js
@@ -11,25 +11,26 @@ describe('Base AWS provider test', function () {
   this.timeout(1000 * 60 * 10);
 
   const bucketName = `serverless-test-${uuid.v4()}`;
+  let serviceDir;
 
   before(async () => {
     await createBucket(bucketName);
+    ({ servicePath: serviceDir } = await fixtures.setup('function', {
+      configExt: { provider: { deploymentBucket: { name: bucketName } } },
+    }));
+    await deployService(serviceDir);
   });
 
   it('should deploy in the configured aws bucket', async () => {
     // we cannot deploy an empty fixture like aws so we go for a small one
-    const { servicePath } = await fixtures.setup('function', {
-      configExt: { provider: { deploymentBucket: { name: bucketName } } },
-    });
-    await deployService(servicePath);
     const res = await awsRequest('S3', 'listObjects', { Bucket: bucketName });
     expect(
       res.Contents.filter((obj) => /compiled-cloudformation-template.json$/.test(obj.Key)).length
     ).to.equal(1);
-    await removeService(servicePath);
   });
 
   after(async () => {
+    await removeService(serviceDir);
     await deleteBucket(bucketName);
   });
 });
