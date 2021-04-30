8860397798398412d7232d9e4e0001a83efb0424
Piotr Grzesik <pj.grzesik@gmail.com>
2021-04-30 11:01:46 +0200

    refactor(Telemetry): Add `resolveErrorLocation` util

---
+++ test/unit/lib/utils/telemetry/resolve-error-location.test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const resolveErrorLocation = require('../../../../../lib/utils/telemetry/resolve-error-location');
+const tokenizeException = require('../../../../../lib/utils/tokenize-exception');
+
+describe('test/unit/lib/utils/resolve-error-location.test.js', () => {
+  it('should be null when stack missing', () => {
+    const err = new Error('test');
+    delete err.stack;
+    const result = resolveErrorLocation(tokenizeException(err));
+    expect(result).to.equal('<not accessible due to non-error exception>');
+  });
+
+  it('should be null for error with code and one-line stacktrace', () => {
+    const err = new Error('test');
+    err.code = 'ERR_CODE';
+    err.stack = 'Oneline stacktrace';
+    const result = resolveErrorLocation(tokenizeException(err));
+    expect(result).to.equal('<not available>');
+  });
+
+  it('should be null if no matching lines found', () => {
+    const err = new Error('test');
+    err.stack = 'no matching\nlines in\nstacktrace';
+    const result = resolveErrorLocation(tokenizeException(err));
+    expect(result).to.equal('<not reflected in stack>');
+  });
+
+  if (process.platform !== 'win32') {
+    it('should return at most 5 lines', () => {
+      const err = new Error('test');
+      err.stack =
+        'Error:\n' +
+        '    at Context.it (/home/xxx/serverless/test/unit/lib/utils/resolve-error-location.test.js:10:17)\n' +
+        '    at callFn (/home/xxx/serverless/node_modules/mocha/lib/runnable.js:366:21)\n' +
+        '    at Test.Runnable.run (/home/xxx/serverless/node_modules/mocha/lib/runnable.js:354:5)\n' +
+        '    at Runner.runTest (/home/xxx/serverless/node_modules/mocha/lib/runner.js:677:10)\n' +
+        '    at next (/home/xxx/serverless/node_modules/mocha/lib/runner.js:801:12)\n' +
+        '    at next (/home/xxx/serverless/node_modules/mocha/lib/runner.js:594:14)\n';
+      const result = resolveErrorLocation(tokenizeException(err));
+      expect(result).to.equal(
+        [
+          '/test/unit/lib/utils/resolve-error-location.test.js:10:17',
+          '/node_modules/mocha/lib/runnable.js:366:21',
+          '/node_modules/mocha/lib/runnable.js:354:5',
+          '/node_modules/mocha/lib/runner.js:677:10',
+          '/node_modules/mocha/lib/runner.js:801:12',
+        ].join('\n')
+      );
+    });
+  }
+
+  if (process.platform === 'win32') {
+    it('should return at most 5 lines and use `/` path separator', () => {
+      const err = new Error('test');
+      err.stack =
+        'Error:\n' +
+        '    at Context.it (C:\\home\\xxx\\serverless\\test\\unit\\lib\\utils\\resolve-error-location.test.js:10:17)\n' +
+        '    at callFn (C:\\home\\xxx\\serverless\\node_modules\\mocha\\lib\\runnable.js:366:21)\n' +
+        '    at Test.Runnable.run (C:\\home\\xxx\\serverless\\node_modules\\mocha\\lib\\runnable.js:354:5)\n' +
+        '    at Runner.runTest (C:\\home\\xxx\\serverless\\node_modules\\mocha\\lib\\runner.js:677:10)\n' +
+        '    at next (C:\\home\\xxx\\serverless\\node_modules\\mocha\\lib\\runner.js:801:12)\n' +
+        '    at next (C:\\home\\xxx\\serverless\\node_modules\\mocha\\lib\\runner.js:594:14)\n';
+      const result = resolveErrorLocation(tokenizeException(err));
+      expect(result).to.equal(
+        [
+          '/test/unit/lib/utils/resolve-error-location.test.js:10:17',
+          '/node_modules/mocha/lib/runnable.js:366:21',
+          '/node_modules/mocha/lib/runnable.js:354:5',
+          '/node_modules/mocha/lib/runner.js:677:10',
+          '/node_modules/mocha/lib/runner.js:801:12',
+        ].join('\n')
+      );
+    });
+  }
+});
