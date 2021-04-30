8defd00b0c98255c9afbe9e9aa4db99f96c3e80c
Piotr Grzesik <pj.grzesik@gmail.com>
2021-04-30 11:20:43 +0200

    feat(Telemetry): Report failures via telemetry

--- test/unit/lib/cli/handle-error.test.js
+++ test/unit/lib/cli/handle-error.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { expect } = require('chai');
+const chai = require('chai');
 const sinon = require('sinon');
 
 const path = require('path');
@@ -8,6 +8,11 @@ const overrideStdoutWrite = require('process-utils/override-stdout-write');
 const handleError = require('../../../../lib/cli/handle-error');
 const isStandaloneExecutable = require('../../../../lib/utils/isStandaloneExecutable');
 const ServerlessError = require('../../../../lib/serverless-error');
+const proxyquire = require('proxyquire');
+
+chai.use(require('sinon-chai'));
+
+const expect = chai.expect;
 
 describe('test/unit/lib/cli/handle-error.test.js', () => {
   it('should log environment information', async () => {
@@ -94,5 +99,104 @@ describe('test/unit/lib/cli/handle-error.test.js', () => {
       () => handleError('NON-ERROR')
     );
     expect(stdoutData).to.have.string('NON-ERROR');
+  });
+
+  describe('with mocked telemetry', () => {
+    const generateTelemetryPayloadStub = sinon.stub().resolves({});
+    const storeTelemetryLocallyStub = sinon.stub();
+    const sendTelemetryStub = sinon.stub();
+
+    const handleErrorWithMocks = proxyquire('../../../../lib/cli/handle-error', {
+      '../utils/telemetry/areDisabled': false,
+      '../utils/telemetry/generatePayload': generateTelemetryPayloadStub,
+      '../utils/telemetry/index': {
+        send: sendTelemetryStub,
+        storeLocally: storeTelemetryLocallyStub,
+      },
+    });
+
+    beforeEach(() => {
+      sinon.resetHistory();
+    });
+
+    it('should record telemetry only if `hasTelemetryBeenReported` is `false`', async () => {
+      // Override to avoid printing to stdout in tests
+      await overrideStdoutWrite(
+        () => {},
+        () =>
+          handleErrorWithMocks(new ServerlessError('Test error', 'ERR_CODE'), {
+            hasTelemetryBeenReported: false,
+          })
+      );
+      expect(generateTelemetryPayloadStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub).to.be.calledOnce;
+      expect(sendTelemetryStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub.getCall(0).args[0]).to.deep.equal({
+        outcome: 'failure',
+        failureReason: {
+          code: 'ERR_CODE',
+          kind: 'user',
+        },
+      });
+    });
+
+    it('should add `location` to `failureReason` in telemetry if error code missing', async () => {
+      // Override to avoid printing to stdout in tests
+      await overrideStdoutWrite(
+        () => {},
+        () =>
+          handleErrorWithMocks(new ServerlessError('Test error'), {
+            hasTelemetryBeenReported: false,
+          })
+      );
+      expect(generateTelemetryPayloadStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub).to.be.calledOnce;
+      expect(sendTelemetryStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub.getCall(0).args[0].failureReason).to.have.property(
+        'location'
+      );
+    });
+
+    it('should add `location` to `failureReason` in telemetry for non-user errors', async () => {
+      // Override to avoid printing to stdout in tests
+      await overrideStdoutWrite(
+        () => {},
+        () =>
+          handleErrorWithMocks(new Error('Test error'), {
+            hasTelemetryBeenReported: false,
+          })
+      );
+      expect(generateTelemetryPayloadStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub).to.be.calledOnce;
+      expect(sendTelemetryStub).to.be.calledOnce;
+      expect(storeTelemetryLocallyStub.getCall(0).args[0].failureReason).to.have.property(
+        'location'
+      );
+    });
+
+    it('should not record telemetry if `hasTelemetryBeenReported` is `true`', async () => {
+      // Override to avoid printing to stdout in tests
+      await overrideStdoutWrite(
+        () => {},
+        () =>
+          handleErrorWithMocks(new ServerlessError('Test error'), {
+            hasTelemetryBeenReported: true,
+          })
+      );
+      expect(generateTelemetryPayloadStub).not.to.be.called;
+      expect(storeTelemetryLocallyStub).not.to.be.called;
+      expect(sendTelemetryStub).not.to.be.called;
+    });
+
+    it('should not record telemetry if `hasTelemetryBeenReported` is not passed', async () => {
+      // Override to avoid printing to stdout in tests
+      await overrideStdoutWrite(
+        () => {},
+        () => handleErrorWithMocks(new ServerlessError('Test error'))
+      );
+      expect(generateTelemetryPayloadStub).not.to.be.called;
+      expect(storeTelemetryLocallyStub).not.to.be.called;
+      expect(sendTelemetryStub).not.to.be.called;
+    });
   });
 });
