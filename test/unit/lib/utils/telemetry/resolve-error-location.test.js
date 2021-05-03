3ab06282fdc9455f364fd73bd14761bae0c8d289
Piotr Grzesik <pj.grzesik@gmail.com>
2021-05-03 11:56:32 +0200

    fix(Telemetry): Handle error locations not enclosed in parens

--- test/unit/lib/utils/telemetry/resolve-error-location.test.js
+++ test/unit/lib/utils/telemetry/resolve-error-location.test.js
@@ -29,6 +29,26 @@ describe('test/unit/lib/utils/resolve-error-location.test.js', () => {
   });
 
   if (process.platform !== 'win32') {
+    it('should correctly handle paths not enclosed in parentheses', () => {
+      const err = new Error('test');
+      err.stack =
+        'Error: spawn E2BIG\n' +
+        '    at ChildProcess.spawn (node:internal/child_process:403:11)\n' +
+        '    at Object.spawn (node:child_process:573:9)\n' +
+        '    at /home/xxx/api/node_modules/bestzip/lib/bestzip.js:75:29\n' +
+        '    at /home/xxx/api/node_modules/async/dist/async.js:1802:20\n';
+
+      const result = resolveErrorLocation(tokenizeException(err));
+      expect(result).to.equal(
+        [
+          'node:internal/child_process:403:11',
+          'node:child_process:573:9',
+          '/bestzip/lib/bestzip.js:75:29',
+          '/async/dist/async.js:1802:20',
+        ].join('\n')
+      );
+    });
+
     it('should return at most 5 lines', () => {
       const err = new Error('test');
       err.stack =
@@ -53,6 +73,26 @@ describe('test/unit/lib/utils/resolve-error-location.test.js', () => {
   }
 
   if (process.platform === 'win32') {
+    it('should correctly handle paths not enclosed in parentheses', () => {
+      const err = new Error('test');
+      err.stack =
+        'Error: spawn E2BIG\n' +
+        '    at ChildProcess.spawn (node:internal/child_process:403:11)\n' +
+        '    at Object.spawn (node:child_process:573:9)\n' +
+        '    at C:\\home\\xxx\\api\\node_modules\\bestzip\\lib\\bestzip.js:75:29\n' +
+        '    at C:\\home\\xxx\\api\\node_modules\\async\\dist\\async.js:1802:20\n';
+
+      const result = resolveErrorLocation(tokenizeException(err));
+      expect(result).to.equal(
+        [
+          'node:internal/child_process:403:11',
+          'node:child_process:573:9',
+          '/bestzip/lib/bestzip.js:75:29',
+          '/async/dist/async.js:1802:20',
+        ].join('\n')
+      );
+    });
+
     it('should return at most 5 lines and use `/` path separator', () => {
       const err = new Error('test');
       err.stack =
