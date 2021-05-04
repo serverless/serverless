e0d32c800f380af8f9f705e4f7294329b7b2d0f7
unknown <ehrothusa@hotmail.com>
2021-05-04 17:31:31 -0400

    update branch to line up with master

---
+++ test/fixtures/programmatic/functionInvocation/callback.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+  if (event && event.shouldFail) {
+    callback(new Error('Failed on request'));
+    return;
+  }
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Invoked',
+      event,
+      clientContext: context.clientContext,
+      env: process.env,
+    }),
+  });
+};
