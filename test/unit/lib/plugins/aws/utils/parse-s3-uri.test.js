40e56fc0e9f71f06068d7d4c30178db8b2260357
Zach Whaley <zachbwhaley@gmail.com>
2021-05-09 14:21:19 -0500

    fix(Packaging): Fix support of the artifact S3 uri with region (#9411)

--- test/unit/lib/plugins/aws/utils/parse-s3-uri.test.js
+++ test/unit/lib/plugins/aws/utils/parse-s3-uri.test.js
@@ -19,12 +19,52 @@ describe('test/unit/lib/plugins/aws/utils/parse-s3-uri.test.js', () => {
     const actual = parseS3URI('https://s3.amazonaws.com/test-bucket/path/to/artifact.zip');
     expect(actual).to.deep.equal(expected);
   });
+  it('should parse an old style S3 URL with region', () => {
+    const expected = {
+      Bucket: 'test-bucket',
+      Key: 'path/to/artifact.zip',
+    };
+    const actual = parseS3URI(
+      'https://s3.us-west-1.amazonaws.com/test-bucket/path/to/artifact.zip'
+    );
+    expect(actual).to.deep.equal(expected);
+  });
+  it('should parse another old style S3 URL with region', () => {
+    const expected = {
+      Bucket: 'test-bucket',
+      Key: 'path/to/artifact.zip',
+    };
+    const actual = parseS3URI(
+      'https://s3-us-west-1.amazonaws.com/test-bucket/path/to/artifact.zip'
+    );
+    expect(actual).to.deep.equal(expected);
+  });
   it('should parse a new style S3 URL', () => {
     const expected = {
       Bucket: 'test-bucket',
       Key: 'path/to/artifact.zip',
     };
     const actual = parseS3URI('https://test-bucket.s3.amazonaws.com/path/to/artifact.zip');
+    expect(actual).to.deep.equal(expected);
+  });
+  it('should parse a new style S3 URL with region', () => {
+    const expected = {
+      Bucket: 'test-bucket',
+      Key: 'path/to/artifact.zip',
+    };
+    const actual = parseS3URI(
+      'https://test-bucket.s3.eu-west-1.amazonaws.com/path/to/artifact.zip'
+    );
+    expect(actual).to.deep.equal(expected);
+  });
+  it('should parse another new style S3 URL with region', () => {
+    const expected = {
+      Bucket: 'test-bucket',
+      Key: 'path/to/artifact.zip',
+    };
+    const actual = parseS3URI(
+      'https://test-bucket.s3-eu-west-1.amazonaws.com/path/to/artifact.zip'
+    );
     expect(actual).to.deep.equal(expected);
   });
   it('should reject non S3 URLs', () => {
