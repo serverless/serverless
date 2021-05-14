acc9010d7906dc7e56a97b60e25bf1920f402925
Piotr Grzesik <pj.grzesik@gmail.com>
2021-05-14 16:19:19 +0200

    feat(CLI Onboarding): Run `npm i` after fetching template

--- test/unit/lib/cli/interactive-setup/service.test.js
+++ test/unit/lib/cli/interactive-setup/service.test.js
@@ -6,12 +6,14 @@ const sinon = require('sinon');
 const configureInquirerStub = require('@serverless/test/configure-inquirer-stub');
 const step = require('../../../../../lib/cli/interactive-setup/service');
 const proxyquire = require('proxyquire');
+const overrideStdoutWrite = require('process-utils/override-stdout-write');
 
 const templatesPath = path.resolve(__dirname, '../../../../../lib/plugins/create/templates');
 
 const { expect } = chai;
 
 chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
 
 const fsp = require('fs').promises;
 const inquirer = require('@serverless/utils/inquirer');
@@ -85,6 +87,114 @@ describe('test/unit/lib/cli/interactive-setup/service.test.js', () => {
         'aws-nodejs',
         'test-project'
       );
+    });
+
+    it('Should run `npm install` if `package.json` present', async () => {
+      const downloadTemplateFromRepoStub = sinon.stub();
+      const spawnStub = sinon.stub();
+      const mockedStep = proxyquire('../../../../../lib/cli/interactive-setup/service', {
+        'child-process-ext/spawn': spawnStub,
+        '../../utils/downloadTemplateFromRepo': {
+          downloadTemplateFromRepo: downloadTemplateFromRepoStub.callsFake(
+            async (templateUrl, projectType, projectName) => {
+              await fsp.mkdir(projectName);
+              const serverlessYmlContent = `
+            service: service
+            provider:
+              name: aws
+           `;
+
+              await fsp.writeFile(path.join(projectName, 'serverless.yml'), serverlessYmlContent);
+              await fsp.writeFile(path.join(projectName, 'package.json'), '{}');
+            }
+          ),
+        },
+      });
+
+      configureInquirerStub(inquirer, {
+        confirm: { shouldCreateNewProject: true },
+        list: { projectType: 'aws-nodejs' },
+        input: { projectName: 'test-project-package-json' },
+      });
+      await mockedStep.run({ options: {} });
+      const stats = await fsp.lstat('test-project-package-json/serverless.yml');
+      expect(stats.isFile()).to.be.true;
+      expect(downloadTemplateFromRepoStub).to.have.been.calledWith(
+        'https://github.com/serverless/examples/tree/master/aws-nodejs',
+        'aws-nodejs',
+        'test-project-package-json'
+      );
+      expect(spawnStub).to.have.been.calledWith('npm', ['install'], {
+        cwd: path.join(process.cwd(), 'test-project-package-json'),
+      });
+    });
+
+    it('Should emit warning if npm installation not found', async () => {
+      const downloadTemplateFromRepoStub = sinon.stub();
+      const mockedStep = proxyquire('../../../../../lib/cli/interactive-setup/service', {
+        'child-process-ext/spawn': sinon.stub().rejects({ code: 'ENOENT' }),
+        '../../utils/downloadTemplateFromRepo': {
+          downloadTemplateFromRepo: downloadTemplateFromRepoStub.callsFake(
+            async (templateUrl, projectType, projectName) => {
+              await fsp.mkdir(projectName);
+              const serverlessYmlContent = `
+            service: service
+            provider:
+              name: aws
+           `;
+
+              await fsp.writeFile(path.join(projectName, 'serverless.yml'), serverlessYmlContent);
+              await fsp.writeFile(path.join(projectName, 'package.json'), '{}');
+            }
+          ),
+        },
+      });
+
+      configureInquirerStub(inquirer, {
+        confirm: { shouldCreateNewProject: true },
+        list: { projectType: 'aws-nodejs' },
+        input: { projectName: 'test-project-missing-npm' },
+      });
+
+      let stdoutData = '';
+      await overrideStdoutWrite(
+        (data) => (stdoutData += data),
+        async () => mockedStep.run({ options: {} })
+      );
+
+      const stats = await fsp.lstat('test-project-missing-npm/serverless.yml');
+      expect(stats.isFile()).to.be.true;
+      expect(stdoutData).to.include('Cannot install dependencies');
+    });
+
+    it('Should emit warning if npm installation not found', async () => {
+      const downloadTemplateFromRepoStub = sinon.stub();
+      const mockedStep = proxyquire('../../../../../lib/cli/interactive-setup/service', {
+        'child-process-ext/spawn': sinon.stub().rejects({ message: 'Error message' }),
+        '../../utils/downloadTemplateFromRepo': {
+          downloadTemplateFromRepo: downloadTemplateFromRepoStub.callsFake(
+            async (templateUrl, projectType, projectName) => {
+              await fsp.mkdir(projectName);
+              const serverlessYmlContent = `
+            service: service
+            provider:
+              name: aws
+           `;
+
+              await fsp.writeFile(path.join(projectName, 'serverless.yml'), serverlessYmlContent);
+              await fsp.writeFile(path.join(projectName, 'package.json'), '{}');
+            }
+          ),
+        },
+      });
+
+      configureInquirerStub(inquirer, {
+        confirm: { shouldCreateNewProject: true },
+        list: { projectType: 'aws-nodejs' },
+        input: { projectName: 'test-project-failed-install' },
+      });
+
+      await expect(mockedStep.run({ options: {} })).to.be.eventually.rejected.and.have.property('code', 'DEPENDENCIES_INSTALL_FAILED')
     });
 
     it('Should create project at not existing directory from a provided `template-path`', async () => {
