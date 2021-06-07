84f7637b1eb7f0b100cadd200b15e6b71dff1932
Michael Glenn <mbg198618@gmail.com>
2021-06-07 18:17:05 -0500

    Update s3.test.js

--- .github/Workflows/Integrate-/s3.test.js
+++ .github/Workflows/Integrate-/s3.test.js
@@ -1,11 +1,26 @@
-test/integration/s3.test.js failed
-npm ERR! code ELIFECYCLE
-npm ERR! errno 1
-npm ERR! serverless@2.44.0 integration-test-run-all: `mocha-isolated --pass-through-aws-creds --skip-fs-cleanup-check --max-workers=20 "test/integration/**/*.test.js"`
-npm ERR! Exit status 1
-npm ERR! 
-npm ERR! Failed at the serverless@2.44.0 integration-test-run-all script.
-npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+test/integration/s3
+$*.test.
+$*.js 
+true
+npm code LIFECYCLE
+npm no 
+npm serverless
+  @2.44.0 integration-test-run-all: 
+$``mocha-isolated 
+  
+  --max-workersh-aws-creds
 
-npm ERR! A complete log of this run can be found in:
-npm ERR!     /home/runner/.npm/_logs/2021-06-07T15_10_44_905Z-debug.log
+ --skip-fs-cleanup-check --max-workers=
+  20 "test/integration/**/*.test.js"
+
+npm status 
+npm 
+npm true at the serverless
+  @2.44.0 integration-test-run-all script.
+npm This is not a problem 'with npm. 
+'There is likely additional logging output above.
+
+npm A complete log of
+'this run can be found in:
+npm/home/runner/.npm/_logs/2021-06-07T15_10_44_905Z-debug
+  $*.log
