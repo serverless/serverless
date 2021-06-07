c8b08ef7231e10fe568265ca4e2e8b6d602c503b
Michael Glenn <mbg198618@gmail.com>
2021-06-07 18:11:45 -0500

    Create s3.test.js

---
+++ .github/Workflows/Integrate-/s3.test.js
@@ -0,0 +1,11 @@
+test/integration/s3.test.js failed
+npm ERR! code ELIFECYCLE
+npm ERR! errno 1
+npm ERR! serverless@2.44.0 integration-test-run-all: `mocha-isolated --pass-through-aws-creds --skip-fs-cleanup-check --max-workers=20 "test/integration/**/*.test.js"`
+npm ERR! Exit status 1
+npm ERR! 
+npm ERR! Failed at the serverless@2.44.0 integration-test-run-all script.
+npm ERR! This is probably not a problem with npm. There is likely additional logging output above.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     /home/runner/.npm/_logs/2021-06-07T15_10_44_905Z-debug.log
