7ddb7bbe702caf89cd0b82b343485b126c2cee51
Sean Dawson <contact@seandawson.info>
2021-06-23 19:42:41 +1000

    test: split out safeMoveFile tests

--- test/unit/lib/plugins/aws/customResources/generateZip.test.js
+++ test/unit/lib/plugins/aws/customResources/generateZip.test.js
@@ -4,45 +4,20 @@ const proxyquire = require('proxyquire').noCallThru();
 const fse = require('fs-extra');
 const sinon = require('sinon');
 const chai = require('chai');
-const fs = require('fs');
 const { getTmpDirPath, listZipFiles } = require('../../../../../utils/fs');
 const { join } = require('path');
 const { expect } = require('chai');
-const fsp = require('fs').promises;
 const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 
-// Stub out functions that generateZip calls to force error conditions
+// Stub out functions that generateZip calls to override hardcoded paths
 const ensureArtifactStub = sinon.stub();
 const getTmpDirPathStub = sinon.stub();
 const generateZip = proxyquire('../../../../../../lib/plugins/aws/customResources/generateZip.js', {
   '../../../utils/ensureArtifact': ensureArtifactStub,
   '../../../utils/fs/getTmpDirPath': getTmpDirPathStub,
 });
-
-/**
- * Returns and Error object that resembles the EXDEV errors that are thrown
- * when you try to rename across device boundaries.
- *
- * @returns an Error object that resembles the EXDEV errors are thrown
- */
-const createFakeExDevError = () => {
-  // Properties captured by using fs.renameSync in the Node v12.20.1 REPL
-  const fakeCrossDeviceError = new Error(
-    "Error: EXDEV: cross-device link not permitted, rename '/foo/bar' -> '/bar/baz'"
-  );
-  // This is the important property that safeMoveFile looks for
-  // to fallback to copy then rename behaviour
-  fakeCrossDeviceError.code = 'EXDEV';
-
-  // These other properties aren't used but it doesn't hurt to have an accurate error object
-  fakeCrossDeviceError.errno = -18;
-  fakeCrossDeviceError.syscall = 'rename';
-  fakeCrossDeviceError.path = '/foo/bar';
-  fakeCrossDeviceError.dest = '/bar/baz';
-  return fakeCrossDeviceError;
-};
 
 /**
  * The name of the artefact that generateZip generates
@@ -69,8 +44,6 @@ const sourcePath = getTmpDirPath();
 const cachedFile = join(cachePath, artefactName);
 
 describe('#generateZip()', () => {
-  let renameStub;
-
   beforeEach(() => {
     // Set up differently by different tests
     ensureArtifactStub.reset();
@@ -78,12 +51,6 @@ describe('#generateZip()', () => {
     // This always returns the same value
     getTmpDirPathStub.reset();
     getTmpDirPathStub.returns(sourcePath);
-
-    // Normally we just want this to work like usual
-    // The source and cache path are on the same device (/tmp) for these tests
-    // so there should not be any cross device renaming during unit tests
-    renameStub = sinon.stub(fsp, 'rename');
-    renameStub.callThrough();
 
     // Ensure the cache directory exists so that the rename doesn't fail
     fse.ensureDirSync(cachePath);
@@ -93,183 +60,65 @@ describe('#generateZip()', () => {
     // Clean up test directories after each test so they don't interfere with each other
     fse.removeSync(cachePath);
     fse.removeSync(sourcePath);
-    fsp.rename.restore();
   });
 
-  describe('when the temporary directory is not on a different device to the cache directory', () => {
-    describe('when the file does not exist by the time the zip is generated', () => {
-      beforeEach(async () => {
-        // Override the ensureArtifact function to set the cache path to the test one
-        ensureArtifactStub.callsFake(async (_, generate) => {
-          await generate(cachePath);
-          return cachePath;
-        });
-      });
-
-      it('should write a file', async () => {
-        await generateZip(artefactName);
-
-        const exists = fse.existsSync(cachedFile);
-        expect(exists).to.be.true;
-        expect(renameStub).to.have.been.calledOnce;
-      });
-    });
-
-    describe('when the file already exists by the time the zip is generated', () => {
-      beforeEach(async () => {
-        // Override the ensureArtifact function so that it always calls the 'generate' function
-        // even if the file already exists
-        // This will simulate the scenario when multiple serverless instances are packaging at once
-        // and one of the instances writes to the cache path while the others are still generating the zip file
-        ensureArtifactStub.callsFake(async (_, generate) => {
-          await generate(cachePath);
-          return cachePath;
-        });
-
-        // Create an existing file that should conflict with the generated artefact
-        fse.ensureFileSync(cachedFile);
-        fse.writeFileSync(cachedFile, 'cached data');
-      });
-
-      describe('when the cached file is open for writing', () => {
-        it('should overwrite the cached file', async () => {
-          const fd = fs.openSync(cachedFile, 'r+');
-
-          try {
-            await generateZip(artefactName);
-
-            const cachedData = fse.readFileSync(cachedFile).toString();
-            expect(cachedData).not.to.eq('cached data');
-            expect(renameStub).to.have.been.calledOnce;
-          } finally {
-            fs.closeSync(fd);
-          }
-        });
-      });
-
-      describe('when the cached file is not open', () => {
-        it('should overwrite the cached file with a valid zip file', async () => {
-          await generateZip(artefactName);
-
-          // Check that the file was actually overwritten
-          const cachedData = fse.readFileSync(cachedFile).toString();
-          expect(cachedData).not.to.eq('cached data');
-
-          expect(renameStub).to.have.been.calledOnce;
-
-          // List the files in the zip to make sure it is valid
-          const filesInZip = await listZipFiles(cachedFile);
-          expect(filesInZip).to.have.all.members([
-            'README.md',
-            'apiGatewayCloudWatchRole/handler.js',
-            'cognitoUserPool/handler.js',
-            'cognitoUserPool/lib/permissions.js',
-            'cognitoUserPool/lib/userPool.js',
-            'eventBridge/handler.js',
-            'eventBridge/lib/eventBridge.js',
-            'eventBridge/lib/permissions.js',
-            'eventBridge/lib/utils.js',
-            's3/handler.js',
-            's3/lib/bucket.js',
-            's3/lib/permissions.js',
-            'utils.js',
-          ]);
-        });
-      });
-    });
-  });
-
-  describe('when the temporary directory is on a different device to the cache directory', () => {
+  describe('when the file does not exist by the time the zip is generated', () => {
     beforeEach(async () => {
       // Override the ensureArtifact function to set the cache path to the test one
       ensureArtifactStub.callsFake(async (_, generate) => {
         await generate(cachePath);
         return cachePath;
       });
-
-      const error = createFakeExDevError();
-      renameStub.onFirstCall().rejects(error);
     });
 
-    describe('when the file does not exist by the time the zip is generated', () => {
-      beforeEach(async () => {
-        // Override the ensureArtifact function to set the cache path to the test one
-        ensureArtifactStub.callsFake(async (_, generate) => {
-          await generate(cachePath);
-          return cachePath;
-        });
+    it('should write a file', async () => {
+      await generateZip(artefactName);
+
+      const exists = fse.existsSync(cachedFile);
+      expect(exists).to.be.true;
+    });
+  });
+
+  describe('when the file already exists by the time the zip is generated', () => {
+    beforeEach(async () => {
+      // Override the ensureArtifact function so that it always calls the 'generate' function
+      // even if the file already exists
+      // This will simulate the scenario when multiple serverless instances are packaging at once
+      // and one of the instances writes to the cache path while the others are still generating the zip file
+      ensureArtifactStub.callsFake(async (_, generate) => {
+        await generate(cachePath);
+        return cachePath;
       });
 
-      it('should write a file', async () => {
-        await generateZip(artefactName);
-
-        const exists = fse.existsSync(cachedFile);
-        expect(exists).to.be.true;
-        expect(renameStub).to.have.been.calledTwice;
-      });
+      // Create an existing file that should conflict with the generated artefact
+      fse.ensureFileSync(cachedFile);
+      fse.writeFileSync(cachedFile, 'cached data');
     });
 
-    describe('when the file already exists by the time the zip is generated', () => {
-      beforeEach(async () => {
-        // Override the ensureArtifact function so that it always calls the 'generate' function
-        // even if the file already exists
-        // This will simulate the scenario when multiple serverless instances are packaging at once
-        // and one of the instances writes to the cache path while the others are still generating the zip file
-        ensureArtifactStub.callsFake(async (_, generate) => {
-          await generate(cachePath);
-          return cachePath;
-        });
+    it('should overwrite the cached file with a valid zip file', async () => {
+      await generateZip(artefactName);
 
-        // Create an existing file that should conflict with the generated artefact
-        fse.ensureFileSync(cachedFile);
-        fse.writeFileSync(cachedFile, 'cached data');
-      });
+      // Check that the file was actually overwritten
+      const cachedData = fse.readFileSync(cachedFile).toString();
+      expect(cachedData).not.to.eq('cached data');
 
-      describe('when the cached file is open for writing', () => {
-        it('should overwrite the cached file', async () => {
-          const fd = fs.openSync(cachedFile, 'r+');
-
-          try {
-            await generateZip(artefactName);
-
-            const cachedData = fse.readFileSync(cachedFile).toString();
-            expect(cachedData).not.to.eq('cached data');
-            expect(renameStub).to.have.been.calledTwice;
-          } finally {
-            fs.closeSync(fd);
-          }
-        });
-      });
-
-      describe('when the cached file is not open', () => {
-        it('should overwrite the cached file with a valid zip file', async () => {
-          await generateZip(artefactName);
-
-          // Check that the file was actually overwritten
-          const cachedData = fse.readFileSync(cachedFile).toString();
-          expect(cachedData).not.to.eq('cached data');
-
-          expect(renameStub).to.have.been.calledTwice;
-
-          // List the files in the zip to make sure it is valid
-          const filesInZip = await listZipFiles(cachedFile);
-          expect(filesInZip).to.have.all.members([
-            'README.md',
-            'apiGatewayCloudWatchRole/handler.js',
-            'cognitoUserPool/handler.js',
-            'cognitoUserPool/lib/permissions.js',
-            'cognitoUserPool/lib/userPool.js',
-            'eventBridge/handler.js',
-            'eventBridge/lib/eventBridge.js',
-            'eventBridge/lib/permissions.js',
-            'eventBridge/lib/utils.js',
-            's3/handler.js',
-            's3/lib/bucket.js',
-            's3/lib/permissions.js',
-            'utils.js',
-          ]);
-        });
-      });
+      // List the files in the zip to make sure it is valid
+      const filesInZip = await listZipFiles(cachedFile);
+      expect(filesInZip).to.have.all.members([
+        'README.md',
+        'apiGatewayCloudWatchRole/handler.js',
+        'cognitoUserPool/handler.js',
+        'cognitoUserPool/lib/permissions.js',
+        'cognitoUserPool/lib/userPool.js',
+        'eventBridge/handler.js',
+        'eventBridge/lib/eventBridge.js',
+        'eventBridge/lib/permissions.js',
+        'eventBridge/lib/utils.js',
+        's3/handler.js',
+        's3/lib/bucket.js',
+        's3/lib/permissions.js',
+        'utils.js',
+      ]);
     });
   });
 });
---
+++ test/unit/lib/utils/fs/safeMoveFile.test.js
@@ -0,0 +1,177 @@
+'use strict';
+
+const fse = require('fs-extra');
+const sinon = require('sinon');
+const chai = require('chai');
+const fs = require('fs');
+const { getTmpDirPath } = require('../../../../utils/fs');
+const { join } = require('path');
+const { expect } = require('chai');
+const fsp = require('fs').promises;
+const sinonChai = require('sinon-chai');
+const safeMoveFile = require('../../../../../lib/utils/fs/safeMoveFile');
+
+chai.use(sinonChai);
+
+/**
+ * Returns and Error object that resembles the EXDEV errors that are thrown
+ * when you try to rename across device boundaries.
+ *
+ * @returns an Error object that resembles the EXDEV errors are thrown
+ */
+const createFakeExDevError = () => {
+  // Properties captured by using fs.renameSync in the Node v12.20.1 REPL
+  const fakeCrossDeviceError = new Error(
+    "Error: EXDEV: cross-device link not permitted, rename '/foo/bar' -> '/bar/baz'"
+  );
+  // This is the important property that safeMoveFile looks for
+  // to fallback to copy then rename behaviour
+  fakeCrossDeviceError.code = 'EXDEV';
+
+  // These other properties aren't used but it doesn't hurt to have an accurate error object
+  fakeCrossDeviceError.errno = -18;
+  fakeCrossDeviceError.syscall = 'rename';
+  fakeCrossDeviceError.path = '/foo/bar';
+  fakeCrossDeviceError.dest = '/bar/baz';
+  return fakeCrossDeviceError;
+};
+
+/**
+ * The name of the file that will be moved
+ */
+const artefactName = 'foo-bar.zip';
+
+// Generate temporary directories once per test run
+// They will be cleaned up when the tests are done
+
+/**
+ * The folder that the test file will be moved to
+ */
+const destinationPath = getTmpDirPath();
+
+/**
+ * The folder that the test file will come from
+ */
+const sourcePath = getTmpDirPath();
+
+/**
+ * The full path to destination where the file is being moved to
+ */
+const destinationFile = join(destinationPath, artefactName);
+
+/**
+ * The full path to the file that is being moved
+ */
+const sourceFile = join(sourcePath, artefactName);
+
+describe('#safeMoveFile()', () => {
+  let renameStub;
+  let copyFileStub;
+  let unlinkStub;
+
+  beforeEach(async () => {
+    renameStub = sinon.stub(fsp, 'rename');
+    copyFileStub = sinon.stub(fsp, 'copyFile');
+    unlinkStub = sinon.stub(fsp, 'unlink');
+    // Allow the fs calls to act as normal until we want to force the rename to fail
+    renameStub.callThrough();
+    copyFileStub.callThrough();
+    unlinkStub.callThrough();
+
+    // Ensure the cache directory exists so that the rename doesn't fail
+    fse.ensureDirSync(sourcePath);
+    fse.ensureDirSync(destinationPath);
+
+    // Write a test file to rename
+    await fse.writeFile(sourceFile, 'source data');
+  });
+
+  afterEach(() => {
+    // Clean up test directories after each test so they don't interfere with each other
+    fse.removeSync(destinationPath);
+    fse.removeSync(sourcePath);
+    // Reset stubbed methods
+    fsp.rename.restore();
+    fsp.copyFile.restore();
+    fsp.unlink.restore();
+  });
+
+  /**
+   * Run the test suite with the provided post assertion function.
+   * The post assertion function will be called after each test scenario is executed
+   * so that the test scenarios can be reused.
+   *
+   * @param {*} postAssertion the function that is called after every test scenario
+   */
+  const runTestScenariosWithPostAssertion = (postAssertion) => {
+    describe('when the file does not exist by the time the zip is generated', () => {
+      it('should write a file', async () => {
+        await safeMoveFile(sourceFile, destinationFile);
+
+        const exists = fse.existsSync(destinationFile);
+        expect(exists).to.be.true;
+        postAssertion();
+      });
+    });
+
+    describe('when the file already exists by the time the zip is generated', () => {
+      beforeEach(async () => {
+        // Create an existing file that is at the destination
+        fse.ensureFileSync(destinationFile);
+        fse.writeFileSync(destinationFile, 'existing destination data');
+      });
+
+      describe('when the cached file is open for writing', () => {
+        it('should overwrite the cached file', async () => {
+          const fd = fs.openSync(destinationFile, 'r+');
+
+          try {
+            await safeMoveFile(sourceFile, destinationFile);
+
+            const cachedData = fse.readFileSync(destinationFile).toString();
+            expect(cachedData).not.to.eq('existing destination data');
+            postAssertion();
+          } finally {
+            fs.closeSync(fd);
+          }
+        });
+      });
+
+      describe('when the cached file is not open', () => {
+        it('should overwrite the cached file with a valid zip file', async () => {
+          await safeMoveFile(sourceFile, destinationFile);
+
+          // Check that the file was actually overwritten
+          const cachedData = fse.readFileSync(destinationFile).toString();
+          expect(cachedData).not.to.eq('existing destination data');
+
+          postAssertion();
+        });
+      });
+    });
+  };
+
+  describe('when the temporary directory is not on a different device to the cache directory', () => {
+    runTestScenariosWithPostAssertion(() => {
+      // Happy path: Only rename is called
+      expect(renameStub).to.have.be.calledOnce;
+      expect(copyFileStub).to.not.have.been.called;
+      expect(unlinkStub).to.not.have.been.called;
+    });
+  });
+
+  describe('when the temporary directory is on a different device to the cache directory', () => {
+    beforeEach(async () => {
+      const error = createFakeExDevError();
+      renameStub.onFirstCall().rejects(error);
+    });
+
+    runTestScenariosWithPostAssertion(() => {
+      // Rename is called twice because the first call will fail due to the cross device rename
+      // The second call is across the same device
+      expect(renameStub).to.have.been.calledTwice;
+      expect(copyFileStub).to.have.been.calledOnce;
+      expect(unlinkStub).to.have.been.calledOnce;
+    });
+  });
+});
