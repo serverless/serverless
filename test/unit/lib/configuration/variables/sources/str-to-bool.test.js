fe693ac15fa401555985db2e19afde36dc3c21d8
Shane Spencer <305301+whardier@users.noreply.github.com>
2021-07-26 19:09:25 +0000

    fix linting/prettier issues with strToBool test/func patch

--- test/unit/lib/configuration/variables/sources/str-to-bool.test.js
+++ test/unit/lib/configuration/variables/sources/str-to-bool.test.js
@@ -31,8 +31,10 @@ describe('test/unit/lib/configuration/variables/sources/str-to-bool.test.js', ()
 
   it('should resolve truthy input', () => expect(configuration.truthy).to.equal(true));
   it('should resolve falsy input', () => expect(configuration.falsy).to.equal(false));
-  it('should resolve uppercase truthy input', () => expect(configuration.truthyUppercase).to.equal(true));
-  it('should resolve mixed case falsy input', () => expect(configuration.falsyMixedCase).to.equal(false));
+  it('should resolve uppercase truthy input', () =>
+    expect(configuration.truthyUppercase).to.equal(true));
+  it('should resolve mixed case falsy input', () =>
+    expect(configuration.falsyMixedCase).to.equal(false));
 
   it('should report with an error missing input', () =>
     expect(variablesMeta.get('noParam').error.code).to.equal('VARIABLE_RESOLUTION_ERROR'));
