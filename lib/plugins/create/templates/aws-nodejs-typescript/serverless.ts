698ac838e290bed8b27eeb05a345e2905e6e4cc9
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2021-09-15 16:12:48 +0200

    Add automatic source maps stacktrace

--- lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
+++ lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
@@ -21,6 +21,7 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
     lambdaHashingVersion: '20201221',
   },
--- lib/plugins/create/templates/aws-nodejs-typescript/src/functions/hello/handler.ts
+++ lib/plugins/create/templates/aws-nodejs-typescript/src/functions/hello/handler.ts
@@ -1,5 +1,3 @@
-import 'source-map-support/register';
-
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
