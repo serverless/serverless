7058e0fc2607cbf2b6819f752657018396a550b0
Seungchan Ahn <dev.issea1015@gmail.com>
2021-09-22 18:42:25 +0900

    Destructure class into functions

--- commands/plugin/install.js
+++ commands/plugin/install.js
@@ -12,17 +12,157 @@ const log = require('@serverless/utils/log');
 const ServerlessError = require('../../lib/serverless-error');
 const yamlAstParser = require('../../lib/utils/yamlAstParser');
 const fileExists = require('../../lib/utils/fs/fileExists');
-const pluginUtils = require('../../lib/commands/plugin-management');
 const npmCommandDeferred = require('../../lib/utils/npm-command-deferred');
 const CLI = require('../../lib/classes/CLI');
+const {
+  getPlugins,
+  getPluginInfo,
+  getServerlessFilePath,
+  validate,
+} = require('../../lib/commands/plugin-management');
+
+const cli = new CLI(undefined);
 
 module.exports = async ({ configuration, serviceDir, configurationFilename, options }) => {
-  await new PluginInstall({
-    configuration,
+  const pluginInfo = getPluginInfo(options.name);
+  options.pluginName = pluginInfo[0];
+  options.pluginVersion = pluginInfo[1] || 'latest';
+
+  validate({ serviceDir });
+  const plugins = await getPlugins();
+  const plugin = plugins.find((item) => item.name === options.pluginName);
+  if (!plugin) {
+    cli.log('Plugin not found in serverless registry, continuing to install');
+  }
+
+  await pluginInstall({ configuration, serviceDir, options });
+  await addPluginToServerlessFile({ serviceDir, configurationFilename, options });
+  await installPeerDependencies({ serviceDir, options });
+
+  const message = [
+    'Successfully installed',
+    ` "${options.pluginName}@${options.pluginVersion}"`,
+  ].join('');
+  cli.log(message);
+};
+
+const pluginInstall = async ({ configuration, serviceDir, options }) => {
+  const packageJsonFilePath = path.join(serviceDir, 'package.json');
+
+  // check if package.json is already present. Otherwise create one
+  const exists = await fileExists(packageJsonFilePath);
+  if (!exists) {
+    cli.log('Creating an empty package.json file in your service directory');
+
+    const packageJsonFileContent = {
+      name: configuration.service,
+      description: '',
+      version: '0.1.0',
+      dependencies: {},
+      devDependencies: {},
+    };
+    await fse.writeJson(packageJsonFilePath, packageJsonFileContent);
+  }
+
+  // install the package through npm
+  const pluginFullName = `${options.pluginName}@${options.pluginVersion}`;
+  const message = [
+    `Installing plugin "${pluginFullName}"`,
+    ' (this might take a few seconds...)',
+  ].join('');
+  cli.log(message);
+  await npmInstall(pluginFullName, { serviceDir });
+};
+
+const addPluginToServerlessFile = async ({ serviceDir, configurationFilename, options }) => {
+  const serverlessFilePath = getServerlessFilePath({ serviceDir, configurationFilename });
+  const fileExtension = path.extname(serverlessFilePath);
+  if (fileExtension === '.js' || fileExtension === '.ts') {
+    requestManualUpdate(serverlessFilePath);
+    return;
+  }
+
+  const checkIsArrayPluginsObject = (pluginsObject) =>
+    pluginsObject == null || Array.isArray(pluginsObject);
+  // pluginsObject type determined based on the value loaded during the serverless init.
+  if (_.last(serverlessFilePath.split('.')) === 'json') {
+    const serverlessFileObj = await fse.readJson(serverlessFilePath);
+    const newServerlessFileObj = serverlessFileObj;
+    const isArrayPluginsObject = checkIsArrayPluginsObject(newServerlessFileObj.plugins);
+    // null modules property is not supported
+    let plugins = isArrayPluginsObject
+      ? newServerlessFileObj.plugins || []
+      : newServerlessFileObj.plugins.modules;
+
+    if (plugins == null) {
+      throw new ServerlessError(
+        'plugins modules property must be present',
+        'PLUGINS_MODULES_MISSING'
+      );
+    }
+
+    plugins.push(options.pluginName);
+    plugins = _.sortedUniq(plugins);
+
+    if (isArrayPluginsObject) {
+      newServerlessFileObj.plugins = plugins;
+    } else {
+      newServerlessFileObj.plugins.modules = plugins;
+    }
+
+    await fse.writeJson(serverlessFilePath, newServerlessFileObj);
+    return;
+  }
+
+  const serverlessFileObj = yaml.load(await fsp.readFile(serverlessFilePath, 'utf8'), {
+    filename: serverlessFilePath,
+    schema: cloudformationSchema,
+  });
+  if (serverlessFileObj.plugins != null) {
+    // Plugins section can be behind veriables, opt-out in such case
+    if (isPlainObject(serverlessFileObj.plugins)) {
+      if (
+        serverlessFileObj.plugins.modules != null &&
+        !Array.isArray(serverlessFileObj.plugins.modules)
+      ) {
+        requestManualUpdate(serverlessFilePath);
+        return;
+      }
+    } else if (!Array.isArray(serverlessFileObj.plugins)) {
+      requestManualUpdate(serverlessFilePath);
+      return;
+    }
+  }
+  await yamlAstParser.addNewArrayItem(
+    serverlessFilePath,
+    checkIsArrayPluginsObject(serverlessFileObj.plugins) ? 'plugins' : 'plugins.modules',
+    options.pluginName
+  );
+};
+
+const installPeerDependencies = async ({ serviceDir, options }) => {
+  const pluginPackageJsonFilePath = path.join(
     serviceDir,
-    configurationFilename,
-    options,
-  }).install();
+    'node_modules',
+    options.pluginName,
+    'package.json'
+  );
+  const pluginPackageJson = await fse.readJson(pluginPackageJsonFilePath);
+  if (pluginPackageJson.peerDependencies) {
+    const pluginsArray = [];
+    Object.entries(pluginPackageJson.peerDependencies).forEach(([k, v]) => {
+      pluginsArray.push(`${k}@"${v}"`);
+    });
+    await Promise.all(pluginsArray.map((plugin) => npmInstall(plugin, { serviceDir })));
+  }
+};
+
+const npmInstall = async (name, { serviceDir }) => {
+  const npmCommand = await npmCommandDeferred;
+  await spawn(npmCommand, ['install', '--save-dev', name], {
+    cwd: serviceDir,
+    stdio: 'ignore',
+  });
 };
 
 const requestManualUpdate = (serverlessFilePath) =>
@@ -30,159 +170,3 @@ const requestManualUpdate = (serverlessFilePath) =>
   Can't automatically add plugin into "${path.basename(serverlessFilePath)}" file.
   Please make it manually.
 `);
-
-class PluginInstall {
-  constructor({ configuration, serviceDir, configurationFilename, options }) {
-    this.configuration = configuration;
-    this.serviceDir = serviceDir;
-    this.configurationFilename = configurationFilename;
-    this.options = options;
-
-    this.cli = new CLI(undefined);
-
-    Object.assign(this, pluginUtils);
-  }
-
-  async install() {
-    const pluginInfo = pluginUtils.getPluginInfo(this.options.name);
-    this.options.pluginName = pluginInfo[0];
-    this.options.pluginVersion = pluginInfo[1] || 'latest';
-
-    await this.validate();
-    const plugins = await this.getPlugins();
-    const plugin = plugins.find((item) => item.name === this.options.pluginName);
-    if (!plugin) {
-      this.cli.log('Plugin not found in serverless registry, continuing to install');
-    }
-
-    await this.pluginInstall();
-    await this.addPluginToServerlessFile();
-    await this.installPeerDependencies();
-
-    const message = [
-      'Successfully installed',
-      ` "${this.options.pluginName}@${this.options.pluginVersion}"`,
-    ].join('');
-    this.cli.log(message);
-  }
-
-  async pluginInstall() {
-    const serviceDir = this.serviceDir;
-    const packageJsonFilePath = path.join(serviceDir, 'package.json');
-
-    // check if package.json is already present. Otherwise create one
-    const exists = await fileExists(packageJsonFilePath);
-    if (!exists) {
-      this.cli.log('Creating an empty package.json file in your service directory');
-
-      const packageJsonFileContent = {
-        name: this.configuration.service,
-        description: '',
-        version: '0.1.0',
-        dependencies: {},
-        devDependencies: {},
-      };
-      await fse.writeJson(packageJsonFilePath, packageJsonFileContent);
-    }
-
-    // install the package through npm
-    const pluginFullName = `${this.options.pluginName}@${this.options.pluginVersion}`;
-    const message = [
-      `Installing plugin "${pluginFullName}"`,
-      ' (this might take a few seconds...)',
-    ].join('');
-    this.cli.log(message);
-    await this.npmInstall(pluginFullName);
-  }
-
-  async addPluginToServerlessFile() {
-    const serverlessFilePath = this.getServerlessFilePath();
-    const fileExtension = path.extname(serverlessFilePath);
-    if (fileExtension === '.js' || fileExtension === '.ts') {
-      requestManualUpdate(serverlessFilePath);
-      return;
-    }
-
-    const checkIsArrayPluginsObject = (pluginsObject) =>
-      pluginsObject == null || Array.isArray(pluginsObject);
-    // pluginsObject type determined based on the value loaded during the serverless init.
-    if (_.last(serverlessFilePath.split('.')) === 'json') {
-      const serverlessFileObj = await fse.readJson(serverlessFilePath);
-      const newServerlessFileObj = serverlessFileObj;
-      const isArrayPluginsObject = checkIsArrayPluginsObject(newServerlessFileObj.plugins);
-      // null modules property is not supported
-      let plugins = isArrayPluginsObject
-        ? newServerlessFileObj.plugins || []
-        : newServerlessFileObj.plugins.modules;
-
-      if (plugins == null) {
-        throw new ServerlessError(
-          'plugins modules property must be present',
-          'PLUGINS_MODULES_MISSING'
-        );
-      }
-
-      plugins.push(this.options.pluginName);
-      plugins = _.sortedUniq(plugins);
-
-      if (isArrayPluginsObject) {
-        newServerlessFileObj.plugins = plugins;
-      } else {
-        newServerlessFileObj.plugins.modules = plugins;
-      }
-
-      await fse.writeJson(serverlessFilePath, newServerlessFileObj);
-      return;
-    }
-
-    const serverlessFileObj = yaml.load(await fsp.readFile(serverlessFilePath, 'utf8'), {
-      filename: serverlessFilePath,
-      schema: cloudformationSchema,
-    });
-    if (serverlessFileObj.plugins != null) {
-      // Plugins section can be behind veriables, opt-out in such case
-      if (isPlainObject(serverlessFileObj.plugins)) {
-        if (
-          serverlessFileObj.plugins.modules != null &&
-          !Array.isArray(serverlessFileObj.plugins.modules)
-        ) {
-          requestManualUpdate(serverlessFilePath);
-          return;
-        }
-      } else if (!Array.isArray(serverlessFileObj.plugins)) {
-        requestManualUpdate(serverlessFilePath);
-        return;
-      }
-    }
-    await yamlAstParser.addNewArrayItem(
-      serverlessFilePath,
-      checkIsArrayPluginsObject(serverlessFileObj.plugins) ? 'plugins' : 'plugins.modules',
-      this.options.pluginName
-    );
-  }
-
-  async installPeerDependencies() {
-    const pluginPackageJsonFilePath = path.join(
-      this.serviceDir,
-      'node_modules',
-      this.options.pluginName,
-      'package.json'
-    );
-    const pluginPackageJson = await fse.readJson(pluginPackageJsonFilePath);
-    if (pluginPackageJson.peerDependencies) {
-      const pluginsArray = [];
-      Object.entries(pluginPackageJson.peerDependencies).forEach(([k, v]) => {
-        pluginsArray.push(`${k}@"${v}"`);
-      });
-      await Promise.all(pluginsArray.map((plugin) => this.npmInstall(plugin)));
-    }
-  }
-
-  async npmInstall(name) {
-    const npmCommand = await npmCommandDeferred;
-    await spawn(npmCommand, ['install', '--save-dev', name], {
-      cwd: this.serviceDir,
-      stdio: 'ignore',
-    });
-  }
-}
