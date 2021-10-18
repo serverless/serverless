829a7f74aa1da2203f7dd440aaf496e694fcbfed
Seungchan Ahn <dev.issea1015@gmail.com>
2021-10-18 22:48:35 +0900

    Rename function to be close to English syntax

--- test/utils/iam.js
+++ test/utils/iam.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const chai = require('chai');
-const { isMergeablePolicyStatement } = require('../../lib/plugins/aws/package/lib/utils');
+const { areMergeablePolicyStatements } = require('../../lib/plugins/aws/package/lib/utils');
 
 const expect = chai.expect;
 
 module.exports = {
   // Confirm that `statement` is reflected in `statements`
   expectToIncludeStatement: (statements, statement) => {
-    const statement_ = statements.find((el) => isMergeablePolicyStatement(el, statement)) || {};
+    const statement_ = statements.find((el) => areMergeablePolicyStatements(el, statement)) || {};
 
     const toArray = (value) => {
       return Array.isArray(value) ? [...value] : [value];
