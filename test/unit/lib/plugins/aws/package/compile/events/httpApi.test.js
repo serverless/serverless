acb93ed07964457fc61a07ff65c07222efd6df36
Piotr Grzesik <pj.grzesik@gmail.com>
2021-10-25 11:31:32 +0200

    feat(AWS HTTP API): Always apply `provider.tags` to HTTP API
    BREAKING CHANGE: Tags from `provider.tags` are applied by default to HTTP API
    Gateway.

--- test/unit/lib/plugins/aws/package/compile/events/httpApi.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/httpApi.test.js
@@ -201,7 +201,6 @@ describe('lib/plugins/aws/package/compile/events/httpApi.test.js', () => {
                   audience: 'audiencexxx',
                 },
               },
-              useProviderTags: true,
             },
             logs: {
               httpApi: true,
