e57dc5aa116e1cbe87a95c594d2c71a1a230ba19
Mariusz Nowak <medyk@medikoo.com>
2021-10-25 10:42:58 +0200

    chore: Setup v3 branch

---
+++ .github/prepare-pre-3.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+packageJson.version = `${packageJson.version}.${process.env.GITHUB_SHA.slice(0, 8)}`;
+packageJson.dependencies['@serverless/utils'] = 'pre-6';
+
+fs.writeFileSync(packageJsonPath, `${JSON.stringify(packageJson, null, 2)}\n`);
