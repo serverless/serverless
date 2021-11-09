4d9c72984eb502d48360b6c2242e5bf8e1ed98f2
Piotr Grzesik <pj.grzesik@gmail.com>
2021-11-09 12:12:14 +0100

    feat: Simplify `logs` command output

--- test/unit/lib/plugins/aws/utils/formatLambdaLogEvent.test.js
+++ test/unit/lib/plugins/aws/utils/formatLambdaLogEvent.test.js
@@ -8,9 +8,12 @@ const formatLambdaLogEvent = require('../../../../../../lib/plugins/aws/utils/fo
 describe('#formatLambdaLogEvent()', () => {
   it('should format invocation report', () => {
     const msg =
-      'REPORT\tRequestId: 99c30000-b01a-11e5-93f7-b8e85631a00e\tDuration: 0.40 ms\tBilled Duration: 100 ms\tMemory Size: 512 MB\tMax Memory Used: 30 MB';
+      'REPORT RequestId: 99c30000-b01a-11e5-93f7-b8e85631a00e\tDuration: 0.40 ms\tBilled Duration: 100 ms\tMemory Size: 512 MB\tMax Memory Used: 30 MB';
+    const expectedMsg = style.aside(
+      'END Duration: 0.40 ms\tBilled Duration: 100 ms\tMemory Size: 512 MB\tMax Memory Used: 30 MB'
+    );
 
-    expect(formatLambdaLogEvent(msg)).to.deep.equal(style.aside(msg));
+    expect(formatLambdaLogEvent(msg)).to.deep.equal(expectedMsg);
   });
 
   it('should format invocation failures', () => {
@@ -19,12 +22,12 @@ describe('#formatLambdaLogEvent()', () => {
   });
 
   it('should format lambda console.log lines', () => {
-    const nodeLogLine = '2016-01-01T12:00:00Z\t99c30000-b01a-11e5-93f7-b8e85631a00e\ttest';
+    const nodeLogLine = '2016-01-01T12:00:00Z\t99c30000-b01a-11e5-93f7-b8e85631a00e\tINFO\ttest';
 
     let expectedLogMessage = '';
-    const date = dayjs('2016-01-01T12:00:00Z').format('YYYY-MM-DD HH:mm:ss.SSS (Z)');
+    const date = dayjs('2016-01-01T12:00:00Z').format('YYYY-MM-DD HH:mm:ss.SSS');
     expectedLogMessage += `${style.aside(date)}\t`;
-    expectedLogMessage += `${style.aside('99c30000-b01a-11e5-93f7-b8e85631a00e')}\t`;
+    expectedLogMessage += 'INFO\t';
     expectedLogMessage += 'test';
 
     expect(formatLambdaLogEvent(nodeLogLine)).to.equal(expectedLogMessage);
@@ -35,9 +38,8 @@ describe('#formatLambdaLogEvent()', () => {
       '[INFO]\t2016-01-01T12:00:00Z\t99c30000-b01a-11e5-93f7-b8e85631a00e\ttest';
 
     let expectedLogMessage = '';
-    const date = dayjs('2016-01-01T12:00:00Z').format('YYYY-MM-DD HH:mm:ss.SSS (Z)');
+    const date = dayjs('2016-01-01T12:00:00Z').format('YYYY-MM-DD HH:mm:ss.SSS');
     expectedLogMessage += `${style.aside(date)}\t`;
-    expectedLogMessage += `${style.aside('99c30000-b01a-11e5-93f7-b8e85631a00e')}\t`;
     expectedLogMessage += `${'[INFO]'}\t`;
     expectedLogMessage += 'test';
 
