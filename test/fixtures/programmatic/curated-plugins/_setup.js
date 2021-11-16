b8d386547f30ee3701e1f6ee6cacc0be08df426a
Mariusz Nowak <medyk@medikoo.com>
2021-11-16 16:07:17 +0100

    test(Plugins): Ensure to not leave broken symlinks in a setup fixture
    Otherwise it leaves a broken symlink of which copying crashes `fse.copy`

--- test/fixtures/programmatic/curated-plugins/_setup.js
+++ test/fixtures/programmatic/curated-plugins/_setup.js
@@ -3,7 +3,12 @@
 const path = require('path');
 const fsp = require('fs').promises;
 
-const slsDependencyDir = path.resolve(__dirname, 'node_modules/serverless');
+const nodeModulesDir = path.resolve(__dirname, 'node_modules');
 
 // Ensure to remove "serverless" installed as peer-dependency to avoid local fallback
-module.exports = async () => fsp.rm(slsDependencyDir, { recursive: true, force: true });
+module.exports = async () =>
+  Promise.all([
+    fsp.rm(path.resolve(nodeModulesDir, 'serverless'), { recursive: true, force: true }),
+    fsp.unlink(path.resolve(nodeModulesDir, '.bin/serverless')).catch(() => {}),
+    fsp.unlink(path.resolve(nodeModulesDir, '.bin/sls')).catch(() => {}),
+  ]);
