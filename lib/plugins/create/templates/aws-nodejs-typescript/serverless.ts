a5ef43f1195ff9b0d96be555cc8dffae371a3c1b
Piotr Grzesik <pj.grzesik@gmail.com>
2021-11-19 18:03:40 +0100

    feat(Templates): Remove `lambdaHashingVersion` setting

--- lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
+++ lib/plugins/create/templates/aws-nodejs-typescript/serverless.ts
@@ -17,7 +17,6 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
     },
-    lambdaHashingVersion: '20201221',
   },
   // import the function via paths
   functions: { hello },
