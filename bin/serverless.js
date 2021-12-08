dd904544528eec675f535a2ae6cad18e36c6d2a2
Mariusz Nowak <medyk@medikoo.com>
2021-12-08 14:45:12 +0100

    feat(CLI): Support `serverless-tencent` CLI

--- bin/serverless.js
+++ bin/serverless.js
@@ -35,6 +35,14 @@ require('../lib/cli/triage')().then((cliName) => {
     case 'serverless':
       require('../scripts/serverless');
       return;
+    case 'serverless-tencent':
+      require('../lib/cli/run-serverless-tencent')().catch((error) => {
+        // Expose eventual resolution error as regular crash, and not unhandled rejection
+        process.nextTick(() => {
+          throw error;
+        });
+      });
+      return;
     case '@serverless/components':
       require('@serverless/components').runComponents();
       return;
