69c53dbf220f118e34a26c4722215b7f96cd3185
Mariusz Nowak <medyk@medikoo.com>
2021-12-23 14:45:50 +0100

    feat: Remove tab autocomplete feature
    Reasoning:
    - Performance is very poor due to slow command startup time
    - `tabtab` package is no longer maintained and shares security issues
    - Feature usage is relatively low (telemetry data shows 7% of users has the autocompletion on)
    
    BREAKING CHANGE:
    Opt-in tab-tab autocompletion feature is removed due to performance and security issues

--- test/unit/lib/utils/tabCompletion/isSupported.test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { expect } = require('chai');
-
-const isSuported = require('../../../../../lib/utils/tabCompletion/isSupported');
-
-describe('isTabtabCompletionSuported', () => {
-  it('Should resolve boolean', () => expect(typeof isSuported).to.equal('boolean'));
-});
