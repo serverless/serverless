114eb10b236205992fce70248965d25faef213da
Mariusz Nowak <medyk@medikoo.com>
2022-01-06 16:34:04 +0100

    feat: Remove `@serverless/cli` CLI integration
    BREAKING CHANGE:
    Serverless Components v1 (`@serverless/cli`) CLI is no longer integrated with Framework CLI.

--- bin/serverless.js
+++ bin/serverless.js
@@ -60,7 +60,20 @@ require('../lib/cli/triage')().then((cliName) => {
       }
       return;
     case '@serverless/cli':
-      require('@serverless/cli').runComponents();
+      {
+        const chalk = require('chalk');
+        process.stdout.write(
+          `${[
+            'Serverless Components CLI v1 is no longer bundled with Serverless Framework CLI',
+            '',
+            "To run it, ensure it's installed:",
+            chalk.bold('npm install -g @serverless/cli'),
+            '',
+            'Then run:',
+            chalk.bold('components-v1 <command> <options>'),
+          ].join('\n')}\n`
+        );
+      }
       return;
     default:
       throw new Error(`Unrecognized CLI name "${cliName}"`);
