39b4b3796e46fefbd962793f803fe1615cfbac92
Mariusz Nowak <medyk@medikoo.com>
2022-01-17 16:09:50 +0100

    refactor: Remove internal `suppressLogIfPrintCommand` method

--- test/unit/lib/classes/CLI.test.js
+++ test/unit/lib/classes/CLI.test.js
@@ -1,11 +1,8 @@
 'use strict';
 
 const chai = require('chai');
-const sinon = require('sinon');
 const CLI = require('../../../../lib/classes/CLI');
-const overrideArgv = require('process-utils/override-argv');
 const Serverless = require('../../../../lib/Serverless');
-const resolveInput = require('../../../../lib/cli/resolve-input');
 
 const { expect } = chai;
 chai.use(require('sinon-chai'));
@@ -42,75 +39,6 @@ describe('CLI', () => {
       cli.setLoadedPlugins(plugins);
 
       expect(cli.loadedPlugins[0]).to.equal(pluginMock);
-    });
-  });
-
-  describe('#suppressLogIfPrintCommand()', () => {
-    let logStub;
-    let consoleLogStub;
-
-    beforeEach(() => {
-      logStub = sinon.stub();
-      consoleLogStub = sinon.stub();
-    });
-
-    it('should do nothing when no command is given', () => {
-      cli = new CLI(serverless);
-      cli.log = logStub;
-      cli.consoleLog = consoleLogStub;
-
-      cli.suppressLogIfPrintCommand({ commands: [], options: {} });
-      cli.log('logged');
-      cli.consoleLog('logged');
-
-      expect(logStub.calledOnce).to.equal(true);
-      expect(consoleLogStub.calledOnce).to.equal(true);
-    });
-
-    it('should do nothing when "print" is given with "--help"', () => {
-      cli = new CLI(serverless);
-      cli.log = logStub;
-      cli.consoleLog = consoleLogStub;
-
-      resolveInput.clear();
-      overrideArgv({ args: ['serverless', '--help'] }, () =>
-        cli.suppressLogIfPrintCommand({
-          commands: ['print'],
-          options: { help: true },
-        })
-      );
-      resolveInput.clear();
-      cli.log('logged');
-      cli.consoleLog('logged');
-
-      expect(logStub.calledOnce).to.equal(true);
-      expect(consoleLogStub.calledOnce).to.equal(true);
-    });
-
-    it('should do nothing when "print" is combined with other command.', () => {
-      cli = new CLI(serverless);
-      cli.log = logStub;
-      cli.consoleLog = consoleLogStub;
-
-      cli.suppressLogIfPrintCommand({ commands: ['other', 'print'], options: {} });
-      cli.log('logged');
-      cli.consoleLog('logged');
-
-      expect(logStub.calledOnce).to.equal(true);
-      expect(consoleLogStub.calledOnce).to.equal(true);
-    });
-
-    it('should suppress log when "print" is given', () => {
-      cli = new CLI(serverless);
-      cli.log = logStub;
-      cli.consoleLog = consoleLogStub;
-
-      cli.suppressLogIfPrintCommand({ commands: ['print'], options: {} });
-      cli.log('NOT LOGGED');
-      cli.consoleLog('logged');
-
-      expect(logStub.calledOnce).to.equal(false);
-      expect(consoleLogStub.calledOnce).to.equal(true);
     });
   });
 });
