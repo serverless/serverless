04db0bb66a6e189b47cb861cf41772f26a6989a3
Mariusz Nowak <medyk@medikoo.com>
2022-01-24 21:36:43 +0100

    test: Remove obsolete files

--- test/unit/lib/plugins/aws/invokeLocal/fixture/asyncHandlerWithError.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports.withError = () => new Error('failed');
-
-module.exports.withMessage = () => new Error('failed');
-
-module.exports.returnsError = () => new Error('failed');
