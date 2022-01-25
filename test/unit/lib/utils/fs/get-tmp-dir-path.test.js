26a38065a7ff9c64f74579c990d74978f5851214
Mariusz Nowak <medyk@medikoo.com>
2022-01-25 12:32:26 +0100

    refactor: Rename `lib/utils/fs/getTmpDirPath.js`

--- test/unit/lib/utils/fs/get-tmp-dir-path.test.js
+++ test/unit/lib/utils/fs/get-tmp-dir-path.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const getTmpDirPath = require('../../../../../lib/utils/fs/getTmpDirPath');
+const getTmpDirPath = require('../../../../../lib/utils/fs/get-tmp-dir-path');
 
 describe('#getTmpDirPath()', () => {
   it('should return a scoped tmp dir path', () => {
