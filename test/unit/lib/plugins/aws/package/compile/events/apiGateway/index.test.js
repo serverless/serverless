21bc38811eb858885ab98d60eb8c2e7b5cfe5981
Mariusz Nowak <medyk@medikoo.com>
2022-01-25 13:01:01 +0100

    refactor: Rename `lib/plugins/aws/lib/getServiceState.js`

--- test/unit/lib/plugins/aws/package/compile/events/apiGateway/index.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/apiGateway/index.test.js
@@ -6,7 +6,7 @@ const AwsProvider = require('../../../../../../../../../lib/plugins/aws/provider
 const AwsCompileApigEvents = require('../../../../../../../../../lib/plugins/aws/package/compile/events/apiGateway/index');
 const Serverless = require('../../../../../../../../../lib/serverless');
 const validate = require('../../../../../../../../../lib/plugins/aws/lib/validate');
-const getServiceState = require('../../../../../../../../../lib/plugins/aws/lib/getServiceState');
+const getServiceState = require('../../../../../../../../../lib/plugins/aws/lib/get-service-state');
 const updateStage = require('../../../../../../../../../lib/plugins/aws/package/compile/events/apiGateway/lib/hack/updateStage');
 const disassociateUsagePlan = require('../../../../../../../../../lib/plugins/aws/package/compile/events/apiGateway/lib/hack/disassociateUsagePlan');
 
