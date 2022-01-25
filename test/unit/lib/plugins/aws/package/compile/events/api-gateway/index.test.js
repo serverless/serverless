93d1c290433d60ca0be21023a6c2ca2f409d8d1d
Mariusz Nowak <medyk@medikoo.com>
2022-01-25 13:38:15 +0100

    refactor: Rename `…events/api-gateway/lib/hack/disassociateUsagePlan.js`

--- test/unit/lib/plugins/aws/package/compile/events/api-gateway/index.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/api-gateway/index.test.js
@@ -8,7 +8,7 @@ const Serverless = require('../../../../../../../../../lib/serverless');
 const validate = require('../../../../../../../../../lib/plugins/aws/lib/validate');
 const getServiceState = require('../../../../../../../../../lib/plugins/aws/lib/get-service-state');
 const updateStage = require('../../../../../../../../../lib/plugins/aws/package/compile/events/api-gateway/lib/hack/updateStage');
-const disassociateUsagePlan = require('../../../../../../../../../lib/plugins/aws/package/compile/events/api-gateway/lib/hack/disassociateUsagePlan');
+const disassociateUsagePlan = require('../../../../../../../../../lib/plugins/aws/package/compile/events/api-gateway/lib/hack/disassociate-usage-plan');
 
 describe('AwsCompileApigEvents', () => {
   let awsCompileApigEvents;
