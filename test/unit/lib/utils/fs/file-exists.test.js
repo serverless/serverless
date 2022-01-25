7da11fd6769d3ab0f65f609a29af542f0787533b
Mariusz Nowak <medyk@medikoo.com>
2022-01-25 12:32:22 +0100

    refactor: Rename `lib/utils/fs/fileExists.js`

--- test/unit/lib/utils/fs/file-exists.test.js
+++ test/unit/lib/utils/fs/file-exists.test.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const chai = require('chai');
-const fileExists = require('../../../../../lib/utils/fs/fileExists');
+const fileExists = require('../../../../../lib/utils/fs/file-exists');
 
 // Configure chai
 chai.use(require('chai-as-promised'));
