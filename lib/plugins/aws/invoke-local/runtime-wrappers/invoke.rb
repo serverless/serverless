ce5bf0b40f64c087fa5ec114d0d28750b4813aaa
Shalvah <shalvah@users.noreply.github.com>
2022-02-16 21:38:25 +0100

    fix(AWS Local Invocation): Improve `ruby` context (#10705)

--- lib/plugins/aws/invoke-local/runtime-wrappers/invoke.rb
+++ lib/plugins/aws/invoke-local/runtime-wrappers/invoke.rb
@@ -1,44 +1,32 @@
 require 'json'
+require 'securerandom'
 
 class FakeLambdaContext
-  attr_reader :function_name, :function_version
+  attr_reader :function_name, :function_version, :aws_request_id, :log_stream_name, :memory_limit_in_mb,
+    :invoked_function_arn, :log_group_name, :deadline_ms
 
-  def initialize(name: 'Fake', version: 'LATEST', timeout: 6, **options)
-    @function_name = name
+  def initialize(function_name: 'Fake', version: 'LATEST', timeout: 6, **options)
+    @function_name = function_name
     @function_version = version
-    @created_time = Time.now()
+    @memory_limit_in_mb = 1024
     @timeout = timeout
-    options.each {|k,v|
-      send(k, v)
-    }
+
+    # Allow overriding defaults
+    options.each do |k,v|
+      instance_variable_set("@#{k}", v)
+    end
+
+    @aws_request_id = SecureRandom.uuid
+    @invoked_function_arn = "arn:aws:lambda:aws-region:acct-id:function:#{@function_name}"
+    @log_group_name = "/aws/lambda/#{@function_name}"
+    @log_stream_name = Time.now.strftime('%Y/%m/%d') +'/[$' + @function_version + ']58419525dade4d17a495dceeeed44708'
+
+    @created_time = Time.now
+    @deadline_ms = (@created_time + @timeout).to_i * 1000
   end
 
   def get_remaining_time_in_millis
-    [@timeout*1000 - ((Time.now() - @created_time)*1000).round, 0].max
-  end
-
-  def invoked_function_arn
-    "arn:aws:lambda:serverless:#{function_name}"
-  end
-
-  def memory_limit_in_mb
-    return '1024'
-  end
-
-  def aws_request_id
-    return '1234567890'
-  end
-
-  def log_group_name
-    return "/aws/lambda/#{function_name}"
-  end
-
-  def log_stream_name
-    return Time.now.strftime('%Y/%m/%d') +'/[$' + function_version + ']58419525dade4d17a495dceeeed44708'
-  end
-
-  def deadline_ms
-    (@created_time + @timeout).to_i * 1000
+    [@timeout * 1000 - ((Time.now - @created_time) * 1000).round, 0].max
   end
 
   def log(message)
@@ -61,8 +49,7 @@ if __FILE__ == $0
     exit 1
   end
 
-  handler_path = ARGV[0]
-  handler_name = ARGV[1]
+  handler_path, handler_name = ARGV
 
   input = JSON.load($stdin) || {}
 
@@ -75,7 +62,7 @@ if __FILE__ == $0
 
   attach_tty
 
-  context = FakeLambdaContext.new(**input.fetch('context', {}))
+  context = FakeLambdaContext.new(**input.fetch('context', {}).transform_keys(&:to_sym))
   result = Object.const_get(handler_class).send(handler_method, event: input['event'], context: context)
 
   puts result.to_json
