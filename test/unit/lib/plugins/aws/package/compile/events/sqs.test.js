57f27193e052b01cd67745c7576f77b2d8988a6f
Jason Rowsell <76700693+jasonrowsell@users.noreply.github.com>
2023-01-18 10:51:50 +0000

    feat(AWS SQS): Support `functions[].events[].sqs.maximumConcurrency`
    (PR #11678)

--- test/unit/lib/plugins/aws/package/compile/events/sqs.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/sqs.test.js
@@ -202,6 +202,7 @@ describe('test/unit/lib/plugins/aws/package/compile/events/sqs.test.js', () => {
                     batchSize: 10,
                     maximumBatchingWindow: 100,
                     filterPatterns: [{ a: [1, 2] }, { b: [3, 4] }],
+                    maximumConcurrency: 250,
                   },
                 },
               ],
@@ -244,6 +245,12 @@ describe('test/unit/lib/plugins/aws/package/compile/events/sqs.test.js', () => {
             Pattern: JSON.stringify({ b: [3, 4] }),
           },
         ],
+      });
+    });
+
+    it('should have correct maximum concurrency', () => {
+      expect(eventSourceMappingResource.Properties.ScalingConfig).to.deep.equal({
+        MaximumConcurrency: 250,
       });
     });
   });
