c64f6bea8da427166de36c92bc767bdab70cf42a
Anuj Jindal <31108317+jindalAnuj@users.noreply.github.com>
2023-01-25 21:21:11 +0530

    Update cloud-front.test.js

--- test/unit/lib/plugins/aws/package/compile/events/cloud-front.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/cloud-front.test.js
@@ -11,7 +11,7 @@ chai.use(require('chai-as-promised'));
 
 const { expect } = chai;
 
-describe.only('AwsCompileCloudFrontEvents', () => {
+describe('AwsCompileCloudFrontEvents', () => {
   let serverless;
   let awsCompileCloudFrontEvents;
   let options;
@@ -168,7 +168,7 @@ describe.only('AwsCompileCloudFrontEvents', () => {
   });
 });
 
-describe.only('test/unit/lib/plugins/aws/package/compile/events/cloudFront.test.js', () => {
+describe('test/unit/lib/plugins/aws/package/compile/events/cloudFront.test.js', () => {
   describe.skip('TODO: Removal notice', () => {
     it('should show preconfigured notice on "sls remove" if service has cloudFront event', async () => {
       // Replaces
