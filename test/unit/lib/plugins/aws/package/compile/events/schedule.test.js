9454dba340752425452a5c54c09aff43ad2564a4
Ryan Seaboyer <ryan.seaboyer@icloud.com>
2023-01-30 07:11:28 -0400

    Fix up review comments

--- test/unit/lib/plugins/aws/package/compile/events/schedule.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/schedule.test.js
@@ -103,9 +103,6 @@ describe('test/unit/lib/plugins/aws/package/compile/events/schedule.test.js', ()
         },
       },
       {
-        schedule: { 'Fn::If': ['Condition', 'cron(0 0/4 ? * MON-FRI *)', 'rate(1 hour)'] },
-      },
-      {
         schedule: {
           rate: [{ 'Fn::If': ['Condition', 'cron(0 0/4 ? * MON-FRI *)', 'rate(1 hour)'] }],
         },
