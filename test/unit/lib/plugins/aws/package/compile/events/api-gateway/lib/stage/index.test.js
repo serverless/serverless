9be9becbc7200f8a5a1bffdb32ec5b8f19f2ce6f
charlie Benger-stevenson <charlie@thebpmgroup.co.uk>
2023-03-03 10:39:05 +0000

    Fix for #11675

--- test/unit/lib/plugins/aws/package/compile/events/api-gateway/lib/stage/index.test.js
+++ test/unit/lib/plugins/aws/package/compile/events/api-gateway/lib/stage/index.test.js
@@ -407,4 +407,21 @@ describe('test/unit/lib/plugins/aws/package/compile/events/apiGateway/lib/stage/
       },
     });
   });
+
+  it('should use stage name from provider if provider.apiGateway.stage is configured', async () => {
+    // https://github.com/serverless/serverless/issues/11675
+    const { cfTemplate, awsNaming } = await runServerless({
+      fixture: 'api-gateway',
+      command: 'package',
+      configExt: {
+        provider: {
+          apiGateway: {
+            stage: 'foo',
+          },
+        },
+      },
+    });
+    expect(cfTemplate).to.be.ok;
+    expect(awsNaming.provider.getStage()).to.equal('foo');
+  });
 });
