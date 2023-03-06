ae15d4ec13a5ffa1e11108795c160c01303f3495
Himanshu Pant <himanshupant127@gmail.com>
2023-03-06 22:54:14 +0530

    fix: Changed runtimeManagement function config test

--- test/unit/lib/plugins/aws/package/compile/functions.test.js
+++ test/unit/lib/plugins/aws/package/compile/functions.test.js
@@ -1351,7 +1351,7 @@ describe('lib/plugins/aws/package/compile/functions/index.test.js', () => {
       const { UpdateRuntimeOn } =
         cfResources[naming.getLambdaLogicalId('foo')].Properties.RuntimeManagementConfig;
 
-      expect(UpdateRuntimeOn).to.equal('Function update');
+      expect(UpdateRuntimeOn).to.equal('FunctionUpdate');
     });
 
     it('should support `provider.versionFunctions: false`', () => {
