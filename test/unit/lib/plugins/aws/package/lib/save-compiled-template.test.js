4d64730130c44cdfbb872f8a16111df409135dc8
Mieszko Kycermann <59399685+Kycermann@users.noreply.github.com>
2023-05-24 16:53:41 +0100

    feat(AWS Deploy): `--minify-template` CLI param (#11980)

--- test/unit/lib/plugins/aws/package/lib/save-compiled-template.test.js
+++ test/unit/lib/plugins/aws/package/lib/save-compiled-template.test.js
@@ -1,46 +1,47 @@
 'use strict';
 
 const expect = require('chai').expect;
-const sinon = require('sinon');
+const runServerless = require('../../../../../../utils/run-serverless');
+const fs = require('fs').promises;
 const path = require('path');
-const AwsPackage = require('../../../../../../../lib/plugins/aws/package/index');
-const Serverless = require('../../../../../../../lib/serverless');
-const AwsProvider = require('../../../../../../../lib/plugins/aws/provider');
 
 describe('#saveCompiledTemplate()', () => {
-  let serverless;
-  let awsPackage;
-  let getCompiledTemplateFileNameStub;
-  let writeFileSyncStub;
-
-  beforeEach(() => {
-    const options = {};
-    serverless = new Serverless({ commands: [], options: {} });
-    serverless.setProvider('aws', new AwsProvider(serverless, options));
-    awsPackage = new AwsPackage(serverless, options);
-    serverless.serviceDir = 'my-service';
-    serverless.service = {
-      provider: {
-        compiledCloudFormationTemplate: 'compiled content',
-      },
-    };
-    getCompiledTemplateFileNameStub = sinon
-      .stub(awsPackage.provider.naming, 'getCompiledTemplateFileName')
-      .returns('compiled.json');
-    writeFileSyncStub = sinon.stub(awsPackage.serverless.utils, 'writeFileSync').returns();
-  });
-
-  afterEach(() => {
-    awsPackage.provider.naming.getCompiledTemplateFileName.restore();
-    awsPackage.serverless.utils.writeFileSync.restore();
-  });
-
-  it('should write the compiled template to disk', async () => {
-    const filePath = path.join(awsPackage.serverless.serviceDir, '.serverless', 'compiled.json');
-
-    return awsPackage.saveCompiledTemplate().then(() => {
-      expect(getCompiledTemplateFileNameStub.calledOnce).to.equal(true);
-      expect(writeFileSyncStub.calledWithExactly(filePath, 'compiled content')).to.equal(true);
+  it('should save the compiled template to disk', async () => {
+    const result = await runServerless({
+      fixture: 'aws',
+      command: ['package'],
     });
+
+    const cfTemplateFilePath = path.join(
+      result.fixtureData.servicePath,
+      '.serverless',
+      'cloudformation-template-update-stack.json'
+    );
+
+    const cfTemplateJsonOnDisk = await fs.readFile(cfTemplateFilePath, 'utf-8');
+    const cfTemplateOnDisk = JSON.parse(cfTemplateJsonOnDisk);
+
+    expect(cfTemplateOnDisk).to.deep.equal(result.cfTemplate);
+  });
+
+  it('should minify compiled template if --minify-template is set', async () => {
+    const result = await runServerless({
+      fixture: 'aws',
+      command: ['package'],
+      options: {
+        'minify-template': true,
+      },
+    });
+
+    const cfTemplateFilePath = path.join(
+      result.fixtureData.servicePath,
+      '.serverless',
+      'cloudformation-template-update-stack.json'
+    );
+
+    const cfTemplateJsonOnDisk = await fs.readFile(cfTemplateFilePath, 'utf-8');
+    const cfMinifiedTemplateJson = JSON.stringify(result.cfTemplate, null, 0);
+
+    expect(cfTemplateJsonOnDisk).to.equal(cfMinifiedTemplateJson);
   });
 });
