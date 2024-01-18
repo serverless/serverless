ee9de3ca853b395db25c4909385a2dc791b6d6e5
Austen Collins <austencollins@gmail.com>
2024-01-17 22:38:07 -0800

    chore: continue refactor

--- lib/plugins/dashboard/sdk-py/vendorize-wrapt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r serverless_sdk/vendor
-mkdir serverless_sdk/vendor
-docker run --rm -it -v $PWD/serverless_sdk/vendor:/var/task/vendor \
-    lambci/lambda:build-python3.7 \
-    pip install --target /var/task/vendor wrapt
