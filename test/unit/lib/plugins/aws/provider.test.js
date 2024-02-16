b75178a547cbe5d795f33d981bb7c8c86bae783b
martinezpl <royfish@proton.me>
2024-02-16 18:56:39 +0100

    Add tests

--- test/unit/lib/plugins/aws/provider.test.js
+++ test/unit/lib/plugins/aws/provider.test.js
@@ -855,6 +855,35 @@ aws_secret_access_key = CUSTOMSECRET
       );
     });
 
+    it('should fail if `functions[].image` references image with both buildOptions and uri', async () => {
+      await expect(
+        runServerless({
+          fixture: 'function',
+          command: 'package',
+          configExt: {
+            provider: {
+              ecr: {
+                images: {
+                  invalidimage: {
+                    buildOptions: ['--no-cache'],
+                    uri: '000000000000.dkr.ecr.sa-east-1.amazonaws.com/test-lambda-docker@sha256:6bb600b4d6e1d7cf521097177dd0c4e9ea373edb91984a505333be8ac9455d38',
+                  },
+                },
+              },
+            },
+            functions: {
+              fnProviderInvalidImage: {
+                image: 'invalidimage',
+              },
+            },
+          },
+        })
+      ).to.be.eventually.rejected.and.have.property(
+        'code',
+        'ECR_IMAGE_BOTH_URI_AND_BUILDOPTIONS_DEFINED_ERROR'
+      );
+    });
+
     it('should fail if `functions[].image` references image with both cacheFrom and uri', async () => {
       await expect(
         runServerless({
@@ -1717,6 +1746,65 @@ aws_secret_access_key = CUSTOMSECRET
           path.join(serviceDir, 'Dockerfile.dev'),
           '--build-arg',
           'TESTKEY=TESTVAL',
+          './',
+        ]);
+      });
+
+      it('should work correctly when image is defined with `buildOptions` set', async () => {
+        const awsRequestStubMap = {
+          ...baseAwsRequestStubMap,
+          ECR: {
+            ...baseAwsRequestStubMap.ECR,
+            describeRepositories: describeRepositoriesStub.resolves({
+              repositories: [{ repositoryUri }],
+            }),
+            createRepository: createRepositoryStub,
+          },
+        };
+        const {
+          awsNaming,
+          cfTemplate,
+          fixtureData: { servicePath: serviceDir },
+        } = await runServerless({
+          fixture: 'ecr',
+          command: 'package',
+          awsRequestStubMap,
+          modulesCacheStub,
+          configExt: {
+            provider: {
+              ecr: {
+                images: {
+                  baseimage: {
+                    path: './',
+                    file: 'Dockerfile.dev',
+                    buildOptions: ['--ssh', 'default=/path/to/file'],
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        const functionCfLogicalId = awsNaming.getLambdaLogicalId('foo');
+        const functionCfConfig = cfTemplate.Resources[functionCfLogicalId].Properties;
+        const versionCfConfig = Object.values(cfTemplate.Resources).find(
+          (resource) =>
+            resource.Type === 'AWS::Lambda::Version' &&
+            resource.Properties.FunctionName.Ref === functionCfLogicalId
+        ).Properties;
+
+        expect(functionCfConfig.Code.ImageUri).to.deep.equal(`${repositoryUri}@sha256:${imageSha}`);
+        expect(versionCfConfig.CodeSha256).to.equal(imageSha);
+        expect(describeRepositoriesStub).to.be.calledOnce;
+        expect(createRepositoryStub.notCalled).to.be.true;
+        expect(spawnExtStub).to.be.calledWith('docker', [
+          'build',
+          '-t',
+          `${awsNaming.getEcrRepositoryName()}:baseimage`,
+          '-f',
+          path.join(serviceDir, 'Dockerfile.dev'),
+          '--ssh',
+          'default=/path/to/file',
           './',
         ]);
       });
