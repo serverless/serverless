5b448e690b1a6829918a8601b5d6c31e5425ae3b
Tomasz Czubocha <tomasz.czubocha@gmail.com>
2024-04-11 18:51:25 +0200

    remove the rest of variables-related code and variablesMeta and isDashboardEnabledForService from Serverless constructor

--- test/unit/lib/configuration/variables/sources/fixture/file-function-errored.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = () => {
-  throw new Error('Stop');
-};
--- test/unit/lib/configuration/variables/sources/fixture/file-function-variable-missing-source.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = async ({ resolveVariable }) => ({
-  varResult: await resolveVariable('file(missing.yaml)'),
-});
