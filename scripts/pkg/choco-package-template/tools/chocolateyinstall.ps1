bb55e0b38c30348c3284bbdba672b97cf18f3d39
Austen Collins <austencollins@gmail.com>
2024-04-24 21:03:03 -0700

    chore: Remove node-fetch

--- scripts/pkg/choco-package-template/tools/chocolateyinstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$fileLocation = Join-Path $toolsDir 'serverless.exe'
-
-Install-BinFile -Name serverless -Path $fileLocation
-Install-BinFile -Name sls -Path $fileLocation
-
-& 'serverless' 'binary-postinstall'
