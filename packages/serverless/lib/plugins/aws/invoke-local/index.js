c1bea519313a28cf9b081c4072ae123c2e9f0859
Dai Akatsuka <d.akatsuka@gmail.com>
2026-01-07 13:11:43 +0900

    feat: add ruby3.4 runtime

--- packages/serverless/lib/plugins/aws/invoke-local/index.js
+++ packages/serverless/lib/plugins/aws/invoke-local/index.js
@@ -415,7 +415,7 @@ class AwsInvokeLocal {
       )
     }
 
-    if (['ruby2.7', 'ruby3.2', 'ruby3.3'].includes(runtime)) {
+    if (['ruby2.7', 'ruby3.2', 'ruby3.3', 'ruby3.4'].includes(runtime)) {
       const handlerComponents = handler.split(/\./)
       const handlerPath = handlerComponents[0]
       const handlerName = handlerComponents.slice(1).join('.')
